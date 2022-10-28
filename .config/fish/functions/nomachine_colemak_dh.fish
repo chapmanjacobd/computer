ea420a13d605de3f36f6a9f6a0506e1fd3caf39a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-10-28 14:08:38 -0500

    funccp nomachine_colemak_dh

---
+++ .config/fish/functions/nomachine_colemak_dh.fish
@@ -0,0 +1,5 @@
+# Defined interactively
+function nomachine_colemak_dh
+    sudo cp /usr/share/X11/xkb/symbols/us /usr/NX/share/X11/xkb/symbols/us
+    setxkbmap -device (xinput list | grep -Eo "Virtual core XTEST keyboard\s*id\=[0-9]{1,2}" | cut -d= -f 2) -model pc105 -layout us -variant colemak_dh
+end
