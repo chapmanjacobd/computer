b35b9047a049ad92b9f382ac8b3eb8a57ca87c9a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-10-29 11:00:27 -0500

    dns-set-google lsnet gitignore

---
+++ .config/fish/functions/dns-set-google.fish
@@ -0,0 +1,7 @@
+function dns-set-google
+    set conName (default "$argv" (nmcli -t -f NAME,TIMESTAMP con show | sort -t: -nk2 | tail -n1 | cut -d: -f1 | sed -e 's/[[:space:]]*$//'))
+    nmcli con mod "$conName" ipv4.dns "1.1.1.1 8.8.8.8"
+    nmcli con mod "$conName" ipv4.ignore-auto-dns yes
+    nmcli con down "$conName"
+    nmcli con up "$conName"
+end
---
+++ .config/fish/functions/lsnet.fish
@@ -0,0 +1,3 @@
+function lsnet
+    nmcli con
+end
