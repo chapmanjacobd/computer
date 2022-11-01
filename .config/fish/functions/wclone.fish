b91c9f45495679e9c48c52cd4bce0abfaac567cb
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-01 02:14:17 -0500

    settings wclone wclone1 gitignore

---
+++ .config/fish/functions/wclone.fish
@@ -0,0 +1,5 @@
+# Defined in /tmp/fish.tOOX4g/wclone.fish @ line 2
+function wclone
+    ~/d/53\ Scrapbook\ Web/y
+    wget --adjust-extension -e robots=off -np -nc -r -l inf -p --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36" $argv
+end
