3a5b0de970b8eaae8fb769dc221734b0c9884140
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-02 10:12:06 -0500

    dlstatus

--- .config/fish/functions/dlstatus.fish
+++ .config/fish/functions/dlstatus.fish
@@ -1,11 +1,7 @@
-# Defined via `source`
+# Defined interactively
 function dlstatus
-    for folder in ~/.jobs/todo/*
-        set folder (basename $folder)
-        fish -c "combine ~/.jobs/todo/$folder not (cat ~/.jobs/done/* | psub) | sort --unique | sponge ~/.jobs/todo/$folder" &
-
-        printf '%s\t%s\n' (combine ~/.jobs/todo/$folder not ~/.jobs/done/$folder | wc -l) $folder
+    for db in ~/lb/audio.db ~/lb/video.db ~/lb/reddit/*db
+        echo $db
+        lb ds $db
     end
-
-    wait
 end
