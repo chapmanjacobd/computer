ceb771efcbdb69ece4dbe5d3a37766359896f2a5
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-03 21:24:18 -0500

    dl

--- .config/fish/functions/dl.fish
+++ .config/fish/functions/dl.fish
@@ -1,14 +1,9 @@
 # Defined interactively
 function dl
-    b dl-get-sounds 82_Audiobooks
-    b dl-get-sounds 83_ClassicalComposers
-    b dl-get-sounds 81_New_Music
-    b dl-get-videos 71_Mealtime_Videos $argv
-    b dl-get-videos 75_MovieQueue $argv --no-match-filter --match-filter 'requested_subtitles & duration > 1440' --reject-title="Um Himmels|Two Dads"
-    b dl-get-videos 96_Weird_History $argv
-    b dl-get-photos 91_New_Art
-    b dl-get-sounds 63_Sounds
-    wait
-    dl-get-videos 69_Taxes $argv
-    dl-get-photos 61_Photos_Unsorted
+lb download ~/lb/audio.db --audio --prefix /mnt/d/81_New_Music/
+lb download ~/lb/video.db --video --small --prefix /mnt/d/71_Mealtime_Videos/
+lb download ~/lb/reddit/63_Sounds.db --audio --safe --prefix ~/d/63_Sounds/
+lb download ~/lb/reddit/81_New_Music.db --audio --safe --prefix ~/d/81_New_Music/
+lb download ~/lb/reddit/83_ClassicalComposers.db --audio --safe --prefix ~/d/83_ClassicalComposers/
+lb download ~/lb/reddit/71_Mealtime_Videos.db --video --small --safe --prefix ~/d/71_Mealtime_Videos/
 end
