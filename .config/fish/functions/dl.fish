7bdadf9277d4d084a63108223fd9da71a45b4aa6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-03 21:24:38 -0500

    dl

--- .config/fish/functions/dl.fish
+++ .config/fish/functions/dl.fish
@@ -1,9 +1,9 @@
 # Defined interactively
 function dl
-lb download ~/lb/audio.db --audio --prefix /mnt/d/81_New_Music/
-lb download ~/lb/video.db --video --small --prefix /mnt/d/71_Mealtime_Videos/
-lb download ~/lb/reddit/63_Sounds.db --audio --safe --prefix ~/d/63_Sounds/
-lb download ~/lb/reddit/81_New_Music.db --audio --safe --prefix ~/d/81_New_Music/
-lb download ~/lb/reddit/83_ClassicalComposers.db --audio --safe --prefix ~/d/83_ClassicalComposers/
-lb download ~/lb/reddit/71_Mealtime_Videos.db --video --small --safe --prefix ~/d/71_Mealtime_Videos/
+    lb download ~/lb/audio.db --audio --prefix /mnt/d/81_New_Music/
+    lb download ~/lb/video.db --video --small --prefix /mnt/d/71_Mealtime_Videos/
+    lb download ~/lb/reddit/63_Sounds.db --audio --safe --prefix ~/d/63_Sounds/
+    lb download ~/lb/reddit/81_New_Music.db --audio --safe --prefix ~/d/81_New_Music/
+    lb download ~/lb/reddit/83_ClassicalComposers.db --audio --safe --prefix ~/d/83_ClassicalComposers/
+    lb download ~/lb/reddit/71_Mealtime_Videos.db --video --small --safe --prefix ~/d/71_Mealtime_Videos/
 end
