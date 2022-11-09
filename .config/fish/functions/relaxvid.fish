cd6218d8ae8735a5be8e54504882fb386ed645f9
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-09 12:22:40 -0600

    relaxvid vnc-10400

--- .config/fish/functions/relaxvid.fish
+++ .config/fish/functions/relaxvid.fish
@@ -2,6 +2,6 @@
 function relaxvid
     fish -c 'lb extract -f ~/lb/tax.db ~/d/60_Now_Watching/ ~/d/64_Relaxation/ ~/d/69_Taxes_Keep/' &
     mpv --input-ipc-server=/tmp/mpv_socket --no-video --af="acompressor=ratio=4,loudnorm" (fd -eOPUS -eOGA . ~/d/64_Relaxation) &
-    lb-dev wt ~/lb/tax.db -L inf --action askkeep --keep-dir /home/xk/d/60_Now_Watching/keep/ -m 2 $argv --sort "path like '%64_Relaxation%' desc, time_modified, duration desc, priority" 
+    lb-dev wt ~/lb/tax.db -L inf --action askkeep --keep-dir /home/xk/d/60_Now_Watching/keep/ -m 2 $argv --sort "path like '%64_Relaxation%' desc, time_modified, duration desc, priority"
     pkill -x mpv
 end
