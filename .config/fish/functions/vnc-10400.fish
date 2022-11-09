319f0fabc675e2f8017e4d5cb0cb5bfe6cd056bc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-09 14:06:37 -0600

    vnc-10400 kglobalshortcutsrc

--- .config/fish/functions/vnc-10400.fish
+++ .config/fish/functions/vnc-10400.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function vnc-10400
-    ssh xk@10400 -fN -L 4102:localhost:4000 && /usr/NX/bin/nxplayer --session ~/bin/nomachine/ssh_10400.nxs
+    ssh xk@10400 -f -L 4102:localhost:4000 sleep 20 && /usr/NX/bin/nxplayer --session ~/bin/nomachine/ssh_10400.nxs
 end
