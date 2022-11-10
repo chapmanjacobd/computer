e2b93927ec3cb1901325fb340e31cb093134e96a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-10 09:04:49 -0600

    untrash

--- .config/fish/functions/untrash.fish
+++
@@ -1,5 +0,0 @@
-function untrash
-    #mv -iv ~/.local/share/Trash/files/$argv[1] ./
-
-    mv -iv ~/.local/share/Trash/files/(ls -snew -G  /home/xk/.local/share/Trash/files/  | tail -1) "./$argv"
-end
