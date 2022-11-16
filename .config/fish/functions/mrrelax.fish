74a38db49092fb55d52ede5ac479a6ebd50456d7
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-16 12:30:59 -0600

    mrrelax gitignore

--- .config/fish/functions/mrrelax.fish
+++ .config/fish/functions/mrrelax.fish
@@ -1,9 +1,9 @@
 function mrrelax
     rsync -a --remove-source-files --backup-dir (date "+%d.%m.%Y") --exclude=".*" ~/d/64_Relaxation/keep/ ~/d/62_Photos_Keep/
 
-    fd --max-results=500 -tf . ~/d/61_Photos_Unsorted/ -0 | xargs -0 -I{} mv "{}" ~/d/64_Relaxation/
+    fd --max-results=50 -tf . ~/d/61_Photos_Unsorted/ -0 | xargs -0 -I{} mv "{}" ~/d/64_Relaxation/
 
-    fd --max-results=500 -tf . ~/d/91_New_Art/ -0 | xargs -0 -I{} mv "{}" ~/d/90_Now_Viewing/
+    fd --max-results=50 -tf . ~/d/91_New_Art/ -0 | xargs -0 -I{} mv "{}" ~/d/90_Now_Viewing/
 
     rsync -a --remove-source-files --backup-dir (date "+%d.%m.%Y") --files-from=(
         lt ~/lb/63_Sounds.db -s /mnt/d/64_Relaxation/ -p f --moved /mnt/d/64_Relaxation/ /mnt/d/ | psub
