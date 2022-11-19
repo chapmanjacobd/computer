08dd0c3ea4e5ab68a3fd8f36ad6cf4eff5c75a04
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-18 18:18:03 -0600

    dlstatus

--- .config/fish/functions/dlstatus.fish
+++ .config/fish/functions/dlstatus.fish
@@ -1,13 +1,7 @@
 # Defined interactively
 function dlstatus
-    lb ds ~/lb/audio.db
-    lb ds ~/lb/yt/podcasts.tl.db
-    lb ds ~/lb/reddit/63_Sounds.db
-    lb ds ~/lb/reddit/81_New_Music.db
-    lb ds ~/lb/yt/classical_music.tl.db
-    lb ds ~/lb/reddit/83_ClassicalComposers.db
-
-    lb ds ~/lb/video.db
-    lb ds ~/lb/reddit/71_Mealtime_Videos.db
-    lb ds ~/lb/hackernews/hackernews_only_direct.tw.db
+    for db in ~/lb/audio.db ~/lb/yt/podcasts.tl.db ~/lb/reddit/63_Sounds.db ~/lb/hackernews/hackernews_only_direct.tw.db ~/lb/reddit/81_New_Music.db ~/lb/yt/classical_music.tl.db ~/lb/reddit/83_ClassicalComposers.db ~/lb/video.db ~/lb/reddit/71_Mealtime_Videos.db
+        echo $db
+        lb ds $db
+    end
 end
