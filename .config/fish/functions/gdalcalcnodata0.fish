fbfec48e24bea301337804b9fdad36970b74260f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-19 17:48:36 -0600

    gdalcalcnodata0

--- .config/fish/functions/gdalcalcnodata0.fish
+++ .config/fish/functions/gdalcalcnodata0.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.2FTqNn/times1nodata0.fish @ line 2
-function times1nodata0
+# Defined in /tmp/fish.2FTqNn/gdalcalcnodata0.fish @ line 2
+function gdalcalcnodata0
     gdal_calc.py -A "$argv" --outfile="$argv" --calc="A*1" --NoDataValue=0 --co="compress=lzw"
 end
