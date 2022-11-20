999846fe4d3132b5038f67283e600ca61857c2df
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-20 00:35:47 -0600

    split-by-silence-cleanup time-until gitignore

--- .config/fish/functions/split-by-silence-cleanup.fish
+++ .config/fish/functions/split-by-silence-cleanup.fish
@@ -1,4 +1,4 @@
 # Defined interactively
-function trash-split-by-silence-records
+function split-by-silence-cleanup
     fd .000. | grep -i '\.000\.' | sed 's|\.000\..*||' | string escape | xargs trash-put
 end
