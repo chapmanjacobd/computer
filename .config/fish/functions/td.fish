71318532339e847f9d89806cea7fa271a944b596
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-22 00:16:14 -0600

    td gitignore

---
+++ .config/fish/functions/td.fish
@@ -0,0 +1,9 @@
+function td
+    set -l prefix temp
+    if test (count $argv) -eq 1
+        set prefix "temp-$argv[1]"
+    end
+    set -l dir /tmp/$prefix-(random)
+    mkdir $dir
+    cd $dir
+end
