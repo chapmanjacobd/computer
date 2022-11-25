634dc9a6fd47d7e1ede43808bd38e52283e57d1f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-25 11:50:08 -0600

    systemd-cron kglobalshortcutsrc gitignore

---
+++ .config/fish/functions/systemd-cron.fish
@@ -0,0 +1,30 @@
+function systemd-cron
+    # systemd-cron wakeup '*-*-* 9:30' lt-wakeup
+    set unit $argv[1]
+    set cal $argv[2]
+    set cmd $argv[3..-1]
+
+    echo "[Unit]
+Description='On $cal run $cmd'
+
+[Service]
+Type=simple
+RemainAfterExit=no
+ExecStart='/usr/bin/fish' '-c' '$cmd'
+" >~/.config/systemd/user/$unit.service
+
+    echo "[Unit]
+Description='On $cal run $cmd'
+
+[Timer]
+Persistent=yes
+OnCalendar=$cal
+
+[Install]
+WantedBy=timers.target
+" >~/.config/systemd/user/$unit.timer
+
+    systemctl --user daemon-reload
+    systemctl --user enable --now $unit.timer
+    systemctl --user list-timers --no-pager --all
+end
