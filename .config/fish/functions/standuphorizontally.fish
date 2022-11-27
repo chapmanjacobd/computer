1bd9b116763b8ed27e396790f583d25a6235185b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-27 06:26:05 -0600

    standuphorizontally gitignore

---
+++ .config/fish/functions/standuphorizontally.fish
@@ -0,0 +1,15 @@
+# Defined interactively
+function standuphorizontally
+    if grep -qEi "(DisplayPort-0)" (kscreen-doctor -o | psub)
+        kscreen-doctor output.DisplayPort-0.disable output.HDMI-A-0.disable output.DVI-D-0.disable
+        kscreen-doctor output.DVI-D-0.enable output.DVI-D-0 output.DVI-D-0.mode.1920x1080@60
+    else
+        kscreen-doctor output.DP-1.disable output.HDMI-1.disable output.DVI-D-1.disable
+        kscreen-doctor output.DVI-D-1.enable output.DVI-D-1 output.DVI-D-1.mode.1920x1080@50
+    end
+    krohnkite_off
+    #bash -c 'kquitapp5 plasmashell || killall plasmashell; kstart5 plasmashell'
+    keepscreenoff && xset +dpms
+    #breaktimer disable
+    ~/.config/autostart.sh &
+end
