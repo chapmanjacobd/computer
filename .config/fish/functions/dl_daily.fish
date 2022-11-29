e8f9cf3d31f4b4a1203d3dc642e16ba40fde58aa
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-28 18:07:20 -0600

    dl_daily settings

--- .config/fish/functions/dl_daily.fish
+++ .config/fish/functions/dl_daily.fish
@@ -1,8 +1,9 @@
 function dl_daily
+    ~/
     sed -i '$ d' .gitignore
+    sed -i '$ d' j/.gitignore
 
     pip install --upgrade gallery-dl
-
     for dfolder in 61_Photos_Unsorted 91_New_Art 95_Memes
         ~/d/$dfolder/
         gallery-dl --input-file (sed 's|^|https://www.instagram.com/\0|' ~/mc/$dfolder-instagram.txt | shuf | head -20 | psub)
