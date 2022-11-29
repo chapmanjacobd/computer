3b3fc6194101e56507c45871eabdf9370ee53b2f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-11-29 03:24:36 -0600

    splitcsv gitignore

---
+++ .config/fish/functions/splitcsv.fish
@@ -0,0 +1,9 @@
+function splitcsv
+    awk -v l=2000000 '(NR==1){header=$0;next}
+                (NR%l==2) { 
+                   c=sprintf("%0.5d",c+1); 
+                   close(file); file=FILENAME; sub(/csv$/,c".csv",file)
+                   print header > file
+                }
+                {print $0 > file}' $argv
+end
