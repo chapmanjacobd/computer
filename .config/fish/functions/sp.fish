134ab6f136acb5fdb1aa7f4c5fa6f21468ee944d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-02 01:14:50 -0600

    sp gitignore

---
+++ .config/fish/functions/sp.fish
@@ -0,0 +1,11 @@
+function sp
+    google-chrome https://www.pexels.com/search/"$argv"
+
+    google-chrome https://unsplash.com/search/photos/"$argv"
+
+    google-chrome https://pixabay.com/en/photos/"$argv"
+
+    google-chrome 'https://www.flickr.com/search/?text='"$argv"'&license=7%2C9%2C10&sort=date-taken-desc'
+
+    google-chrome https://stocksnap.io/search/"$argv"
+end
