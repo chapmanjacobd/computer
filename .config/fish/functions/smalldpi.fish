eb5e6dffa6f28c053be5eec3e30d11b9417187b4
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-05 01:05:22 -0600

    smalldpi gitignore

---
+++ .config/fish/functions/smalldpi.fish
@@ -0,0 +1,10 @@
+function smalldpi
+    xrandr --dpi 96
+    echo 'Xft.dpi: 96' | xrdb -override
+    sed -i 's/ScaleFactor=2/ScaleFactor=1' ~/.config/kdeglobals
+    set fopt layout.css.devPixelsPerPx
+    set fset 1
+    cd ~/.mozilla/firefox/8eqln2tw.default/
+    sed -i 's/user_pref("'$fopt'",.*);/user_pref("'$fopt'","'$fset'");/' user.js
+    grep -q $fopt user.js || echo "user_pref(\"$fopt\",$fset);" >>user.js
+end
