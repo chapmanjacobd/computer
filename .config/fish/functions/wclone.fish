ae29a331a2204ba531490206c9497e40ebe8f29d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-06 23:22:55 -0600

    settings wclone

--- .config/fish/functions/wclone.fish
+++ .config/fish/functions/wclone.fish
@@ -1,5 +1,5 @@
-# Defined in /tmp/fish.tOOX4g/wclone.fish @ line 2
+# Defined interactively
 function wclone
-    ~/d/53\ Scrapbook\ Web/y
+    ~/d/53_Scrapbook_Web/
     wget --adjust-extension -e robots=off -np -nc -r -l inf -p --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36" $argv
 end
