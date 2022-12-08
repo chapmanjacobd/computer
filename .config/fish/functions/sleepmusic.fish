65311bde6f8e92b396d5968563663b1880e2a472
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-08 09:54:34 -0600

    sleepmusic gitignore

---
+++ .config/fish/functions/sleepmusic.fish
@@ -0,0 +1,10 @@
+# Defined interactively
+function sleepmusic
+    catt -d "Bedroom pair" set_default
+    fish -c '~/lb/ && lb lt -L 99 -cast -cast-to "Bedroom pair" -s relax' &
+    sleep 8
+    catt volume 25
+    sleep 2700
+    lt-stop
+    catt -d "Xylo and Orchestra" set_default
+end
