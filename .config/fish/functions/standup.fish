b54ed53a01e78ad33a0f097b73d9f5e02d60e622
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-13 00:14:56 -0600

    sitdown standup tv

--- .config/fish/functions/standup.fish
+++ .config/fish/functions/standup.fish
@@ -1,11 +1,11 @@
 # Defined interactively
 function standup
     if grep -qEi "(DisplayPort-0)" (kscreen-doctor -o | psub)
-        kscreen-doctor output.DisplayPort-0.disable output.HDMI-A-1.disable output.DVI-D-1.disable
-        kscreen-doctor output.DVI-D-1.enable output.DVI-D-1.rotation.right output.DVI-D-1.mode.1920x1080@60
+        kscreen-doctor output.DisplayPort-0.disable output.HDMI-A-0.disable output.DVI-D-0.disable
+        kscreen-doctor output.DVI-D-0.enable output.DVI-D-1.rotation.right output.DVI-D-1.mode.1920x1080@60
     else
-        kscreen-doctor output.DP-1.disable output.HDMI-1.disable output.DVI-D-1.disable
-        kscreen-doctor output.DVI-D-1.enable output.DVI-D-1.rotation.right output.DVI-D-1.mode.1920x1080@50
+        kscreen-doctor output.DP-1.disable output.HDMI-1.disable output.DVI-D-0.disable
+        kscreen-doctor output.DVI-D-0.enable output.DVI-D-1.rotation.right output.DVI-D-1.mode.1920x1080@50
     end
     krohnkite_off
     #bash -c 'kquitapp5 plasmashell || killall plasmashell; kstart5 plasmashell'
