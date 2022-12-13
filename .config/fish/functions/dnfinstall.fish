29e2fdadac8e6003bea187e92ef194be75f9c026
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-12 23:15:06 -0600

    dnfinstall

--- .config/fish/functions/dnfinstall.fish
+++ .config/fish/functions/dnfinstall.fish
@@ -2,6 +2,6 @@
 function dnfinstall --wraps='dnf install'
     sudo dnf install $argv
     and echo $argv >>~/.github/dnf_installed
-    
+
     sort --unique --ignore-case ~/.github/dnf_installed | sponge ~/.github/dnf_installed
 end
