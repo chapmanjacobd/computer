14e2e878caa4616d897d8674b398ea33400e9947
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-13 13:25:40 -0600

    config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 set CLOUDSDK_PYTHON python3.9
-bass source /etc/profile.d/flatpak.sh /etc/profile.d/kde.sh /etc/profile.d/vte.sh
-bass 'eval "$(dircolors)"'
+bass source /etc/profile
+#bass 'eval "$(dircolors)"'
 set NPM_PACKAGES "$HOME/.npm-packages"
 set PATH $PATH $NPM_PACKAGES/bin
 fzf_configure_bindings --variables
