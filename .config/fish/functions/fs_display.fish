6ab2227de450cd70f012cbb3e65d482717520c8f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-16 00:56:02 -0600

    fs_display dnf_installed

---
+++ .config/fish/functions/fs_display.fish
@@ -0,0 +1,11 @@
+# Defined interactively
+function fs_display --argument target
+    for enabled in (kscreen-doctor -o | grep -v disconnected | grep -i enabled | cut -d' ' -f3)
+        kscreen-doctor output.$enabled.disable
+    end
+    kscreen-doctor output.$target.enable output.$target.mode.1920x1080@60
+    krohnkite_off
+    keepscreenon && xset s off
+    pactl set-default-sink alsa_output.pci-0000_01_00.1.hdmi-stereo-extra3
+    ~/.config/autostart.sh &
+end
