b45a455fae8485e6239ed04fa9ae604c348ef167
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-16 16:11:22 -0600

    lt-toggle

---
+++ .config/fish/functions/lt-toggle.fish
@@ -0,0 +1,18 @@
+# Defined interactively
+function lt-toggle
+    if pgrep -f 'lb lt'
+        lb stop
+    else
+        if test $hostname = pakon
+            lt-start
+        else if timeout 0.4 nc -z 192.168.1.114 22 2>/dev/null 
+            if ssh xk@192.168.1.114 pgrep lb >/dev/null
+                ssh xk@192.168.1.114 lt-stop
+            else
+                ssh xk@192.168.1.114 lt-start
+            end
+        else
+            lt
+        end
+    end
+end
