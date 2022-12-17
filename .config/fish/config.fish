4561c84e582dedd9e1be8f44ad338ea5560d4b58
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-17 16:37:03 -0600

    config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,11 @@
-set CLOUDSDK_PYTHON python3.9
 bass source /etc/profile
-#bass 'eval "$(dircolors)"'
+source ~/.config/fish/functions/ls.fish
+
+set CLOUDSDK_PYTHON python3.9
 set NPM_PACKAGES "$HOME/.npm-packages"
 set PATH $PATH $NPM_PACKAGES/bin
 fzf_configure_bindings --variables
+
 bind \b backward-kill-bigword
 bind \cy redo
 
