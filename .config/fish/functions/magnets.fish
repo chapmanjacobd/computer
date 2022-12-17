f0012e33e86b2c4b45cbddc64c5fe70a60a2b20c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-17 00:05:29 -0600

    magnets

---
+++ .config/fish/functions/magnets.fish
@@ -0,0 +1,22 @@
+# Defined interactively
+function magnets
+    rmdir ~/b/Downloads/*** 2> /dev/null
+
+    if test $DISPLAY = ':1001'
+        pgrep tixati >/dev/null
+        set tixati_is_running $status
+        if not test $tixati_is_running -eq 0
+            tixati &
+            sleep 5
+        end
+
+        cat ~/.local/magnets | xargs tixati
+        true >~/.local/magnets
+
+        if not test $tixati_is_running -eq 0
+            sleep 5
+            pkill tixati
+        end
+
+    end
+end
