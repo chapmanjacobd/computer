31cf8c4a167d46a0224dfd26d608f2fd113e2f16
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-18 15:06:09 -0600

    vnc-pakon gitignore

--- .config/fish/functions/vnc-pakon.fish
+++ .config/fish/functions/vnc-pakon.fish
@@ -1,4 +1,6 @@
 # Defined interactively
 function vnc-pakon
-    ssh xk@pakon -f -L 4102:localhost:4000 sleep 20 && /usr/NX/bin/nxplayer --session ~/.ssh/ssh_pakon.nxs
+    ssh xk@pakon -f -L 4102:localhost:4000 sleep 20 &
+    sleep 2
+    /usr/NX/bin/nxplayer --session ~/.ssh/ssh_pakon.nxs
 end
