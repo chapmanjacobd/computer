0893176a56f0d2b82c12a370b924234610c76e55
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-20 12:24:27 -0600

    lt-toggle

--- .config/fish/functions/lt-toggle.fish
+++ .config/fish/functions/lt-toggle.fish
@@ -4,7 +4,7 @@ function lt-toggle
         lb stop
     else
         if test $hostname = pakon
-            lt-start
+            lt
         else if timeout 0.4 nc -z 192.168.1.114 22 2>/dev/null
             if ssh xk@192.168.1.114 pgrep lb >/dev/null
                 ssh xk@192.168.1.114 lt-stop
