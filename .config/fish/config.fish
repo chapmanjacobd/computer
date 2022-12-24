c3a61e5d2a6fb78e794463c2981154916a9a334d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-24 13:03:03 -0600

    config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ set NPM_PACKAGES "$HOME/.npm-packages"
 set PATH $PATH $NPM_PACKAGES/bin
 fzf_configure_bindings --variables
 
+bind \e\[1\;5C forward-bigword
+bind \e\[1\;5D backward-bigword
 bind \b backward-kill-bigword
 bind \cy redo
-
