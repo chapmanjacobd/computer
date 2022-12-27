da8f4e1099e228b27ead861b560f7f44bf312e11
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-27 16:57:47 -0600

    reddit-links-update

--- .config/fish/functions/reddit-links-update.fish
+++
@@ -1,5 +0,0 @@
-function reddit-links-update --argument folder
-    set curatifile "/home/xk/mc/""$folder""-reddit.txt"
-    set subr (cat $curatifile | string join ', ')
-    reddit-links-m "$subr" | grep '^http' >>~/.jobs/todo/$folder
-end
