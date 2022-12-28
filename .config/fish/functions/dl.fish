ec92ffc062cc93d632fe5e721fe2d7224be3c5e4
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-27 22:51:30 -0600

    dl

--- .config/fish/functions/dl.fish
+++ .config/fish/functions/dl.fish
@@ -1,7 +1,7 @@
 # Defined interactively
 function dl
-    lb download ~/lb/audio.db --audio --prefix /mnt/d/81_New_Music/
     lb download ~/lb/yt/podcasts.tl.db --audio --prefix ~/d/82_Audiobooks/
+    lb download ~/lb/audio.db --audio --prefix /mnt/d/81_New_Music/
     lb download ~/lb/reddit/63_Sounds.db --audio --safe --prefix ~/d/63_Sounds/
     lb download ~/lb/reddit/81_New_Music.db --audio --safe --prefix ~/d/81_New_Music/
     lb download ~/lb/yt/classical_music.tl.db --audio --prefix ~/d/83_ClassicalComposers/
