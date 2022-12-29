0f403d74461ae75160dd2b5ff99e985e674f77d1
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-29 17:14:04 -0600

    filterfile

--- .config/fish/functions/filterfile.fish
+++ .config/fish/functions/filterfile.fish
@@ -1,10 +1,5 @@
 # Defined interactively
-function filterfile
-    set word $argv[(count $argv)] # last arg
-    set files $argv[1..-2]
-
-    for file in $files
-        grep -i $word $file
-        grep -iv $word $file | sponge $file
-    end
+function filterfile --argument file word
+    grep -i "$word" "$file"
+    grep -iv "$word" "$file" | sponge "$file"
 end
