797d1b51f19d38fcbff5210bf7f78088ec4a544f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-29 17:09:52 -0600

    env filterfile

--- .config/fish/completions/filterfile.fish
+++ .config/fish/completions/filterfile.fish
@@ -1,1 +1,2 @@
-complete -c filterfile -xa "(cat (commandline -opc)[2])"
+complete -k -c filterfile --require-parameter --no-files -a "(cat (commandline -opc)[2])"
+complete -k -c filterfile --require-parameter
