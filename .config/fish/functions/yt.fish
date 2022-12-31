91712656144a393f31280a74e4c368f905d2118e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-30 19:48:29 -0600

    yt yts

--- .config/fish/functions/yt.fish
+++ .config/fish/functions/yt.fish
@@ -1,4 +1,4 @@
-function yt
+function yt -w yt-dlp
     ytdl -i -f bestvideo[height<=576]+bestaudio/best[height<=576]/best --write-sub --write-auto-sub \
         --sub-lang 'en,EN,en.*,en-*,EN.*,EN-*eng,ENG,english,English,ENGLISH' \
         --embed-subs --compat-options no-keep-subs \
