95d67443dde863f5f23a2ca0190fe94db70bc6f0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-04 03:43:45 -0600

    coolpeople

--- .config/fish/functions/coolpeople.fish
+++ .config/fish/functions/coolpeople.fish
@@ -1,5 +1,5 @@
 # Defined interactively
 function coolpeople
-set file ~/j/public/people.mentors.md
-echo "$argv" | cat - "$file" | sort --unique --ignore-case | sponge "$file"
+    set file ~/j/public/people.mentors.md
+    echo "$argv" | cat - "$file" | sort --unique --ignore-case | sponge "$file"
 end
