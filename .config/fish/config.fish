54cca3a7863d0e1d6a53968ddc4a3816a151f662
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-04 18:41:33 -0600

    config fish_variables kglobalshortcutsrc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
 bass source /etc/profile
 source ~/.config/fish/functions/ls.fish
 
-set CLOUDSDK_PYTHON python3.9
 set NPM_PACKAGES "$HOME/.npm-packages"
 set PATH $PATH $NPM_PACKAGES/bin
 fzf_configure_bindings --variables
