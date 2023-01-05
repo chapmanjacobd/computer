c68d8ef70aac53da9585cb7a96b13dfc6f633fb8
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-05 17:14:34 -0600

    skipthrough input

--- .config/fish/functions/skipthrough.fish
+++ .config/fish/functions/skipthrough.fish
@@ -1,8 +1,8 @@
+# Defined interactively
 function skipthrough
-
-    echo 'seek 20' | socat - ~/../tmp/mpv_socket
+    echo 'seek 20' | socat - $MPV_SOCKET
     sleep 2
-    echo 'seek 20' | socat - ~/../tmp/mpv_socket
+    echo 'seek 20' | socat - $MPV_SOCKET
     sleep 1
-    echo 'seek 50' | socat - ~/../tmp/mpv_socket
+    echo 'seek 50' | socat - $MPV_SOCKET
 end
