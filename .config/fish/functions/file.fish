1df97612161ef2a961ef6e6d0be4e3e5a6f42283
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-06 23:58:02 -0600

    file

--- .config/fish/functions/file.fish
+++ .config/fish/functions/file.fish
@@ -1,8 +1,8 @@
-# Defined interactively
+# Defined via `source`
 function file
-if isatty stdin
-command file $argv
-else
-xargs -I FILE file -b FILE
-end
+    if isatty stdin
+        command file $argv
+    else
+        xargs -I FILE file -b FILE $argv
+    end
 end
