15e76229bc534b3678410a33a58465ea0e745a21
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-06 21:34:01 -0600

    gitls

--- .config/fish/functions/gitls.fish
+++ .config/fish/functions/gitls.fish
@@ -1,7 +1,14 @@
 # Defined via `source`
 function gitls
-    for f in *
+    set -l files
+    for f in $argv
+        set -a files $f/*
+    end
+    if not string length --quiet $files
+        set files *
+    end
+
+    for f in $files
         git log -1 --date=iso-strict-local --format="%ad %>(12) %cr %<(10) %an %x09 %h  $f" -- "$f"
     end | sort --general-numeric-sort
-
 end
