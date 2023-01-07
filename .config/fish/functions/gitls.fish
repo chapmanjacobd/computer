54bc9e242bf2be61c013df597977f48e7b191fd4
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-06 23:35:54 -0600

    gitls

--- .config/fish/functions/gitls.fish
+++ .config/fish/functions/gitls.fish
@@ -1,6 +1,4 @@
-# Defined via `source`
+# Defined in /home/xk/.config/fish/functions/gitls.fish @ line 1
 function gitls
-    for f in (git ls-files --sparse --full-name)
-        git log -1 --date=iso-strict-local --format="%ad %>(12) %cr %<(10) %an %x09 %h  $f" -- "$f"
-    end | sort --general-numeric-sort
+    git ls-files --sparse --full-name -z | xargs -0 -I FILE -P 20 git log -1 --date=iso-strict-local --format='%ad %>(14) %cr %<(5) %an  %h ./FILE' -- FILE | sort --general-numeric-sort
 end
