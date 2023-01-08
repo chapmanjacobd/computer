d495586dc72d909823aadd46949bb1a5d29f3eb4
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-07 22:43:54 -0600

    gitls-commits

--- .config/fish/functions/gitls-commits.fish
+++ .config/fish/functions/gitls-commits.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function gitls-commits
-    git ls-files --sparse --full-name -z | xargs -0 -I FILE echo "printf '%s\tFILE\n' (git rev-list HEAD --count -- FILE)" | parallel -j40 | sort --general-numeric-sort
+    git log --pretty=format: --name-only | sed '/^$/d' | asc
 end
