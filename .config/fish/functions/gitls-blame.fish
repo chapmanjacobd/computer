bc3bdd2452a7e2841dba1750815455f59bf9b2a4
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-08 23:36:52 -0600

    gitls-blame

--- .config/fish/functions/gitls-blame.fish
+++ .config/fish/functions/gitls-blame.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function gitls-blame
-    for f in (git grep --full-name --cached -Il '')
+    for f in (git grep --full-name --cached -Il '')  # skip binary files
         printf "%s\t%s\n" "$f" (gitblame-percent "$f" | string join ', ')
     end | column -t
 
