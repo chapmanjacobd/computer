4e6dd3a5e0aa752f37e3d11ca537a39ab0813179
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-08 23:29:49 -0600

    gitls-blame

--- .config/fish/functions/gitls-blame.fish
+++ .config/fish/functions/gitls-blame.fish
@@ -1,5 +1,5 @@
 # Defined interactively
-function gitls-authors
+function gitls-blame
     for f in (git grep --full-name --cached -Il '')
         printf "%s\t%s\n" "$f" (gitblame-percent "$f" | string join ', ')
     end | column -t
