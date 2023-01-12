6394bc9640702d49a1559499c716e0ec3f1bf3bb
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-12 01:44:29 -0600

    playSomethingReturnFiles

--- .config/fish/functions/playSomethingReturnFiles.fish
+++
@@ -1,5 +0,0 @@
-# Defined in - @ line 2
-function playSomethingReturnFiles --argument genre manyMult
-    set many (default $manyMult 1)
-    filesDeep ~/Music/ | fileTypeAudio | shuf -n (math "100*$many") | filterByGenre $genre
-end
