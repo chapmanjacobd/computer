79bf85a9475b0a7763d63cd9a723195a2fbcd9fa
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-14 15:01:57 -0600

    vpn

--- .config/fish/functions/vpn.fish
+++ .config/fish/functions/vpn.fish
@@ -2,4 +2,5 @@ function vpn
     sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
     # fish -c 'sudo openvpn --mute-replay-warnings --script-security 2 --config /home/xk/d/30_Computing/oci.ovpn' & disown
     sshuttle --dns -r oci 0/0 ::/0
+    or pkill tixati
 end
