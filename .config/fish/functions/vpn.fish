af50d8cca8bacd30bb6408e5e3a62fca19f8c5b1
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-14 16:37:16 -0600

    vpn

--- .config/fish/functions/vpn.fish
+++ .config/fish/functions/vpn.fish
@@ -1,11 +1,13 @@
 function vpn
     sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
 
-    sudo ip route add local default dev lo table 100
-    sudo ip rule add fwmark 0x01 lookup 100
-    # sudo ip -6 route add local default dev lo table 100
-    # sudo ip -6 rule add fwmark 0x01 lookup 100
+    # UDP
+    #sudo ip route add local default dev lo table 100
+    #sudo ip rule add fwmark 0x01 lookup 100
+    ## sudo ip -6 route add local default dev lo table 100
+    ## sudo ip -6 rule add fwmark 0x01 lookup 100
+    #sudo sshuttle --dns -r oci --ssh-cmd 'ssh -F /home/xk/.ssh/config' -x 144.24.93.111 -x 192.168.0.0/16 --method tproxy --disable-ipv6 0/0 # ::/0
 
-    sudo sshuttle --dns -r oci --ssh-cmd 'ssh -F /home/xk/.ssh/config' -x 144.24.93.111 -x 192.168.0.0/16 --method tproxy --disable-ipv6 0/0 # ::/0
+    sshuttle --dns -r oci -x 144.24.93.111 -x 192.168.0.0/16 0/0 --disable-ipv6 --no-latency-control
     or pkill tixati
 end
