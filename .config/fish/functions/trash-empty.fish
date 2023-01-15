b47c48e26c3b690ce44f46bf91979d2fcd69a693
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-14 20:36:33 -0600

    trash-empty

--- .config/fish/functions/trash-empty.fish
+++ .config/fish/functions/trash-empty.fish
@@ -3,4 +3,5 @@ function trash-empty
     trash-list | tee -a ~/.local/share/trashed.txt
     command trash-empty $argv
     sort --unique --ignore-case ~/.local/share/trashed.txt | sponge ~/.local/share/trashed.txt
+    btrfs_check_delete_snapshot ~/d/
 end
