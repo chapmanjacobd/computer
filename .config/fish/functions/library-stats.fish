d45a83e780e3f22d16f314c8edf3bdb21d0e5873
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-17 01:13:46 -0600

    library-stats gitignore

---
+++ .config/fish/functions/library-stats.fish
@@ -0,0 +1,24 @@
+# Defined in /home/xk/.config/fish/functions/library-stats.fish @ line 2
+function library-stats
+    for db in ~/lb/61_Photos_Unsorted.db ~/lb/fs/tax.db ~/lb/video.db
+        echo $db
+        lb-dev wt $db -p a
+        echo $db deleted
+        lb-dev wt $db -w is_deleted=1 -p a
+    end
+
+    for db in ~/lb/63_Sounds.db ~/lb/audio.db
+        echo $db
+        lb-dev lt $db -p a
+        echo $db deleted
+        lb-dev lt $db -w time_deleted'>'0 -p a
+        echo $db played
+        lb-dev lt $db -w play_count'>'0 -p a
+    end
+
+    for db in ~/lb/61_Photos_Unsorted.db ~/lb/fs/tax.db ~/lb/video.db ~/lb/63_Sounds.db ~/lb/audio.db
+        echo $db
+        lb-dev bigdirs $db -L 5
+    end
+
+end
