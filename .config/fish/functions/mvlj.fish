4cce111d79bf091b16969dc14bd782248214e894
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-19 01:10:36 -0600

    mvlj mvljs

--- .config/fish/functions/mvlj.fish
+++
@@ -1,6 +0,0 @@
-# Defined interactively
-function mvlj
-    for dfolder in $argv
-        mvl ~/.jobs/todo/$dfolder ~/.jobs/done/$dfolder
-    end
-end
--- .config/fish/functions/mvljs.fish
+++
@@ -1,7 +0,0 @@
-# Defined interactively
-function mvljs --argument s
-    for folder in ~/.jobs/todo/*
-        set dfolder (basename "$folder")
-        mvl -s"$s" ~/.jobs/todo/$dfolder ~/.jobs/done/$dfolder
-    end
-end
