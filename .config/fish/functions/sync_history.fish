5ffbd32ca09a6fdd31d8bb176442d51d0ed8c4ec
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-19 17:45:29 -0600

    sync_history

--- .config/fish/functions/sync_history.fish
+++ .config/fish/functions/sync_history.fish
@@ -2,4 +2,7 @@
 function sync_history
     cat ~/.local/share/fish/fish_history.sync-conflict-* >>~/.local/share/fish/fish_history
     trash-put ~/.local/share/fish/fish_history.sync-conflict-*
+
+    cat ~/.local/share/z/data.sync-conflict* >>~/.local/share/z/data
+    trash-put ~/.local/share/z/data.sync-conflict*
 end
