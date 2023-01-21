5b733a1bd6888b4a8bc3a2ae4885fe665f6e2754
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-20 21:23:00 -0600

    nmove daily dl_monthly dl_weekly monthly weekly

---
+++ .config/fish/functions/nmove.fish
@@ -0,0 +1,11 @@
+# Defined interactively
+function nmove
+    set old $argv[1]
+    set new $argv[2]
+    set paths $argv[3..-1]
+
+    for f in $paths
+        sed "s|\b$old\b|$new|" -i "$f"
+        mv $f (echo $f | sed "s|\b$old\b|$new|")
+    end
+end
