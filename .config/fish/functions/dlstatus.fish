499f16208b49da57c1c3b5e9e34edb8461ea1a71
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-21 22:45:52 -0600

    dlstatus

--- .config/fish/functions/dlstatus.fish
+++ .config/fish/functions/dlstatus.fish
@@ -1,7 +1,24 @@
-# Defined interactively
 function dlstatus
-    for db in ~/lb/audio.db ~/lb/yt/podcasts.tl.db ~/lb/reddit/63_Sounds.db ~/lb/hackernews/hackernews_only_direct.tw.db ~/lb/reddit/81_New_Music.db ~/lb/yt/classical_music.tl.db ~/lb/reddit/83_ClassicalComposers.db ~/lb/video.db ~/lb/reddit/71_Mealtime_Videos.db
-        echo $db
-        lb ds $db
-    end
+    echo audio.db
+    lb-dev ds ~/lb/audio.db
+    echo yt/podcasts.tl.db
+    lb-dev ds ~/lb/yt/podcasts.tl.db
+    echo yt/classical_music.tl.db
+    lb-dev ds ~/lb/yt/classical_music.tl.db
+
+    echo reddit/63_Sounds.db
+    lb-dev ds ~/lb/reddit/63_Sounds.db --safe
+    echo reddit/81_New_Music.db
+    lb-dev ds ~/lb/reddit/81_New_Music.db --safe
+    echo reddit/83_ClassicalComposers.db
+    lb-dev ds ~/lb/reddit/83_ClassicalComposers.db --safe
+
+    echo video.db
+    lb-dev ds ~/lb/video.db
+    echo reddit/71_Mealtime_Videos.db
+    lb-dev ds ~/lb/reddit/71_Mealtime_Videos.db --safe
+    echo hackernews/hackernews_only_direct.tw.db
+    lb-dev ds ~/lb/hackernews/hackernews_only_direct.tw.db --safe
+    echo fs/tax.db
+    lb-dev ds ~/lb/fs/tax.db --safe
 end
