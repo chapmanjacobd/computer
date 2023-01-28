46ffb3325dfb05b75ccbe39ddcdb1ed90e9d5185
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-28 09:56:58 -0600

    settings trash-empty

--- .config/fish/functions/trash-empty.fish
+++ .config/fish/functions/trash-empty.fish
@@ -4,7 +4,7 @@ function trash-empty
     command trash-empty $argv
     sort --unique --ignore-case ~/.local/share/trashed.txt | sponge ~/.local/share/trashed.txt
     if gum confirm 'Refresh snapshots?'
-        for mnt in /mnt/d1 /mnt/d2
+        for mnt in /mnt/d1 /mnt/d2 /home
             btrfs_check_delete_snapshot $mnt
         end
     end
