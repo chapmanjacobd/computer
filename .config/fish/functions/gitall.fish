524dfad860a32976329be0aaa09f40531d8bf5ba
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-28 19:49:02 -0600

    gitall

--- .config/fish/functions/gitall.fish
+++ .config/fish/functions/gitall.fish
@@ -1,6 +1,7 @@
 # Defined interactively
 function gitall
     for g in /home/xk/ /home/xk/j/ /home/xk/mc/ /home/xk/lb/
+        echo $g
         git --git-dir=$g/.git/ --work-tree=$g $argv
     end
 end
