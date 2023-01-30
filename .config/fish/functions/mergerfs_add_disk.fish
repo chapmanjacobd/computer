d2595c1b83e845fb1e1225fc6edeb90646ec365a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-29 23:02:46 -0600

    mergerfs_add_disk fstab

--- .config/fish/functions/mergerfs_add_disk.fish
+++ .config/fish/functions/mergerfs_add_disk.fish
@@ -1,12 +1,12 @@
 # Defined interactively
 function mergerfs_add_disk --argument dev label
     sudo mkfs.btrfs -R free-space-tree -m dup -d single $dev -L $label # ie. d1
-    echo run fstab to add the disk. copy the UUID from above
-    kitty fish -c fstab
     sudo btrfs inspect-internal dump-super -f $dev | sudo tee /mnt/superblocks/$label
+    kitty fish -c fstab
 
     sudo mkdir /mnt/$label
     sudo chown xk:xk /mnt/$label
+    sudo systemctl daemon-reload
     mount /mnt/$label
     sudo mergerfs.ctl -m /mnt/d add path /mnt/$label
     sudo mergerfs.ctl -m /mnt/d info
