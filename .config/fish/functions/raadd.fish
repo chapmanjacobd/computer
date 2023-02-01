aaf2a9bb5168227523643a931a1fb19a15748693
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-01-31 20:55:47 -0600

    raadd radd

--- .config/fish/functions/raadd.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function raadd --argument dfolder
-    lb-dev redditadd --audio -c $dfolder --subreddits --lookback 900 -v reddit/$dfolder.db (cat ~/mc/$dfolder-reddit.txt)
-end
--- .config/fish/functions/radd.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function radd --argument dfolder
-    lb-dev redditadd -c $dfolder --subreddits --lookback 900 -v reddit/$dfolder.db (cat ~/mc/$dfolder-reddit.txt)
-end
