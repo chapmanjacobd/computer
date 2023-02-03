079a7225e86014d92756ecebf4a9a577f9d12c57
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-03 00:00:42 -0600

    trash-empty

--- .config/fish/functions/trash-empty.fish
+++ .config/fish/functions/trash-empty.fish
@@ -2,7 +2,7 @@
 function trash-empty
     if gum confirm --default=no 'Refresh snapshots?'
         for mnt in /mnt/d1 /mnt/d2 /mnt/d3 /mnt/d4 /mnt/d5 /mnt/d6 /mnt/d7 /home
-            btrfs_check_delete_snapshot $mnt
+            kitty fish -c "btrfs_check_delete_snapshot $mnt" &
         end
     end
 
