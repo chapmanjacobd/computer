056996b44d07d66e41d112ca60b5f304f3132d23
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-07 18:34:21 -0600

    last_watched

--- .config/fish/functions/last_watched.fish
+++ .config/fish/functions/last_watched.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function last_watched
-    lb wt video.db -p v --cols path,duration,time_partial_first,time_partial_last
+    lb wt ~/lb/video.db -p v --cols path,duration,time_partial_first,time_partial_last -w time_deleted'>'-1 -l 10 $argv
 end
