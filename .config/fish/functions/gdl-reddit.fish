01d9b71ab686ade4daa299c8de809c04162ea04b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-10 00:31:51 -0600

    gdl-reddit

--- .config/fish/functions/gdl-reddit.fish
+++
@@ -1,7 +0,0 @@
-# Defined interactively
-function gdl-reddit
-    cd ~/d/61_Photos_Unsorted/
-    python -m bdfr download bdfr/ -S top --subreddit $argv
-    python -m bdfr download bdfr/ -S top -t month -L 400 --subreddit $argv
-    python -m bdfr download bdfr/ -S top -t year -L 400 --subreddit $argv
-end
