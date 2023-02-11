b4c47616ea90a68de9defd346521745d423c3ad5
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-11 09:03:14 -0600

    kill-on-port gitignore

---
+++ .config/fish/functions/kill-on-port.fish
@@ -0,0 +1,20 @@
+function kill-on-port --description 'kills the process using the provided port'
+    set -l pid (lsof -t -i:$argv[1])
+    if count $pid >/dev/null
+        set -l cmd (ps -p $pid -o command | tail -n 1)
+
+        set_color -o
+        echo "Pew! Pew!"
+        set_color normal
+
+        kill $pid
+
+        printf "We killed "
+        set_color red
+        printf $cmd
+        set_color normal
+        printf "!\n"
+    else
+        echo "It was not very effective"
+    end
+end
