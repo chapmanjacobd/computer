6038934fbd555d47c729e70ef46a1c8b5962e411
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-12 00:08:02 -0600

    redditadd

--- .config/fish/functions/redditadd.fish
+++ .config/fish/functions/redditadd.fish
@@ -1,16 +1,18 @@
 # Defined interactively
 function redditadd --argument reddit_db
-    set subreddits (echo $argv[2..-1] | string lower)
-    lb redditadd --subreddits -v $reddit_db $subreddits
-
     ~/github/xk/reddit_mining/links/
-    for subreddit in $subreddits
+    for subreddit in $argv[2..-1]
+        set subreddit (echo $subreddit | string lower)
+        lb redditadd --subreddits -v $reddit_db $subreddit
+    end
+
+    for subreddit in $argv[2..-1]
         if not test -e "$subreddit.csv"
             echo "octosql -o csv \"select path,score,'https://old.reddit.com/r/$subreddit/' as playlist_path from `../reddit_links.parquet` where lower(playlist_path) = '$subreddit' order by score desc \" > $subreddit.csv"
         end
     end | parallel -j8
 
-    for subreddit in $subreddits
+    for subreddit in $argv[2..-1]
         sqlite-utils upsert --pk path --alter --csv --detect-types $reddit_db media $subreddit.csv
     end
 
