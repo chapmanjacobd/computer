084c9c62829e23376baa9ae9015b849dde2e3927
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-15 13:42:14 -0600

    wip

--- .config/fish/functions/nextSongDelete.fish
+++ .config/fish/functions/nextSongDelete.fish
@@ -2,6 +2,8 @@
 function nextSongDelete
     if pgrep -f 'lb listen'
         lb next --delete
+    else if pgrep -f 'mpv .*/6'
+        lb-dev next --delete
     else
         ssh pakon lb next --delete
     end
