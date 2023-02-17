7199a129c7c1661097820fe3fb961ea414b1dc66
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-17 16:49:49 -0600

    wt

--- .config/fish/functions/wt.fish
+++ .config/fish/functions/wt.fish
@@ -1,4 +1,3 @@
 function wt
-    ~/lb/
-    /home/xk/.local/bin/lb wt -u 'ntile(10000) over (order by size / duration) desc' -k delete video.db $argv
+    /home/xk/.local/bin/lb wt -u 'ntile(10000) over (order by size / duration) desc' -k delete ~/lb/video.db $argv
 end
