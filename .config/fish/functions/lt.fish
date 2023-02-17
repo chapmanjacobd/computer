1fa528df07b6968269c9c0320d7cdc0003a82656
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-17 12:06:07 -0600

    lt

--- .config/fish/functions/lt.fish
+++ .config/fish/functions/lt.fish
@@ -1,4 +1,3 @@
 function lt
-    ~/lb
-    /home/xk/.local/bin/lb listen -w 'play_count=0' -u random -O -k delete-if-audiobook audio.db $argv
+    /home/xk/.local/bin/lb listen -w 'play_count=0' -u random -O -k delete-if-audiobook ~/lb/audio.db $argv
 end
