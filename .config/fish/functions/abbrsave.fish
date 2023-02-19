e2b40c3f174c4d0c76663a21d32d579b39ab8d82
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-19 10:16:56 -0600

    abbreviations abbrsave cr

--- .config/fish/functions/abbrsave.fish
+++ .config/fish/functions/abbrsave.fish
@@ -1,7 +1,7 @@
 # Defined via `source`
 function abbrsave
-    abbr -a -U -- $argv[1] (string escape -- $argv[2..-1])
+    abbr -a -U -- $argv[1] $argv[2..-1]
 
-    filterfile ~/.config/fish/abbreviations "abbr -a -U -- $argv[1]"
-    echo abbr -a -U -- $argv[1] (string escape -- $argv[2..-1]) >>~/.config/fish/abbreviations
+    filterfile ~/.config/fish/abbreviations "abbr -a -U -- $argv[1] "
+    echo abbr -a -U -- $argv[1] $argv[2..-1] >>~/.config/fish/abbreviations
 end
---
+++ .config/fish/functions/cr.fish
@@ -0,0 +1,6 @@
+# Defined interactively
+function cr
+    pkill catt
+    catt volume 0 && catt volume 55
+    catt stop
+end
