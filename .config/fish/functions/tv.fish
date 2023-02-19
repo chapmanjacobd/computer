e71cd2e42ddecceed84d41e48e16cd35ce47b51b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-19 02:08:30 -0600

    sitdown standup tv

--- .config/fish/functions/tv.fish
+++ .config/fish/functions/tv.fish
@@ -1,11 +1,11 @@
 # Defined interactively
 function tv
     if grep -qEi "(DisplayPort-0)" (kscreen-doctor -o | psub)
-        kscreen-doctor output.DisplayPort-0.disable output.DVI-D-0.disable
         kscreen-doctor output.HDMI-A-0.enable output.HDMI-A-0.mode.1920x1080@60
+        kscreen-doctor output.DisplayPort-0.disable output.DVI-D-0.disable
     else
-        kscreen-doctor output.DP-1.disable output.DVI-D-0.disable
         kscreen-doctor output.HDMI-1.enable output.HDMI-1.mode.1920x1080@60
+        kscreen-doctor output.DP-1.disable output.DVI-D-0.disable
     end
     krohnkite_off
     #bash -c 'kquitapp5 plasmashell || killall plasmashell; kstart5 plasmashell'
