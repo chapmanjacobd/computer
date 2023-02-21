9044e08d7f556bbc9f0b6d248e5bd0e60d66c882
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-21 10:08:58 -0600

    getsubtitles gitignore

---
+++ .config/fish/functions/getsubtitles.fish
@@ -0,0 +1,11 @@
+# Defined interactively
+function getsubtitles
+    for video in (fd . | fileTypeVideos )
+        ffmpeg -i $video -c copy -map 0:s:0 -frames:s 1 -f null - -v 0 -hide_banner
+        if test $status = 0
+            echo yay
+        else
+            subliminal --opensubtitles $OPEN_SUBTITLE_CREDENTIALS download -l en "$video"
+        end
+    end
+end
