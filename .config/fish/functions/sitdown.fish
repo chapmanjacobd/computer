3584d11e339974580e403f368559d17aa75af4df
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-22 12:06:13 -0600

    sitdown

--- .config/fish/functions/sitdown.fish
+++ .config/fish/functions/sitdown.fish
@@ -10,8 +10,8 @@ function sitdown
         kscreen-doctor output.DP-1.enable
         kscreen-doctor output.HDMI-1.disable output.DVI-D-0.disable
     end
+    #bash -c 'kquitapp5 plasmashell || killall plasmashell; kstart5 plasmashell'
     krohnkite_on
-    bash -c 'kquitapp5 plasmashell || killall plasmashell; kstart5 plasmashell'
     kwin_x11 --replace & disown
     pactl set-default-sink alsa_output.pci-0000_00_1f.3.pro-output-0
     ~/.config/autostart.sh &
