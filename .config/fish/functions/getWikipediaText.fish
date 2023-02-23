417c2781f1516b6f87c4f9ea99bb3babb37545e2
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-23 02:13:47 -0600

    getWikipediaText

--- .config/fish/functions/getWikipediaText.fish
+++
@@ -1,3 +0,0 @@
-function getWikipediaText
-    curl "https://en.wikipedia.org/w/api.php?action=parse&prop=text&format=json&page=$argv" | jq '.parse.text[]' | html2text --ignore-links --images-to-alt --ignore-emphasis --no-automatic-links
-end
