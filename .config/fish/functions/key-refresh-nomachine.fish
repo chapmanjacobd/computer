cbae3c04856a47be71a8e226d1dfcb9831909959
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-26 13:43:40 -0600

    key-refresh-nomachine authorized

--- .config/fish/functions/key-refresh-nomachine.fish
+++ .config/fish/functions/key-refresh-nomachine.fish
@@ -4,7 +4,7 @@ function key-refresh-nomachine
     mv ~/.ssh/id_rsa_nx $oldkey
     mv ~/.ssh/id_rsa_nx.pub $oldkey.pub
 
-    ssh-keygen -t rsa -C nomachine -q -N '' </dev/zero || true
+    ssh-keygen -t rsa -C nomachine -q -N '' -f ~/.ssh/id_rsa_nx </dev/zero || true
     cat ~/.ssh/id_rsa_nx.pub >> ~/.nx/config/authorized.crt
     filterfile ~/.nx/config/authorized.crt (cat $oldkey.pub)
 end
