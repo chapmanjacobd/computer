4b0994be65040c857d9b282b9c08d85a8cbe54f5
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-27 01:46:11 -0600

    gcp-resize gitignore

---
+++ .config/fish/functions/gcp-resize.fish
@@ -0,0 +1,7 @@
+# Defined interactively
+function gcp-resize
+    gcloud compute disks resize --size 2500 --zone us-west1-b --quiet $argv
+    echo growpart /dev/sda 1
+    echo sudo resize2fs /dev/sda1
+    echo df -h
+end
