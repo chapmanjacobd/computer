2aa27fb797fb787aa195f1292ac9d050d8c30f83
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-03-04 16:54:53 -0600

    organize_audio_in_video

--- .config/fish/functions/organize_audio_in_video.fish
+++ .config/fish/functions/organize_audio_in_video.fish
@@ -1,17 +1,5 @@
 # Defined interactively
-function organize_audio_in_video
-    has_video "$argv"
-    set video $status
-
-    has_audio "$argv"
-    set audio $status
-
-    if test $video -eq 0 -a $audio -eq 0
-        return 0
-    else if test $video -eq 0
-        mv "$argv" ~/d/61_Photos_Unsorted/gifs/from_video/
-        return 0
-    else if test $audio -eq 0
-        mv "$argv" ~/d/82_Audiobooks/unsorted/from_video/
-    end
+function organize_audio_in_video -a db
+    wt --db $db -w 'video_count=0 and audio_count>=1' -pf | parallel mv {} ~/d/82_Audiobooks/unsorted/from_video/
+    wt --db $db -w 'video_count>=1 and audio_count=0' -pf | parallel mv {} ~/d/61_Photos_Unsorted/gifs/from_video/
 end
