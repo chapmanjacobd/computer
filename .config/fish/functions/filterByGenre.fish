05f555482bb8261b15e086674f49eb75ee3ce5dd
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-03-06 20:54:43 -0600

    filterByGenre gitignore

---
+++ .config/fish/functions/filterByGenre.fish
@@ -0,0 +1,9 @@
+# Defined in - @ line 1
+function filterByGenre
+    while read file
+        if ffprobe "$file" 2>&1 | sed -E -n 's/^ *GENRE *: (.*)/\1/p' | grep -q "$argv"
+            echo "$file"
+        else
+        end
+    end
+end
