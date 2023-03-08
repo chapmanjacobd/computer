b364894a8968a7e241cfec91b58fd848d92555d8
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-03-08 01:35:49 -0600

    dpCML

--- .config/fish/functions/dpCML.fish
+++
@@ -1,9 +0,0 @@
-# Defined interactively
-function dpCML --argument file
-    set DATETIME (string split '/' $file -f 2 | string replace 'linkmap_' '' | string replace '.dat' '')
-    set DATE (string match -r '\d{8}' $DATETIME)
-    set TIME (string sub --start 9 --length 5 $DATETIME)
-    head -1 $file | sed 's|\(^.*$\)|date,time,\1|'
-    tail -n +2 $file | grep -v '# Ordinary kriging interpolation. Predefined sill, range, and nugget.' \
-        | sed "s|\(^.*\$\)|$DATE,$TIME,\1|"
-end
