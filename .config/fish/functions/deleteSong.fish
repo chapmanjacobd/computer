2ea95be082d92c647457b42238d39818b48b9996
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-03-11 00:42:31 -0600

    deleteSong

--- .config/fish/functions/deleteSong.fish
+++
@@ -1,6 +0,0 @@
-function deleteSong
-    set song (echo '{ "command": ["get_property", "path"] }' | socat - /tmp/mpvsocket | jq .data | sed -e 's/^"//' -e 's/"$//')
-
-    echo playlist_next | socat - /tmp/mpvsocket
-    trash $song
-end
