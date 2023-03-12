866023b5e841eb31a733bb243d7e46edce5ced0d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-03-12 12:25:21 -0500

    dorganize

--- .config/fish/functions/dorganize.fish
+++ .config/fish/functions/dorganize.fish
@@ -1,10 +1,14 @@
 # Defined via `source`
 function dorganize
-    ~/d/03_Downloads/ && fd -eZIP -eRAR -e7z -x bash -c 'unar "{}" && rm "{}"'
+    ~/d/03_Downloads/
 
-    ~/d/03_Downloads/ && fd -d1 -eEPUB -x mv "{}" ~/d/50_eBooks/
-    ~/d/03_Downloads/ && fd -d1 -eHTML -x mv "{}" ~/d/53_Scrapbook_Web/
+    fd -eZIP -eRAR -e7z -x bash -c 'unar "{}" && rm "{}"'
 
-    ~/d/03_Downloads/ && fd -d1 -eJPEG -x mv "{}" {.}.jpg
-    ~/d/03_Downloads/ && fd -d1 -eJPG -ePNG -eWEBP -x mv "{}" ~/d/61_Photos_Unsorted/
+    fd -d1 -eEPUB -x mv "{}" ~/d/50_eBooks/
+    fd -d1 -eHTML -ePDF -x mv "{}" ~/d/53_Scrapbook_Web/
+
+    fd -d1 -eJPEG -x mv "{}" {.}.jpg
+    fd -d1 -eJPG -ePNG -eWEBP -x mv "{}" ~/d/61_Photos_Unsorted/
+
+	remove_empty_directories
 end
