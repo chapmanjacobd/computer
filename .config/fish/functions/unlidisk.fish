1c0c9ec799ba76517eafe4d208170f19e2cdb5cc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-03-11 20:35:35 -0600

    unlidisk

--- .config/fish/functions/unlidisk.fish
+++ .config/fish/functions/unlidisk.fish
@@ -1,6 +1,7 @@
 # Defined in - @ line 2
 function unlidisk
-    sshfs -o reconnect,IdentityFile=/home/xk/.ssh/admin_rsa admin@unli.xyz:/ ~/.mnt/web/
+    sshfs -o reconnect,IdentityFile=/home/xk/.ssh/old/admin_rsa admin@unli.xyz:/ ~/.mnt/web/
+    sleep 2
     code ~/.mnt/web/home/production/admin/public_html/
     echo fusermount -zu ~/.mnt/web/
 end
