3e993061dc963daa21e455c9bd80508425b3583e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-03-13 00:00:15 -0500

    mrmusic

--- .config/fish/functions/mrmusic.fish
+++ .config/fish/functions/mrmusic.fish
@@ -2,7 +2,7 @@ function mrmusic
     rsync -a --remove-source-files --backup-dir (date "+%d.%m.%Y") --files-from=(lt ~/lb/audio.db -s /mnt/d/80_Now_Listening/ -p f --moved /mnt/d/80_Now_Listening/ /mnt/d/ | psub) /mnt/d/80_Now_Listening/ /mnt/d/
 
     rsync -a --remove-source-files --backup-dir (date "+%d.%m.%Y") --files-from=(
-        lt ~/lb/audio.db -w 'play_count=0' -u random -L 300 -p f --moved /mnt/d/ /mnt/d/80_Now_Listening/ | psub
+        lt ~/lb/audio.db -w 'play_count=0' -u random -L 400 -p f --moved /mnt/d/ /mnt/d/80_Now_Listening/ | psub
     ) /mnt/d/ /mnt/d/80_Now_Listening/
 
     lb fsadd --audio ~/lb/audio.db /mnt/d/80_Now_Listening/
