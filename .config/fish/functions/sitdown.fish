cf18215052fb06ff0494fe966f1bbcae038df1de
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-03-17 19:19:39 -0500

    sitdown

--- .config/fish/functions/sitdown.fish
+++ .config/fish/functions/sitdown.fish
@@ -11,7 +11,7 @@ function sitdown
         kscreen-doctor output.HDMI-1.disable output.DVI-D-0.disable
     end
     #bash -c 'kquitapp5 plasmashell || killall plasmashell; kstart5 plasmashell'
-    krohnkite_on
+    #krohnkite_on
     kwin_x11 --replace & disown
     pactl set-default-sink alsa_output.usb-Apple__Inc._USB-C_to_3.5mm_Headphone_Jack_Adapter_DWH152405TPJKLTA5-00.analog-stereo
     ~/.config/autostart.sh &
