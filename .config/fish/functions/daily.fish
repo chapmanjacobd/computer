9aa016b11479ec60d556e19a99a551e5b5ef49fd
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-03-19 19:31:01 -0500

    daily

--- .config/fish/functions/daily.fish
+++ .config/fish/functions/daily.fish
@@ -7,7 +7,7 @@ function daily
 
     catt volume 0 && catt volume 40
     pip install --upgrade gallery-dl pychromecast
-    for dfolder in 61_Photos_Unsorted 91_New_Art
+    for dfolder in 61_Photos_Unsorted 91_New_Art 95_Memes
         ~/d/$dfolder/
         gallery-dl --input-file (sed 's|^|https://www.instagram.com/\0|' ~/mc/$dfolder-instagram.txt | shuf | head -20 | psub)
     end
