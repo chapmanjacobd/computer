a5108021ac1f2e320c9e01d26d086f3d513bfa69
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-03-27 11:09:40 +0800

    key-refresh-ssh

--- .config/fish/functions/key-refresh-ssh.fish
+++ .config/fish/functions/key-refresh-ssh.fish
@@ -1,10 +1,10 @@
 # Defined via `source`
-function key-refresh-ssh --argument label
+function key-refresh-ssh
     set oldkey (mktemp --tmpdir=(path resolve ~/.ssh/old/) id_ed25519_XXXXX)
     mv ~/.ssh/id_ed25519 $oldkey
     mv ~/.ssh/id_ed25519.pub $oldkey.pub
 
-    ssh-keygen -t ed25519 -C $label -q -N '' </dev/zero || true
+    ssh-keygen -t ed25519 -C (hostname) -q -N '' </dev/zero || true
     cat ~/.ssh/id_ed25519.pub >>~/.ssh/authorized_keys
     filterfile ~/.ssh/authorized_keys (cat $oldkey.pub)
     cat ~/.ssh/id_ed25519.pub
