3c1983b9bdc09efddb84bd1b604c95d2bb38f6f3
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-03-30 04:06:36 -0500

    bigdpi gitignore

---
+++ .config/fish/functions/bigdpi.fish
@@ -0,0 +1,10 @@
+function bigdpi
+    xrandr --dpi 192
+    echo 'Xft.dpi: 192' | xrdb -override
+    sed -i 's/ScaleFactor=1/ScaleFactor=2' ~/.config/kdeglobals
+    set fopt layout.css.devPixelsPerPx
+    set fset 1.7
+    cd ~/.mozilla/firefox/8eqln2tw.coalesce/
+    sed -i 's/user_pref("'$fopt'",.*);/user_pref("'$fopt'","'$fset'");/' user.js
+    grep -q $fopt user.js || echo "user_pref(\"$fopt\",$fset);" >>user.js
+end
