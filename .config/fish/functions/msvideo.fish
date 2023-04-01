d2cc6658b999ee6e053aee023ef341c85968d4ad
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-01 16:02:08 -0500

    msvideo

--- .config/fish/functions/msvideo.fish
+++ .config/fish/functions/msvideo.fish
@@ -1,4 +1,4 @@
 # Defined via `source`
 function msvideo
-    rsync -a --remove-source-files --files-from=(                                   wt ~/lb/video.db -L 10 $argv -p f --moved /mnt/d/ /mnt/d/70_Now_Watching/ | psub                                                                       ) /mnt/d/ /mnt/d/70_Now_Watching/
+    rsync -a --remove-source-files --files-from=(                                   lb wt -u 'ntile(10000) over (order by size / duration) desc' -L 10 ~/lb/video.db $argv -p f --moved /mnt/d/ /mnt/d/70_Now_Watching/ | psub                                                                       ) /mnt/d/ /mnt/d/70_Now_Watching/
 end
