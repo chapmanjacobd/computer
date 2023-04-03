f28fd8d266b4383bb8ecf55366856e7585b8eab5
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-03 09:48:17 +0800

    katevirc autosub dnf_installed

--- .config/mpv/scripts/autosub.lua
+++ .config/mpv/scripts/autosub.lua
@@ -1,6 +1,3 @@
--- default keybinding: b
--- add the following to your input.conf to change the default keybinding:
--- keyname script_binding auto_load_subs
 local utils = require 'mp.utils'
 
 function display_error()
@@ -8,26 +5,38 @@ function display_error()
   mp.osd_message("Subtitle download failed")
 end
 
-function load_sub_fn()
-  path = mp.get_property("path")
-  print(path)
-  path = path:gsub("[%-_ ]*%[[%w%-_][%w%-_][%w%-_][%w%-_][%w%-_][%w%-_][%w%-_][%w%-_][%w%-_][%w%-_][%w%-_]%](%.%w%w%w%w?)$", '%1')
-  print(path)
-  t = { args = { "subliminal", "--opensubtitles", "xk3", "idiZQc2kVyvsQk8!", "download", "-s", "-f", "-l", "en", path } }
+function load_sub_fn(langs)
+  return function()
+    path = mp.get_property("path")
+    print(path)
+    path = path:gsub("[%-_ ]*%[[%w%-_][%w%-_][%w%-_][%w%-_][%w%-_][%w%-_][%w%-_][%w%-_][%w%-_][%w%-_][%w%-_]%](%.%w%w%w%w?)$", '%1')
+    print(path)
 
-  mp.osd_message("Searching subtitle")
-  res = utils.subprocess(t)
-  if res.error == nil then
-    srt_path = string.gsub(path, "%.%w+$", ".srt")
-    if mp.commandv("sub_add", srt_path) then
-      mp.msg.warn("Subtitle download succeeded")
-      mp.osd_message("Subtitle '" .. srt_path .. "' download succeeded")
+    -- Subtitles
+    t = { args = { "subliminal", "--opensubtitles", "xk3", "idiZQc2kVyvsQk8!", "download", "-s", "-f" } }
+    for i = 1, #langs do
+      table.insert(t.args, "-l")
+      table.insert(t.args, langs[i])
+    end
+    table.insert(t.args, path)
+
+    mp.osd_message("Searching subtitles")
+    res = utils.subprocess(t)
+    if res.error == nil then
+      for i = 1, #langs do
+        srt_path = string.gsub(path, "%.%w+$", "." .. langs[i] .. ".srt")
+        if mp.commandv("sub_add", srt_path) then
+          mp.msg.warn("Subtitle download succeeded")
+          mp.osd_message("Subtitle '" .. srt_path .. "' download succeeded")
+        else
+          display_error()
+        end
+      end
     else
       display_error()
     end
-  else
-    display_error()
   end
 end
 
-mp.add_key_binding("ctrl+j", "auto_load_subs", load_sub_fn)
+mp.add_key_binding("ctrl+j", "auto_load_subs_en", load_sub_fn({"en"}))
+mp.add_key_binding("ctrl+alt+j", "auto_load_subs_zh", load_sub_fn({"zt", "ze", "zh"}))
