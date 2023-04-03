29063dd3ae70ebda23e88750fa3708732a36b2a7
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-03 04:55:01 -0500

    mrvideo

--- .config/fish/functions/mrvideo.fish
+++ .config/fish/functions/mrvideo.fish
@@ -2,11 +2,11 @@ function mrvideo
     rsync -a --remove-source-files --backup-dir (date "+%d.%m.%Y") --exclude=".*" ~/d/70_Now_Watching/keep/ ~/d/77_Library/
 
     lb relmv (
-        lb watch ~/lb/video.db -E /70_Now_Watching/ --local-only --lower 6 --upper 30 -p f | sort --unique --ignore-case  | head -n 8
+        lb watch ~/lb/video.db -E /70_Now_Watching/ --local-only --lower 6 --upper 30 -p bf | shuf  | head -n 2
     ) /mnt/d/70_Now_Watching/
 
     lb relmv (
-        lb watch ~/lb/video.db -E /70_Now_Watching/ --local-only --upper 6 -p f | sort --unique --ignore-case  | head -n 10
+        lb watch ~/lb/video.db -E /70_Now_Watching/ --local-only --upper 6 -p bf | shuf | head -n 5
     ) /mnt/d/70_Now_Watching/
 
     lb fsadd --video ~/lb/video.db /mnt/d/70_Now_Watching/
