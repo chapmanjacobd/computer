c411569dc0257e652504a5f88bb7be3587e63e53
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-06 00:39:22 -0500

    trash-empty

--- .config/fish/functions/trash-empty.fish
+++ .config/fish/functions/trash-empty.fish
@@ -1,9 +1,11 @@
 # Defined interactively
 function trash-empty
     trash-size
-    if gum confirm --default=no 'Refresh snapshots?'
-        for mnt in /mnt/d1 /mnt/d2 /mnt/d3 /mnt/d4 /mnt/d5 /mnt/d6 /mnt/d7 /home
-            kitty fish -c "btrfs_check_delete_snapshot $mnt" &
+    if test (hostname) = pakon
+        if gum confirm --default=no 'Refresh snapshots?'
+            for mnt in /mnt/d1 /mnt/d2 /mnt/d3 /mnt/d4 /mnt/d5 /mnt/d6 /mnt/d7 /home
+                kitty fish -c "btrfs_check_delete_snapshot $mnt" &
+            end
         end
     end
     wait
