c3baf8892f8b6eb1269dec5d09cb3e8609df7690
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-08 11:40:10 +0800

    pipinstall pip_installed

---
+++ .config/fish/functions/pipinstall.fish
@@ -0,0 +1,7 @@
+# Defined interactively
+function pipinstall --wraps='pip install'
+    pip install $argv
+    and echo $argv | string split ' ' >>~/.github/pip_installed
+
+    sort --unique --ignore-case ~/.github/pip_installed | sponge ~/.github/pip_installed
+end
