450585f9222d923636f4aba1adf083d91b20715c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-08 00:53:07 -0500

    allscreens gitignore

---
+++ .config/fish/functions/allscreens.fish
@@ -0,0 +1,9 @@
+# Defined interactively
+function allscreens
+    if grep -qEi "(DisplayPort-0)" (kscreen-doctor -o | psub)
+        kscreen-doctor output.DisplayPort-0.enable output.HDMI-A-0.enable output.DVI-D-0.enable
+    else
+        kscreen-doctor output.DP-1.enable output.HDMI-1.enable output.DVI-D-1.enable
+    end
+    bash -c 'kquitapp5 plasmashell || killall plasmashell; kstart5 plasmashell'
+end
