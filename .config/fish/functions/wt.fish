1e69570fd52716d9470712048f491262e52a6a8c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-09 10:14:47 -0500

    wt-dev wt

--- .config/fish/functions/wt.fish
+++ .config/fish/functions/wt.fish
@@ -1,3 +1,3 @@
 function wt
-    /home/xk/.local/bin/lb wt --keep-dir ~/d/70_Now_Watching/Keep/ -u 'ntile(10000) over (order by size / duration) desc' -k delete ~/lb/video.db $argv
+    /home/xk/.local/bin/lb wt --keep-dir /mnt/d/70_Now_Watching/Keep/ -u 'ntile(10000) over (order by size / duration) desc' -k delete ~/lb/video.db $argv
 end
