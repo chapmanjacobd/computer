583fec079192f46e833555d7518901a27acc8b1d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-11 11:11:22 +0800

    pyformat

--- .config/fish/functions/pyformat.fish
+++ .config/fish/functions/pyformat.fish
@@ -1,6 +1,6 @@
 # Defined via `source`
 function pyformat
     for file in $argv
-        ssort $file && isort --profile black -p atlasai.data_processor --line-length=120 $file && black --line-length=120 --skip-string-normalization $file
+        ssort $file && isort --profile black -p atlasai --line-length=120 $file && black --line-length=120 --skip-string-normalization $file
     end
 end
