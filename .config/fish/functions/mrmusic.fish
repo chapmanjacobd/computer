fb2a708039621b27cdbf400431dc7c163122539d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-11 22:15:56 -0500

    mrmusic

--- .config/fish/functions/mrmusic.fish
+++ .config/fish/functions/mrmusic.fish
@@ -5,5 +5,9 @@ function mrmusic
         lt --db ~/lb/audio.db --local-media-only -w 'play_count=0' -u random -L 400 -p f --moved /mnt/d/ /mnt/d/80_Now_Listening/ | psub
     ) /mnt/d/ /mnt/d/80_Now_Listening/
 
+    lb relmv (
+        lb listen ~/lb/audio.db -E /80_Now_Listening/ --local-only --lower 4 --upper 16 -p bf | shuf | head -n 8
+    ) /mnt/d/80_Now_Listening/
+
     lb fsadd --audio ~/lb/audio.db /mnt/d/80_Now_Listening/
 end
