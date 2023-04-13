7c439f010d0fd0bd4fa7f362de690a33d00ed6df
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-13 14:06:57 +0800

    pip_installed gitignore eda parquet pkl

--- bin/eda.py
+++ bin/eda.py
@@ -1,6 +1,69 @@
+#!/usr/bin/python
+
+import argparse
+
+import magic
+import pandas as pd
+
+
+def read_file_to_dataframe(file_path, table_index=0, nrows=None):
+    mime = magic.detect_from_filename(file_path)
+    print(mime)
+
+    if mime.mime_type == "text/csv":
+        df = pd.read_csv(file_path, nrows=nrows)
+    elif mime.mime_type == "text/tab-separated-values":
+        df = pd.read_csv(file_path, delimiter="\t", nrows=nrows)
+    elif mime.mime_type == "application/vnd.ms-excel":
+        df = pd.read_excel(file_path, nrows=nrows)
+    elif mime.mime_type == "application/json":
+        df = pd.read_json(file_path)
+    elif mime.name == 'Apache Parquet' or mime.mime_type == "application/parquet":
+        df = pd.read_parquet(file_path)
+    elif mime.mime_type == "application/octet-stream":
+        df = pd.read_pickle(file_path)
+    elif mime.mime_type == "text/html":
+        dfs = pd.read_html(file_path)
+        df = dfs[table_index]
+        if nrows is not None:
+            df = df.head(nrows)
+    else:
+        raise ValueError(f"{file_path}: Unsupported file type: {mime}")
+
+    return df
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Perform EDA on one or more files")
+    parser.add_argument(
+        "file_paths",
+        metavar="file_path",
+        nargs="+",
+        help="path to one or more files",
+    )
+    parser.add_argument(
+        "--table",
+        "-t",
+        type=int,
+        default=0,
+    )
+    parser.add_argument("--end-row", "--nrows", type=int, default=None)
+    args = parser.parse_args()
+    for file_path in args.file_paths:
+        print('##', file_path)
+        df = read_file_to_dataframe(file_path, table_index=args.table, nrows=args.end_row)
+        eda(df)
+
 
 def print_md(df):
     print(df.to_markdown(tablefmt="github"))
+
+
+def print_series(s):
+    if len(s) > 0:
+        print()
+        print("\n".join([f"- {col}" for col in s]))
+        print()
 
 
 def eda(df):
@@ -37,8 +100,6 @@ def eda(df):
     print_md((df.isna().mean().round(3) * 100).rename('% missing'))
     print()
 
-def print_series(s):
-    if len(s) > 0:
-        print()
-        print("\n".join([f"- {col}" for col in s]))
-        print()
+
+if __name__ == '__main__':
+    main()
