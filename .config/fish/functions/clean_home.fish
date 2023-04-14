68503081e26deae88d4da82acf10981570b8d448
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-14 10:34:10 -0500

    clean_home

--- .config/fish/functions/clean_home.fish
+++ .config/fish/functions/clean_home.fish
@@ -1,11 +1,11 @@
 # Defined interactively
 function clean_home
     ~/
-    rmdir * 2> /dev/null
-    trash-put .config/ksmserverrc .xsession-errors 2> /dev/null
-    fd -d1 -tf -eDESKTOP -x rm 
+    rmdir * 2>/dev/null
+    trash-put .config/ksmserverrc .xsession-errors 2>/dev/null
+    fd -d1 -tf -eDESKTOP -x rm
 
     git add .
-    git pull
-    git reset
+    and git pull
+    and git reset
 end
