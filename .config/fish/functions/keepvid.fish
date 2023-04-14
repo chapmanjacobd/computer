2e9b5b7cbece1e9a0b8e565a29b52a689db9879e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-14 22:25:09 +0800

    keepvid

--- .config/fish/functions/keepvid.fish
+++ .config/fish/functions/keepvid.fish
@@ -3,5 +3,5 @@ function keepvid
     cp "$argv" ~/d/77_Library/
     set f (basename "$argv[1]")
     ffsmall "$HOME/d/77_Library/$f" $argv[2..-1]
-    and /bin/rm "$argv[1]"
+    and /bin/rm "$argv[1]" "$HOME/d/77_Library/$f"
 end
