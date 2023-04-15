9f5ace94c71ad14acf1f5e08182a62d06ddf2472
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-14 21:18:44 -0500

    clean_home

--- .config/fish/functions/clean_home.fish
+++ .config/fish/functions/clean_home.fish
@@ -3,7 +3,8 @@ function clean_home
     ~/
     rmdir * 2>/dev/null
     trash-put .config/ksmserverrc .xsession-errors 2>/dev/null
-    fd -d1 -tf -eDESKTOP -x rm
+    fd -d1 -tf -eDESKTOP -x trash
+    fd -d2 -tf -H '.syncthing.*.tmp' -x trash
 
     git add .
     and git pull
