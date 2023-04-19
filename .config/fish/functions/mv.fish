a619bae0eaa9260ec055b209d702be865745d4cd
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-19 08:25:28 +0800

    mv

--- .config/fish/functions/mv.fish
+++ .config/fish/functions/mv.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function mv
-    if not test -e $argv[-1]
+    if not path extension $argv[-1] > /dev/null; and not test -e $argv[-1]
         mkdir -p $argv[-1]
     end
     command mv $argv
