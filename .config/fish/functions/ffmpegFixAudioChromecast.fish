aa551fb6fee87668a0fee5ce6cb2d2a98a3d7b8f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-21 12:29:40 +0800

    ffmpegFixAudioChromecast

--- .config/fish/functions/ffmpegFixAudioChromecast.fish
+++ .config/fish/functions/ffmpegFixAudioChromecast.fish
@@ -1,6 +1,6 @@
 # Defined via `source`
 function ffmpegFixAudioChromecast --wraps=ffmpeg
     set out (random-filename "$argv[1]")
-    ffmpeg -nostdin -loglevel error -stats -i "$argv[1]" -map 0 -scodec webvtt -vcodec h264 -preset fast -profile:v high -level 4.1 -crf 17 -pix_fmt yuv420p -acodec libopus -ac 2 -b:a 96k -filter:a loudnorm=i=-18:lra=17 $argv[2..-1] $out
+    ffmpeg -nostdin -loglevel error -stats -i "$argv[1]" -map 0 -c:v copy -acodec libopus -ac 2 -b:a 96k -filter:a loudnorm=i=-18:lra=17 $argv[2..-1] $out
     echo $out
 end
