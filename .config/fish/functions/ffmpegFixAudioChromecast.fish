0e8b83391f59ac43f24eafa8e9fa774edc0fe7df
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-21 12:31:23 +0800

    ffmpegFixAudioChromecast

--- .config/fish/functions/ffmpegFixAudioChromecast.fish
+++ .config/fish/functions/ffmpegFixAudioChromecast.fish
@@ -2,5 +2,6 @@
 function ffmpegFixAudioChromecast --wraps=ffmpeg
     set out (random-filename "$argv[1]")
     ffmpeg -nostdin -loglevel error -stats -i "$argv[1]" -map 0 -c:v copy -acodec libopus -ac 2 -b:a 96k -filter:a loudnorm=i=-18:lra=17 $argv[2..-1] $out
+    and trash "$argv[1]"
     echo $out
 end
