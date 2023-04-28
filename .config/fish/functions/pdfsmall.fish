9f0452a3efc65d7b92ac285a0cc3e2f6d2f68330
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-04-28 18:48:33 +0800

    pdfsmall pdfsmaller

---
+++ .config/fish/functions/pdfsmall.fish
@@ -0,0 +1,4 @@
+# Defined interactively
+function pdfsmall
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=(random-filename "$argv") $argv
+end
---
+++ .config/fish/functions/pdfsmaller.fish
@@ -0,0 +1,4 @@
+# Defined interactively
+function pdfsmaller
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=(random-filename "$argv") $argv
+end
