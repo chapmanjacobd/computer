7920a871516ac0a88712a1a971e739e1082f6cb9
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-01 05:27:26 -0500

    mrmusic

--- .config/fish/functions/mrmusic.fish
+++ .config/fish/functions/mrmusic.fish
@@ -1,13 +1,11 @@
 function mrmusic
-    rsync -a --remove-source-files --backup-dir (date "+%d.%m.%Y") --files-from=(lt --db ~/lb/audio.db -s /mnt/d/80_Now_Listening/ -p f --moved /mnt/d/80_Now_Listening/ /mnt/d/ | psub) /mnt/d/80_Now_Listening/ /mnt/d/
+    lb relmv /mnt/d/ /mnt/d/80_Now_Listening/
 
-    rsync -a --remove-source-files --backup-dir (date "+%d.%m.%Y") --files-from=(
-        lt --db ~/lb/audio.db --local-media-only -w 'play_count=0' -u random -L 600 -p f --moved /mnt/d/ /mnt/d/80_Now_Listening/ | psub
-    ) /mnt/d/ /mnt/d/80_Now_Listening/
+    lb relmv (
+        lt --db ~/lb/audio.db --local-media-only -w 'play_count=0' -u random -L 600 -p
+    ) /mnt/d/80_Now_Listening/
 
     lb relmv (
         lb listen ~/lb/audio.db -E /80_Now_Listening/ --local-only --lower 4 --upper 16 -p bf | shuf | head -n 8
     ) /mnt/d/80_Now_Listening/
-
-    lb fsadd --audio ~/lb/audio.db /mnt/d/80_Now_Listening/
 end
