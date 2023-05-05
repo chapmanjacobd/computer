c813a841c8ade2d6653726112929e6e396ee2e01
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-05 06:01:12 -0500

    vpn config

--- .config/fish/functions/vpn.fish
+++ .config/fish/functions/vpn.fish
@@ -8,6 +8,6 @@ function vpn
     ## sudo ip -6 rule add fwmark 0x01 lookup 100
     #sudo sshuttle --dns -r oci --ssh-cmd 'ssh -F /home/xk/.ssh/config' -x 144.24.93.111 -x 192.168.0.0/16 --method tproxy --disable-ipv6 0/0 # ::/0
 
-    sshuttle --dns -r oci -x 144.24.93.111 -x 192.168.0.0/16 0/0 --disable-ipv6 --no-latency-control
+    sshuttle --dns -r oci -x 144.24.94.247 -x 192.168.0.0/16 0/0 --disable-ipv6 --no-latency-control
     or pkill tixati
 end
