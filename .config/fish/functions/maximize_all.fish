0e1774f183f459aaba2d0de193eeb653c247352a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-06 07:57:20 -0500

    maximize_all

--- .config/fish/functions/maximize_all.fish
+++ .config/fish/functions/maximize_all.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function maximize_all
     for win in (lswin | grep -iv plasma | cut -f1 -d' ')
-        wmctrl -i -r $win -b add,maximized_horz,maximized_vert
+        wmctrl -i -r $win -b toggle,maximized_horz,maximized_vert
     end
 end
