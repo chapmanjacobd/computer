714355773f3e5f4943f17d4f71d91aca6b0386c1
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-08 20:27:56 -0500

    ffsmall_delete

--- .config/fish/functions/ffsmall_delete.fish
+++ .config/fish/functions/ffsmall_delete.fish
@@ -1,10 +1,5 @@
 # Defined interactively
 function ffsmall_delete
-    while test (pgrep ffmpeg | count) -ge 3
-        sleep (random_thousanths 0 5000)
-    end
-
-    set filen (string split -r -m1 . "$argv[1]")[1]
-    ffmpeg -nostdin -hide_banner -dn -y -i "$argv[1]" $argv[2..-1] -vf "scale=-2:min(ih\,1440)" -vcodec libx265 -preset 4 -acodec libopus -b:a 96k "$filen".small.mkv
+    ffsmall $argv
     and trash-put "$argv[1]"
 end
