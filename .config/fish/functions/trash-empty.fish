09cc257ba4273755ce80d872f9e1d6af38416ecc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-14 15:00:01 -0500

    trash-empty

--- .config/fish/functions/trash-empty.fish
+++ .config/fish/functions/trash-empty.fish
@@ -1,21 +1,28 @@
 # Defined interactively
 function trash-empty
     trash-size
+    trash-list | tee -a ~/.local/share/trashed.txt
 
-    for d in /mnt/d{1,2,3,4,5,6,7}/.Tras*/*/files/
-        NO_COLOR=1 ncdu $d
-    end
-
-    if gum confirm --default=no 'Empty trash?'
-        if test (hostname) = pakon
-            if gum confirm --default=no 'Refresh snapshots?'
-                for mnt in /mnt/d1 /mnt/d2 /mnt/d3 /mnt/d4 /mnt/d5 /mnt/d6 /mnt/d7 /home
-                    kitty fish -c "btrfs_check_delete_snapshot $mnt" &
-                end
-            end
+    if test (hostname) = pakon
+        for d in /mnt/d{1,2,3,4,5,6,7}/.Tras*/*/files/
+            NO_COLOR=1 ncdu $d
         end
-        wait
-        trash-list | tee -a ~/.local/share/trashed.txt
-        command trash-empty -f $argv
+
+        if gum confirm --default=no 'Empty trash?'
+            command trash-empty -f $argv
+        end
+
+        if gum confirm --default=no 'Refresh snapshots?'
+            for mnt in /mnt/d1 /mnt/d2 /mnt/d3 /mnt/d4 /mnt/d5 /mnt/d6 /mnt/d7 /home
+                kitty fish -c "btrfs_check_delete_snapshot $mnt" &
+            end
+            wait
+        end
+
+    else
+        if gum confirm --default=no 'Empty trash?'
+            command trash-empty -f $argv
+        end
     end
+
 end
