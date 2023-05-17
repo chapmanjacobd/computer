bf47d0a95ada5118ac0bb5ebf6266db517be0125
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-17 01:21:32 -0500

    mrporn

--- .config/fish/functions/mrporn.fish
+++ .config/fish/functions/mrporn.fish
@@ -4,9 +4,9 @@ function mrporn
     lb relmv /mnt/d/60_Now_Watching/69_Taxes/ /mnt/d/69_Taxes/
 
     lb relmv (
-    lb wt --db ~/lb/fs/tax.db --portrait -L 24 -u play_count,time_created -p f -E 60_Now_Watching -E /keep/ --local-only
+    lb wt ~/lb/fs/tax.db --portrait -L 24 -u play_count,time_created -p f -E 60_Now_Watching -E /keep/ --local-only
      ) /mnt/d/60_Now_Watching/
-    lb relmv (                                                          lb wt --db ~/lb/fs/tax.db -E /60_Now_Watching/ -E /keep/ --local-only --lower 4 --upper 16 -p bf | shuf | head -n 8
+    lb relmv (                                                          lb wt ~/lb/fs/tax.db -E /60_Now_Watching/ -E /keep/ --local-only --lower 4 --upper 16 -p bf | shuf | head -n 8
 ) /mnt/d/60_Now_Watching/
 
     lb fsadd --video ~/lb/fs/tax.db /mnt/d/60_Now_Watching/
