5326e0fe2dd168a7b6fe5d3748543d85144052d6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-17 10:39:21 -0500

    relaxvid relaxvidd

--- .config/fish/functions/relaxvidd.fish
+++ .config/fish/functions/relaxvidd.fish
@@ -2,6 +2,6 @@ function relaxvidd
     fish -c 'lb extract ~/lb/fs/tax.db ~/d/60_Now_Watching/ ~/d/64_Relaxation/' &
     mpv --input-ipc-server=/tmp/mpv_socket --no-video --af="acompressor=ratio=4,loudnorm" (fd -eOPUS -eOGA . ~/d/64_Relaxation) &
     mount_pakond
-    lb-dev wt ~/lb/sshfs_tax.db -L inf --action askkeep --keep-dir /mnt/d/60_Now_Watching/keep/ --sort "path like '%64_Relaxation%' desc, time_modified, duration desc, priority" -m 3 $argv
+    lb-dev wt ~/lb/sshfs_tax.db -L inf --action ask_move_or_delete --keep-dir /mnt/d/60_Now_Watching/keep/ --sort "path like '%64_Relaxation%' desc, time_modified, duration desc, priority" -m 3 $argv
     pkill -x mpv
 end
