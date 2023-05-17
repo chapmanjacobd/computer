ff306df085a14fd6c111e568c88b7eb7c12c9ddc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-17 01:19:34 -0500

    mrporn

--- .config/fish/functions/mrporn.fish
+++ .config/fish/functions/mrporn.fish
@@ -1,13 +1,13 @@
 function mrporn
     rsync -a --remove-source-files --backup-dir (date "+%d.%m.%Y") --exclude=".*" ~/d/60_Now_Watching/keep/ ~/d/69_Taxes_Keep/
 
-    rsync -a --remove-source-files --files-from=(
-        lb wt --db ~/lb/fs/tax.db --portrait -L 24 -u play_count,time_created -p f -E 60_Now_Watching -s 69 --moved /mnt/d/ /mnt/d/60_Now_Watching/ | psub
-    ) /mnt/d/ /mnt/d/60_Now_Watching/
+    lb relmv /mnt/d/60_Now_Watching/69_Taxes/ /mnt/d/69_Taxes/
 
-    rsync -a --remove-source-files --files-from=(
-        lb wt --db ~/lb/fs/tax.db -L 48 -u 'play_count,duration desc' -p f -E 60_Now_Watching -s 69 --moved /mnt/d/ /mnt/d/60_Now_Watching/ | psub
-    ) /mnt/d/ /mnt/d/60_Now_Watching/
+    lb relmv (
+    lb wt --db ~/lb/fs/tax.db --portrait -L 24 -u play_count,time_created -p f -E 60_Now_Watching -E /keep/ --local-only
+     ) /mnt/d/60_Now_Watching/
+    lb relmv (                                                          lb wt --db ~/lb/fs/tax.db -E /60_Now_Watching/ -E /keep/ --local-only --lower 4 --upper 16 -p bf | shuf | head -n 8
+) /mnt/d/60_Now_Watching/
 
     lb fsadd --video ~/lb/fs/tax.db /mnt/d/60_Now_Watching/
 end
