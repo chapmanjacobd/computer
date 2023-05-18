7b2cc222d2501270e39241c8ad863061c8c616c3
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-17 21:03:22 -0500

    wt-dev

--- .config/fish/functions/wt-dev.fish
+++ .config/fish/functions/wt-dev.fish
@@ -1,5 +1,5 @@
 # Defined interactively
 function wt-dev
     ~/lb/
-    python -m xklb.lb watch --keep-dir /mnt/d/70_Now_Watching/Keep/ -u 'ntile(10000) over (order by size / duration) desc' -k delete video.db $argv
+    python -m xklb.lb watch --keep-dir /mnt/d/70_Now_Watching/Keep/ -k delete video.db $argv
 end
