9c2c7964fa4d9a999435ba7ab1a3eca478764e5c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-19 13:47:24 -0500

    ffsmallpar gitignore config runs-without-willing-to-cite will-cite

--- .config/fish/functions/ffsmallpar.fish
+++ .config/fish/functions/ffsmallpar.fish
@@ -2,6 +2,6 @@
 function ffsmallpar
     set tmpfile (mktemp)
     fd -S+200MB | grep -v .small. >$tmpfile
-    cat $tmpfile | parallel --dry-run --sshlogin :,backup,pulse15 --jobs 3,2,2 --transfer --return {.}.small.mkv --cleanup ffmpeg -nostdin -hide_banner -dn -y -i {} -vf "scale=-2:min(ih\,1440)" -vcodec libx265 -preset 4 -acodec libopus -b:a 96k {.}.small.mkv
+    cat $tmpfile | parallel --sshlogin backup,pulse15,: --jobs 2,2,3 --transfer "ffmpeg -nostdin -hide_banner -dn -y -i {} -vf 'scale=-2:min(ih\,1440)' -vcodec libx265 -preset 4 -acodec libopus -b:a 96k {.}.small.mkv && rm {} && rsync -auh --remove-sent-files {.}.small.mkv" $hostname:(pwd)
     cat $tmpfile | parallel --dry-run rm
 end
