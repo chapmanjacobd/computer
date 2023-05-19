224ed659c1ebf18e7357a9435a6e2593a7d830d0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-19 09:09:05 -0500

    ffsmallpar

--- .config/fish/functions/ffsmallpar.fish
+++ .config/fish/functions/ffsmallpar.fish
@@ -2,5 +2,6 @@
 function ffsmallpar
     set tmpfile (mktemp)
     fd -S+200MB | grep -v .small. >$tmpfile
-    cat $tmpfile | parallel --dry-run
+    cat $tmpfile | parallel --dry-run --sshlogin :,backup,pulse15 --jobs 3,2,2 --transfer --return {.}.small.mkv --cleanup ffmpeg -nostdin -hide_banner -dn -y -i {} -vf "scale=-2:min(ih\,1440)" -vcodec libx265 -preset 4 -acodec libopus -b:a 96k {.}.small.mkv
+    cat $tmpfile | parallel --dry-run rm
 end
