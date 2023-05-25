c541b9178e8081c00720b1de61766bf1f9ac4eab
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-25 17:09:39 -0500

    ffsmallpar

--- .config/fish/functions/ffsmallpar.fish
+++ .config/fish/functions/ffsmallpar.fish
@@ -16,5 +16,5 @@ function ffsmallpar
     ssh -fN backup
     cat $tmpfile | parallel --filter-hosts --sshlogin backup,pulse15,: --jobs 2,2,3 --transfer "ffmpeg -nostdin -hide_banner -dn -y -i {} -vf 'scale=-2:min(ih\,1440)' -vcodec libx265 -preset 4 -acodec libopus -b:a 96k {.}.small.mkv && rm {} && rsync -auh --remove-sent-files {.}.small.mkv" $hostname:(pwd)
     and cat $tmpfile | parallel rm || true
-    or cat $tmpfile >>~/ffsmallpar_errors.txt
+    or cat $tmpfile >~/ffsmallpar_errors.txt
 end
