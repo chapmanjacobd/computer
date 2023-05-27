13d394737416a32228f3b9d6554ce542b3cc11eb
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-26 21:47:13 -0500

    gitignore singlecopymaplink

---
+++ bin/singlecopymaplink.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+sleep .05
+xdotool keydown Control key c key Control
+sleep .$[ ( $RANDOM % 6 ) + 7 ]s
+xdotool keydown Alt key Tab key Alt
+sleep .05
+xdotool keydown Control key t key Control
+sleep .005
+xdotool keydown Control key l key Control
+sleep .0$[ ( $RANDOM % 6 ) + 7 ]s
+xdotool keydown Control key v key Control
+sleep .$[ ( $RANDOM % 6 ) + 7 ]s
+xdotool key Return
+sleep .05
+xdotool keydown Alt key Tab key Alt
+sleep .05
+xdotool key Down
+xdotool keydown Control key c key Control
+sleep .$[ ( $RANDOM % 6 ) + 7 ]s
+xdotool keydown Alt key Tab key Alt
+sleep .05
+xdotool keydown Control key t key Control
+sleep .005
+xdotool keydown Control key l key Control
+sleep .0$[ ( $RANDOM % 6 ) + 7 ]s
+xdotool keydown Control key v key Control
+sleep .$[ ( $RANDOM % 6 ) + 7 ]s
+xdotool key Return
+sleep .05
+xdotool keydown Alt key Tab key Alt
+sleep .05
+xdotool key Down
+sleep .05
+xdotool keydown Control key c key Control
+sleep .$[ ( $RANDOM % 6 ) + 7 ]s
+xdotool keydown Alt key Tab key Alt
+sleep .05
+xdotool keydown Control key t key Control
+sleep .005
+xdotool keydown Control key l key Control
+sleep .0$[ ( $RANDOM % 6 ) + 7 ]s
+xdotool keydown Control key v key Control
+sleep .$[ ( $RANDOM % 6 ) + 7 ]s
+xdotool key Return
+sleep .05
+xdotool keydown Control key Tab key Control
+sleep .05
+xdotool keydown Control key Tab key Control
+xdotool keydown Control key l key Control
+sleep .0$[ ( $RANDOM % 6 ) + 7 ]s
+xdotool keydown Control key c key Control
+sleep .05
+xdotool keydown Control key w key Control
+sleep .005
+xdotool keydown Alt key Tab key Alt
+sleep .05
+xdotool key Up
+xdotool key Up
+xdotool key Right
+xdotool keydown Control key v key Control
+xdotool key Down
+xdotool keydown Alt key Tab key Alt
+sleep .0$[ ( $RANDOM % 6 ) + 7 ]s
+xdotool keydown Control key l key Control
+sleep .05
+xdotool keydown Control key c key Control
+sleep .05
+xdotool keydown Control key w key Control
+sleep .005
+xdotool keydown Alt key Tab key Alt
+sleep .05
+xdotool keydown Control key v key Control
+xdotool key Down
+xdotool key Left
