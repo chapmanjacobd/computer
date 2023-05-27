f0b7a7007e054147ca8d58ee46715c661d241311
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-26 23:06:55 -0500

    mrmusic

--- .config/fish/functions/mrmusic.fish
+++ .config/fish/functions/mrmusic.fish
@@ -2,7 +2,7 @@ function mrmusic
     lb relmv /mnt/d/80_Now_Listening/ /mnt/d/
 
     lb relmv (
-        lt --db ~/lb/audio.db --local-media-only -w 'play_count=0' -u random -L 600 -p
+        lt --db ~/lb/audio.db --local-media-only -w 'play_count=0' -u random -L 600 -p f
     ) /mnt/d/80_Now_Listening/
 
     lb relmv (
