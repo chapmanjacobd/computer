e3d419fdc66b8625e39e3eee1b2aa398cbaf13bd
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-05-29 13:16:12 -0500

    maxmem10

--- .config/fish/functions/maxmem10.fish
+++ .config/fish/functions/maxmem10.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function maxmem10
-    systemd-run --user --scope -p MemoryMax=10G -p MemorySwapMax=10G $argv
+    systemd-run --user --scope -p MemoryMax=10G -p MemorySwapMax=10G fish -c "$argv"
 end
