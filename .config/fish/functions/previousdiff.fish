f1f622f0bea236fb295817b2adf34f84b97aa3fd
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-06-02 20:19:42 -0500

    previousdiff

---
+++ .config/fish/functions/previousdiff.fish
@@ -0,0 +1,13 @@
+# Defined interactively
+function previousdiff --argument name
+    set name (coalesce $argv[1] 'test')
+    set current_output /tmp/cmd_diff_2_$name
+    set previous_output /tmp/cmd_diff_1_$name
+
+    cat /dev/stdin | tee $current_output
+
+    if test -e $previous_output
+        diff $previous_output $current_output
+    end
+    mv $current_output $previous_output
+end
