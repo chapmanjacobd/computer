0a8e2f2bf867ae172b73c674344fee4398c0cad6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-06-09 00:21:26 -0500

    gdl

--- .config/fish/functions/gdl.fish
+++ .config/fish/functions/gdl.fish
@@ -1,5 +1,5 @@
 # Defined interactively
 function gdl
     ~/d/61_Photos_Unsorted/
-    gallery-dl --download-archive $HOME/.local/share/gallerydl.sqlite3 $argv
+    gallery-dl --no-check-certificate --download-archive $HOME/.local/share/gallerydl.sqlite3 $argv
 end
