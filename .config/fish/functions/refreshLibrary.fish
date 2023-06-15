01c16822183668d2d6c74fb8102c1d147369ed27
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-06-15 13:25:34 -0500

    refreshLibrary

--- .config/fish/functions/refreshLibrary.fish
+++ .config/fish/functions/refreshLibrary.fish
@@ -2,7 +2,7 @@
 function refreshLibrary
     ~/lb/
     for db in video.db fs/tax.db audio.db fs/63_Sounds.db
-        lb fsupdate $db --delete-unplayable
+        lb fsupdate $db
         b sqlite-utils rebuild-fts $db media
     end
     wait
