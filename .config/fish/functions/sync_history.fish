0ef988bafbf355505ae6dd78a7446a75a157e9e6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-06-19 23:08:12 -0500

    sync_history

--- .config/fish/functions/sync_history.fish
+++ .config/fish/functions/sync_history.fish
@@ -1,10 +1,10 @@
 # Defined interactively
 function sync_history
-    cat ~/.local/share/fish/fish_history.sync-conflict-* >>~/.local/share/fish/fish_history
-    trash-put ~/.local/share/fish/fish_history.sync-conflict-*
+    count ~/.local/share/fish/fish_history.sync-conflict-* >/dev/null; and cat ~/.local/share/fish/fish_history.sync-conflict-* >>~/.local/share/fish/fish_history
+    count ~/.local/share/fish/fish_history.sync-conflict-* >/dev/null; and trash-put ~/.local/share/fish/fish_history.sync-conflict-*
 
     cp ~/.local/share/fish/fish_history ~/d/00_Metadata/SECURE/
 
-    cat ~/.local/share/z/data.sync-conflict* >>~/.local/share/z/data
-    trash-put ~/.local/share/z/data.sync-conflict*
+    count ~/.local/share/z/data.sync-conflict* >/dev/null; cat ~/.local/share/z/data.sync-conflict* >>~/.local/share/z/data
+    count ~/.local/share/z/data.sync-conflict* >/dev/null; trash-put ~/.local/share/z/data.sync-conflict*
 end
