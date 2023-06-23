102d5091f71377daa8fd78dea467eae3ff30ad90
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-06-23 15:35:50 -0500

    setLGBrightness

--- .config/fish/functions/setLGBrightness.fish
+++ .config/fish/functions/setLGBrightness.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function setLGBrightness
-    sudo modprobe i2c-dev && repeal sudo ddcutil setvcp 10 $argv
+    sudo modprobe i2c-dev && repeal sudo ddcutil setvcp --mfg GSM 10 $argv
 end
