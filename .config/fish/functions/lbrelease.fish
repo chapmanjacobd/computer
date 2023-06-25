fa80088d4383aa4be4ba2e581fc451cacae6e3d0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-06-24 20:02:27 -0500

    lbrelease

--- .config/fish/functions/lbrelease.fish
+++ .config/fish/functions/lbrelease.fish
@@ -3,7 +3,6 @@ function lbrelease --argument oldver newver
 
     #PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring poetry update
     sed -i "s|$oldver|$newver|" xklb/__init__.py
-    pdm lock --group :all
 
     echo "All of these things should be assigning to a variable; if updating data use db.conn.execute"
     rg db.execute
@@ -12,8 +11,9 @@ function lbrelease --argument oldver newver
     python readme.py >README.md
     if wipm $newver
         git tag -a v$newver && git push --tags
-        pip install --upgrade pip
-        sleep 500
+        pip install --upgrade pip pdm
+        pdm lock --group :all
+        sleep 400
         python3.11 -m pip install --upgrade xklb
         python3.11 -m pip install --upgrade xklb
     end
