e8e1b33449781a9b51eb2e2b7f7d05e8631d4781
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-06-29 19:06:10 -0500

    dlstatus

--- .config/fish/functions/dlstatus.fish
+++ .config/fish/functions/dlstatus.fish
@@ -1,10 +1,13 @@
 function dlstatus
     echo audio.db
     lb-dev ds ~/lb/audio.db
+    echo
 
     echo video.db
     lb-dev ds ~/lb/video.db
+    echo
 
     echo fs/tax.db
     lb-dev ds ~/lb/fs/tax.db
+    echo
 end
