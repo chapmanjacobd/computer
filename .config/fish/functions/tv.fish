3e791378e9ccde911e98c083fa4e629d0ba6f8ba
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-06-30 18:45:08 -0500

    tv

--- .config/fish/functions/tv.fish
+++ .config/fish/functions/tv.fish
@@ -13,5 +13,5 @@ function tv
     #breaktimer disable && pkill breaktimer
     pactl set-default-sink alsa_output.pci-0000_01_00.1.hdmi-stereo-extra3
     ~/.config/autostart.sh &
-    pkill projectM-jack && projectM-jack &
+    pkill projectM-pulseaudio && projectM-pulseaudio &
 end
