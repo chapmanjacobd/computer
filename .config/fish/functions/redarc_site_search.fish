45932c6412867eec83347e1edcb8369389e9155e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-07-01 21:44:58 -0500

    redarc_site_search

---
+++ .config/fish/functions/redarc_site_search.fish
@@ -0,0 +1,6 @@
+# Defined interactively
+function redarc_site_search --argument site
+    for f in (fd -eZST)
+        zstdcat $f | head -10000 | jq -r 'select(.score > 7) | .url' | grep -vEi '^/r/|^/user/|\. |^null$|^$' | grep -i $site | head
+    end
+end
