454c931d438423475f2bbcd7c937a3bccc9fffec
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-07-01 21:50:52 -0500

    redarc_audio redarc_gifs redarc_pics redarc_video

---
+++ .config/fish/functions/redarc_pics.fish
@@ -0,0 +1,4 @@
+# Defined interactively
+function redarc_pics
+    zstdcat (fd -eZST) | jq -r 'select(.score > 7) | .url' | grep -iE 'tumblr.com|bdsmlr.com|ibb.co|subimg.net|imgur.com|i.redd.it|reddit.com/gallery/|artstation.com|fitnakedgirls.com|i.reddituploads.com|facebook.com/photo|facebook.com/.*/photos|facebook.com/.*/posts|vidble.com|flickr.com|minus.com|blogspot.com|twitter.com'
+end
---
+++ .config/fish/functions/redarc_video.fish
@@ -0,0 +1,4 @@
+# Defined interactively
+function redarc_video
+    zstdcat (fd -eZST) | jq -r 'select(.score > 7) | .url' | grep -iE 'spankbang.com|xhamster.com|xnxx.com|xvideos.com|pbs.org|tiktok.com|npr.org|dailymotion.com|archive.org|redtube.com|ruleporn.com|youtube.com|youtu.be|youporn.com|pornhub.com|v.redd.it|zenra.net|facebook.com/video|facebook.com/.*/videos|vimeo.com|streamable.com|erome.com'
+end
