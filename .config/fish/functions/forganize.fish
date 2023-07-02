ddebda69d41fe0786defea5211379158a702845a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-07-02 15:26:37 -0500

    forganize

--- .config/fish/functions/forganize.fish
+++ .config/fish/functions/forganize.fish
@@ -12,7 +12,8 @@ function forganize
     fd -tf -d1 --fixed-strings ? . (cat d/.stignore | grep !/ | sed 's|!/\(.*\)|/home/xk/d/\1/|') -x rename ? '' {}
 
     refreshLibrary
-    wt ~/fs/tax.db -l inf --local-media-only -d-0.7 -pfd | xargs -P 20 -n 50 rm {}
+    lb wt --keep-dir /mnt/d/70_Now_Watching/Keep/ ~/lb/fs/tax.db -l inf --local-media-only -d-0.8 -pf | xargs -P 20 -I{} rm {}
+    lb wt --keep-dir /mnt/d/70_Now_Watching/Keep/ ~/lb/fs/tax.db -l inf --local-media-only -d-0.8 -pfd
 
     set audio_dirs ~/d/63_Sounds/ ~/d/81_New_Music/ ~/d/82_Audiobooks/ ~/d/83_ClassicalComposers/ ~/d/85_Inspiration/
     set photo_dirs ~/d/61_Photos_Unsorted/ ~/d/96_Weird_History/ ~/d/94_Cool/ ~/d/93_BG/ ~/d/91_New_Art/ ~/d/98_Me/ ~/d/99_Art/
