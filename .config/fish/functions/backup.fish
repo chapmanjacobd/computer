7938da486c76888c779a440d14a81faea1e3bb07
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-07-02 08:34:47 -0500

    backup

--- .config/fish/functions/backup.fish
+++ .config/fish/functions/backup.fish
@@ -1,14 +1,16 @@
 # Defined via `source`
-function backup --argument source
-    set target (string trim --right --chars=/ "$source")
-    set backup_suffix ".bak"
-    set backup_number 0
+function backup
+    for source in $argv
+        set target (string trim --right --chars=/ "$source")
+        set backup_suffix ".bak"
+        set backup_number 0
 
-    while test -e "$target$backup_suffix$backup_number"
-        set backup_number (math $backup_number + 1)
+        while test -e "$target$backup_suffix$backup_number"
+            set backup_number (math $backup_number + 1)
+        end
+
+        set backup_target "$target$backup_suffix$backup_number"
+
+        cp -r --reflink=auto "$source" "$backup_target"
     end
-
-    set backup_target "$target$backup_suffix$backup_number"
-
-    cp -r --reflink=auto "$source" "$backup_target"
 end
