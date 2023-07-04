2485ead45f196650ce0d2a2cc2c6dbb27848adf2
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-07-04 13:02:57 -0500

    lsof_fuser

--- .config/fish/functions/lsof_fuser.fish
+++ .config/fish/functions/lsof_fuser.fish
@@ -1,6 +1,6 @@
 # Defined via `source`
 function lsof_fuser
     for pid in (fuser -m $argv &| awk '{for(i=3;i<=NF;i++){sub(/c$/,"",$i); print $i}}')
-        lsof -p $pid | awk '$5=="REG" {print}' | grep -v mem
+        lsof -p $pid | awk '$5=="REG" && $4!="mem" {print}'
     end
 end
