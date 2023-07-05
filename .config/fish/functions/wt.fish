04f502dd0c20737f2c1b48797b8bf966215cdac9
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-07-05 10:30:47 -0500

    wt

--- .config/fish/functions/wt.fish
+++ .config/fish/functions/wt.fish
@@ -1,3 +1,3 @@
 function wt
-    /home/xk/.local/bin/lb wt --keep-dir /mnt/d/70_Now_Watching/Keep/ -k delete --local-media-only --prefetch 3 ~/lb/video.db $argv
+    /home/xk/.local/bin/lb wt --keep-dir /mnt/d/70_Now_Watching/Keep/ -k delete --local-media-only ~/lb/video.db $argv
 end
