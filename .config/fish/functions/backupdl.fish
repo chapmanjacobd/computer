d06295973244912751e2ab487f1c8a161db8b39d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-07-08 14:12:09 -0500

    backupdl

--- .config/fish/functions/backupdl.fish
+++ .config/fish/functions/backupdl.fish
@@ -1,6 +1,6 @@
-# Defined interactively
+# Defined via `source`
 function backupdl
-    ~/.local/Downloads/
+    ~/.local/data/
     sshfs -o noauto,noatime,_netdev,reconnect,ConnectTimeout=10,ServerAliveInterval=8,TCPKeepAlive=no xk@pakon:/mnt/d/ ~/.local/data/d/
     sshfs -o noauto,noatime,_netdev,reconnect,ConnectTimeout=10,ServerAliveInterval=8,TCPKeepAlive=no xk@pakon:lb/ ~/.local/data/lb/
     lb dl --image --photos lb/fs/61_Photos_Unsorted.db --prefix d/61_Photos_Unsorted/gallery-dl/
