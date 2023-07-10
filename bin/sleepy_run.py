db21c4fe90406818deb481203c87fd1947af5c48
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-07-09 23:45:28 -0500

    dl sleepy_run

--- bin/sleepy_run.py
+++ bin/sleepy_run.py
@@ -2,20 +2,30 @@
 # ~/bin/sleepy_run.py --trigger "429 Too Many Requests" yes 429 Too Many Requests
 
 import argparse
-import sys
+import random
 import signal
 import subprocess
+import sys
 import time
+
+
+class Actions:
+    sleep = 'sleep'
+    signal = 'signal'
 
 
 def sleep_proc(args, process):
     process.send_signal(signal.SIGTSTP)
     print("Program paused")
 
-    time.sleep(args.time)
+    time.sleep(args.time or (random.uniform(3.0, 10.0) * 60))
 
     process.send_signal(signal.SIGCONT)
     print("Program resumed")
+
+
+def signal_proc(args, process):
+    process.send_signal(args.exit_signal)
 
 
 def sleepy_run(args):
@@ -29,7 +39,8 @@ def sleepy_run(args):
         sys.stdout.buffer.write(line)
         for b in args.trigger:
             if b in line:
-                sleep_proc(args, process)
+                if args.action == 'sleep':
+                    sleep_proc(args, process)
                 break
 
     sys.exit(process.wait())
@@ -37,15 +48,29 @@ def sleepy_run(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Run a program and pause it when a specific error message is encountered."
+        description="Run a program and do something when a specific message is encountered (stdout or stderr)."
     )
-    parser.add_argument("--time", "-T", default=10 * 60, type=int, help="Seconds to sleep for")
+    parser.add_argument(
+        "--action",
+        default=Actions.sleep,
+        choices=[Actions.sleep, Actions.signal],
+        help="Action to take after trigger",
+    )
+    parser.add_argument(
+        "--time", "-T", default=None, type=float, help="Seconds to sleep (for sleep action; default random 3~10 mins)"
+    )
+    parser.add_argument(
+        "--signal", "-s", default="SIGTERM", help="Exit signal to send (for signal action; default SIGTERM)"
+    )
+
     parser.add_argument("--trigger", "-t", action='append', help="Error message to monitor for")
     parser.add_argument("program", nargs="+", help="Program command and arguments")
     args = parser.parse_args()
 
+    args.exit_signal = getattr(signal, args.signal.upper())
+
     if not args.trigger:
-        args.trigger = ['429 Too Many Requests', 'Bad Request', 'Rate limit']
+        args.trigger = ['429 Too Many Requests', '429 Unknown', 'Rate limit']
     args.trigger = [s.encode() for s in args.trigger]
 
     sleepy_run(args)
