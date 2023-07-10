0be02463789e45db0c77bb743a2fc517b4e957b7
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-07-09 21:49:42 -0500

    sqlite-counts

--- .config/fish/functions/sqlite-counts.fish
+++ .config/fish/functions/sqlite-counts.fish
@@ -3,14 +3,12 @@ function sqlite-counts --argument db table col
     set total (sqlite-count $db $table)
 
     set null (sqlite --raw-lines $db "select count(*) from $table where $col is null")
-    printf 'NULL: %s (%s%%)\n' $null (percent $null $total)
-
     set zero (sqlite --raw-lines $db "select count(*) from $table where $col =0")
-    printf '0: %s (%s%%)\n' $zero (percent $zero $total)
-
     set empty (sqlite --raw-lines $db "select count(*) from $table where $col =''")
-    printf "'': %s (%s%%)\n" $empty (percent $empty $total)
-
     set values (math $total-$empty-$zero-$null)
+
     printf "values: %s (%s%%)\n" $values (percent $values $total)
+    printf 'NULL: %s (%s%%)\n' $null (percent $null $total)
+    printf '0: %s (%s%%)\n' $zero (percent $zero $total)
+    printf "'': %s (%s%%)\n" $empty (percent $empty $total)
 end
