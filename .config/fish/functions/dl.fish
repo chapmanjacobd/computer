91d2af36c8364e94d58b19f90df1c4f763fce544
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-07-10 09:22:59 -0500

    dl

--- .config/fish/functions/dl.fish
+++ .config/fish/functions/dl.fish
@@ -1,5 +1,5 @@
 function dl
-    set n (random 1 (filelength /home/xk/.jobs/dl.sh))
+    set n (random 1 (math (filelength /home/xk/.jobs/dl.sh)-1))
     echo $n
     eval (sed -n "$n"p /home/xk/.jobs/dl.sh)
 end
