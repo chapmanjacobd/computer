2535ad03f8a17bb3f1737f31282264a7ebbe2940
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-07-22 02:33:12 -0500

    fdupe

--- .config/fish/functions/fdupe.fish
+++ .config/fish/functions/fdupe.fish
@@ -3,9 +3,8 @@ function fdupe
     fd rmlint -x rm
 
     set audio_dirs ~/d/63_Sounds/ ~/d/81_New_Music/ ~/d/82_Audiobooks/ ~/d/83_ClassicalComposers/ ~/d/85_Inspiration/
-    set photo_dirs ~/d/61_Photos_Unsorted/ ~/d/96_Weird_History/ ~/d/91_New_Art/ ~/d/98_Me/ ~/d/99_Art/
 
-    for dir in $photo_dirs $audio_dirs
+    for dir in $audio_dirs
         if not test -e $dir/rmlint.sh
             $dir/
             # for size in 0-1M 1M-10M 10M
@@ -15,8 +14,13 @@ function fdupe
         end
     end
 
-    ~/d/61_Photos_Unsorted/
-    cbird -i.algos 1 -update
+    set photo_dirs ~/d/61_Photos_Unsorted/ ~/d/96_Weird_History/ ~/d/91_New_Art/ ~/d/98_Me/ ~/d/99_Art/
+    for dir in $photo_dirs
+        $dir/
+        cbird -i.algos 1 -update
+        yes | cbird -dups -first -select-result -nuke
+        # yes | cbird -p.dht 1 -similar -first -select-result -nuke
+    end
 
     ~/d/69_Taxes/
     rmlint --match-extension --progress --xattr --keep-all-tagged ~/d/69_Taxes/ // ~/d/69_Taxes_Keep/
