93f53d89f3cbd389decb139796c5651f5e50f92c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-01 14:03:55 -0500

    fdupe

--- .config/fish/functions/fdupe.fish
+++ .config/fish/functions/fdupe.fish
@@ -1,28 +1,22 @@
 function fdupe
     ~/d/
-    fd rmlint -x rm
-
-    set audio_dirs ~/d/63_Sounds/ ~/d/81_New_Music/ ~/d/82_Audiobooks/ ~/d/83_ClassicalComposers/ ~/d/85_Inspiration/
-
-    for dir in $audio_dirs
-        if not test -e $dir/rmlint.sh
-            $dir/
-            # for size in 0-1M 1M-10M 10M
-            rmlint --match-extension --progress --xattr # --size $size
-            bash rmlint.sh -d && rm rmlint.json
-            # end
-        end
-    end
 
     set photo_dirs ~/d/61_Photos_Unsorted/ ~/d/96_Weird_History/ ~/d/91_New_Art/ ~/d/98_Me/ ~/d/99_Art/
     for dir in $photo_dirs
         $dir/
-        cbird -i.algos 1 -update
-        yes | cbird -dups -select-result -sort-rev resolution -chop -nuke
-        # yes | cbird -p.dht 1 -similar -select-result -sort-rev resolution -chop -nuke
+        czkawka image -d (pwd) > ~/Downloads/(path basename $dir)_dupes
     end
 
-    ~/d/69_Taxes/
-    rmlint --match-extension --progress --xattr --keep-all-tagged ~/d/69_Taxes/ // ~/d/69_Taxes_Keep/
-    bash rmlint.sh -d && rm rmlint.json
+    set audio_dirs ~/d/63_Sounds/ ~/d/81_New_Music/ ~/d/82_Audiobooks/ ~/d/83_ClassicalComposers/ ~/d/85_Inspiration/
+    for dir in $photo_dirs
+        $dir/
+        czkawka music -d (pwd) > ~/Downloads/(path basename $dir)_dupes
+    end
+
+    set video_dirs ~/d/69_Taxes/ ~/d/71_Mealtime_Videos ~/d/72_Education ~/d/73_Entertainment ~/d/75_MovieQueue ~/d/76_CityVideos
+    for dir in $photo_dirs
+        $dir/
+        czkawka video -d (pwd) > ~/Downloads/(path basename $dir)_dupes
+    end
+
 end
