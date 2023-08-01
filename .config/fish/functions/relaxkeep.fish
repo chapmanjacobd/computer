50cfaf55fde4f93f3abf0ba9dcf6ebbde317f689
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-01 17:11:44 -0500

    relax relaxkeep relaxvid relaxvidd

--- .config/fish/functions/relaxkeep.fish
+++ .config/fish/functions/relaxkeep.fish
@@ -1,7 +1,7 @@
 # Defined via `source`
 function relaxkeep
     ~/d/64_Relaxation/keep/
-    mpv --input-ipc-server=/tmp/mpv_socket --af="acompressor=ratio=4,loudnorm" (fd -eOPUS -eOGA -eWEBM . ~/d/64_Relaxation) &
+    mpv --af="acompressor=ratio=4,loudnorm" (fd -eOPUS -eOGA -eWEBM . ~/d/64_Relaxation) &
     feh --hide-pointer --randomize --on-last-slide resume --action1 "/home/xk/.local/bin/trash-put '%f'" -G --auto-zoom --zoom max --draw-tinted --image-bg black --scale-down -D 2 .
     pkill mpv
 end
