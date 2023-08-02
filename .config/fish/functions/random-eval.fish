0d231b4cf672b3ba36257f8980234a6ab952d4d0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-01 23:12:48 -0500

    random-eval

--- .config/fish/functions/random-eval.fish
+++ .config/fish/functions/random-eval.fish
@@ -1,7 +1,7 @@
 # Defined interactively
 function random-eval
     set file (coalesce $argv[1] /dev/stdin)
-    set cmd (shuf -n 1 $file)
+    set cmd (shuf -n 1 $file) $argv[2..-1]
     echo $cmd
     eval $cmd
 end
