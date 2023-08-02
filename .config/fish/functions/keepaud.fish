62e6ac82dde50fb0ff71fba42ecdc5a9f1ca6669
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-01 23:20:03 -0500

    keepaud

--- .config/fish/functions/keepaud.fish
+++ .config/fish/functions/keepaud.fish
@@ -1,7 +1,10 @@
 # Defined interactively
 function keepaud
-    cp "$argv" ~/d/85_Inspiration/ffa/
-    set f (basename "$argv")
-    ffa "$HOME/d/85_Inspiration/ffa/$f"
-    and /bin/rm "$argv"
+    set dest $argv[2]
+    or set dest ~/d/85_Inspiration/ffa/
+
+    cp $argv[1] $dest
+    set f (basename $argv[1])
+    ffa "$dest/$f"
+    and /bin/rm $argv[1]
 end
