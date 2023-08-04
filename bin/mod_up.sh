1f9892fa6ecda0b3316f71cf4f196722f8b6871e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-03 20:23:11 -0500

    mod_up

---
+++ bin/mod_up.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo keyup Control_L | xte
+echo keyup Control_R | xte
+echo keyup Shift_L | xte
+echo keyup Shift_R | xte
+echo keyup Meta_L | xte
+echo keyup Meta_R | xte
+echo keyup Alt_L | xte
+echo keyup Alt_R | xte
+echo keyup Super_L | xte
+echo keyup Super_R | xte
