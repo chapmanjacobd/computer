1765ed254b60b11c82b4e8c6da0e0acbee0889dc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-03 20:14:12 -0500

    grep_percent match_percentage

---
+++ bin/grep_percent.py
@@ -0,0 +1,36 @@
+import sys
+import os
+
+def get_total_lines(file_path):
+    try:
+        with open(file_path) as f:
+            return sum(1 for line in f)
+    except FileNotFoundError:
+        try:
+            return int(file_path)
+        except ValueError:
+            print("Error: The specified argument is neither an integer nor a valid file path.")
+            sys.exit(1)
+
+def convert_to_percentage(total_lines, line_number):
+    percentage = (line_number / total_lines) * 100
+    return f"{percentage:.2f}%"
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <file path or total number of lines>")
+        sys.exit(1)
+
+    total_lines = get_total_lines(sys.argv[1])
+
+    for line in sys.stdin:
+        try:
+            line_number = int(line.split(':')[0])
+            percentage = convert_to_percentage(total_lines, line_number)
+            print(percentage, end=' ')
+            print(line.split(':', 1)[1], end='')
+        except ValueError:
+            print(line, end='')
+
+if __name__ == "__main__":
+    main()
---
+++ bin/match_percentage.py
@@ -0,0 +1,29 @@
+import argparse
+
+def grep_file(file_path, search_term):
+    try:
+        with open(file_path, 'r') as file:
+            total_lines = 0
+            search_count = 0
+            for line in file:
+                total_lines += 1
+                if search_term in line:
+                    search_count += 1
+
+            if total_lines == 0:
+                print("Error: File is empty.")
+            else:
+                percentage = (search_count / total_lines) * 100
+                print(f"The search term '{search_term}' appears in {percentage:.2f}% of the file.")
+
+    except FileNotFoundError:
+        print("Error: File not found.")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Search for a term in a file and print the percentage of occurrences.")
+    parser.add_argument("file_path", help="Path to the file to search.")
+    parser.add_argument("search_term", help="The term to search for.")
+
+    args = parser.parse_args()
+
+    grep_file(args.file_path, args.search_term)
