5cf793c69025dd9e918da50277c93b3dbe460aa5
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-15 11:26:02 -0500

    wkt2geojson

---
+++ bin/wkt2geojson.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import geopandas as gpd
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--s_srs', '-s_srs', default='EPSG:4326', help='SRS of source vector')
+args = parser.parse_args()
+
+wkt = sys.stdin.read()
+
+gdf = gpd.GeoDataFrame(geometry=gpd.GeoSeries.from_wkt([wkt], crs=args.s_srs))
+gdf = gdf.to_crs(epsg=4326)
+
+print(gdf.to_json())
