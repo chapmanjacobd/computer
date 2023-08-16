973ef9bf4a20a68ec1d786b0218a98f262d90f82
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-16 16:16:51 -0500

    daily weekly watch

--- .config/fish/functions/daily.fish
+++ .config/fish/functions/daily.fish
@@ -1,6 +1,7 @@
 function daily
     ~/
     popline ~/.gitignore
+    popline ~/j/.gitignore
 
     rsync -auh --info=progress2 --no-inc-recursive --remove-sent-files backup:.local/Downloads/. ~/d/75_MovieQueue/from_backup/
     lb relmv /mnt/d/70_Now_Watching/Keep/* /mnt/d/77_Library/
@@ -15,12 +16,19 @@ function daily
 
     lb hnadd --oldest ~/lb/hackernews/hn.db
 
-    ~
     sync_history
     # command trash-empty 10 -f
+
+    ~/j/computing/
     library tildes ~/d/30_Computing/tildes.db xk3 --cookies ~/.local/cookies-tildes-net.txt
+    for title in (sqlite --no-headers --raw-lines ~/d/30_Computing/tildes.db 'select path from media' | sed 's|.*\(/\)||')
+        sqlite --no-headers --raw-lines ~/d/30_Computing/tildes.db "select text from media where path like '%$title'" > $title.html
+    end
+    fd -S-200b -tf -x rm
+
     lbdl ~/lb/audio.db --audio --prefix /mnt/d/81_New_Music/
 
+    ~
     if not pgrep ffmpeg >/dev/null
         ~/d/69_Taxes_Keep/ && ffsmallpar
     end
