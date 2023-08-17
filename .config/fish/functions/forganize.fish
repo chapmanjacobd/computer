1df6a6c92fa5907fe224d5b6b4d129cf923d8e6e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-17 08:17:03 -0500

    forganize

--- .config/fish/functions/forganize.fish
+++ .config/fish/functions/forganize.fish
@@ -50,7 +50,7 @@ function forganize
     ~/d/
     yes | bfs -nohidden -type d -exec bfs -f {} -not -type d -exit 1 \; -prune -ok bfs -f {} -type d -delete \;
 
-    for m in /mnt/d1 /mnt/d2 /mnt/d3 /mnt/d4 /mnt/d5 /mnt/d6 /mnt/d7
+    for m in /mnt/d{1,2,3,4,5,6,7,8,9}
         lb fsadd --filesystem ~/lb/fs/d.db $m/*
     end
 end
