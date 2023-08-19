1cd115b07881b55dc6c2df58b26e0d095eee53e5
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-19 01:18:58 -0500

    coln lsodl

--- .config/fish/functions/coln.fish
+++ .config/fish/functions/coln.fish
@@ -1,5 +1,10 @@
+# Defined interactively
 function coln
-    while read -l input
-        echo $input | awk '{print $'$argv[1]'}'
+    set col $argv[1]
+    if test $col -gt 0
+        awk '{print $'$argv[1]'}'
+    else if test $col -lt 0
+        awk 'NF && NF'$col' { print ( $(NF'$col') ) }'
     end
+    awk '{print $(NF)}'
 end
---
+++ .config/fish/functions/lsodl.fish
@@ -0,0 +1,4 @@
+# Defined interactively
+function lsodl
+    repeat eval "lsof /mnt/d/ | grep -v 'r  '| coln 0 | sed 's|.part.*||' ; sleep 1" | dedupe.py
+end
