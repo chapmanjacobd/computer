ea9a0108d3248036b51d91b6c4d96a1cb1256d13
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-25 18:57:17 -0500

    dockerpatch

--- .config/fish/functions/dockerpatch.fish
+++ .config/fish/functions/dockerpatch.fish
@@ -2,7 +2,7 @@
 function dockerpatch
     docker run -it -v (pwd):/wd $argv[1] /bin/bash
 
-    set container_id (docker container ls -lq)
+    set container_id (docker ps -aqlf ancestor=$argv[1])
     docker commit $container_id $argv[2]
     docker push $argv[2]
 end
