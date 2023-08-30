cd0231020f7642fe8116d69a7a577a263b3963e7
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-30 12:53:19 -0500

    ar

--- .config/fish/functions/ar.fish
+++ .config/fish/functions/ar.fish
@@ -1,5 +1,5 @@
 # Defined in /tmp/fish.sUt5Y8/ar.fish @ line 2
 function ar
     set target (string replace '/' '' $argv)
-    zip -qr $target.zip $target; and trash-put $target
+    zip -qr "$target".zip "$target"; and trash-put "$target"
 end
