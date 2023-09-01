ee79df3fc05cf7b6d035e0661d44d571dbea2c2f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-09-01 15:39:38 -0500

    remove_empty_directories

--- .config/fish/functions/remove_empty_directories.fish
+++ .config/fish/functions/remove_empty_directories.fish
@@ -3,5 +3,5 @@ function remove_empty_directories
     if set -q argv[1]
         $argv
     end
-    yes | bfs -nohidden -type d -exec bfs -f {} -not -type d -exit 1 \; -prune -ok bfs -f {} -type d -delete \;
+    yes | bfs -type d -exec bfs -f {} -not -type d -exit 1 \; -prune -ok bfs -f {} -type d -delete \;
 end
