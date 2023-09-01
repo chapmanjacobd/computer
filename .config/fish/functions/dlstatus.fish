9e4a513be0bcba9d332c0fab49016cbca7b55fbf
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-08-31 20:43:30 -0500

    dlstatus

--- .config/fish/functions/dlstatus.fish
+++ .config/fish/functions/dlstatus.fish
@@ -1,7 +1,9 @@
 # Defined interactively
-function dlstatus --argument db
-    lb wt -w time_downloaded\>(timestamp 24 hours ago) -pa $db
-    lb ds $db
-    lb history --freqency weekly downloaded $db
+function dlstatus
+    for db in $argv
+        lb wt -w time_downloaded\>(timestamp 24 hours ago) -pa $db
+        lb ds $db
+        lb history --freqency weekly downloaded $db
+    end
     # lsodl
 end
