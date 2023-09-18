c26fd431e8af09d569570b3785ab6e262248e39a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-09-18 06:43:33 -0500

    log logeach

---
+++ .config/fish/functions/logeach.fish
@@ -0,0 +1,11 @@
+# Defined via `source`
+function logeach
+    for s in (systemctl list-unit-files --type service --state enabled --no-legend --no-pager | cut -f1 -d' ') 
+        log -u $s
+    end
+
+    for s in (systemctl list-unit-files --user --type service --no-legend --no-pager | cut -f1 -d' ')
+        log --user -u $s
+    end
+
+end
