9da896548fd9547f139f7a2b9048b0286058dad0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-09-18 22:28:09 -0500

    diskstatus

--- .config/fish/functions/diskstatus.fish
+++ .config/fish/functions/diskstatus.fish
@@ -6,15 +6,20 @@ function diskstatus
         echo ---
         sudo btrfs fi usage -T $mnt
         echo
-        sudo btrfs device stats -T $mnt
-        echo
-        sudo btrfs scrub status $mnt
-        echo ---
-        echo
+    end
+
+    for mnt in / /mnt/d{1,2,3,4,5,6,7,8,9}
+        sudo btrfs device stats $mnt
     end
 
     smartls Power_On_Hours Power_Cycle_Count Load_Cycle_Count UDMA_CRC_Error_Count
     smartls Reallocated_Sector_Ct Reallocated_Event_Count Current_Pending_Sector Offline_Uncorrectable
+
+    for mnt in / /mnt/d{1,2,3,4,5,6,7,8,9}
+        if not sudo btrfs scrub status $mnt | grep -q 'no errors'
+            sudo btrfs scrub status $mnt
+        end
+    end
 
     lb mu /mnt/d{1,2,3,4,5,6,7,8,9}
 end
