63ecee4da54fb439ea3151c8c577089d5502f494
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-09-20 08:54:37 -0500

    abbrsave gitignore detectstillimagevideo

--- .config/fish/functions/abbrsave.fish
+++ .config/fish/functions/abbrsave.fish
@@ -1,7 +1,7 @@
-# Defined via `source`
+# Defined interactively
 function abbrsave
     abbr -a -- $argv[1] $argv[2..-1]
 
-    filterfile ~/.config/fish/abbreviations "abbr -a -- $argv[1] "
-    echo abbr -a -- $argv[1] (string escape -- $argv[2..-1]) >>~/.config/fish/abbreviations
+    filterfile $__fish_config_dir/abbreviations "abbr -a -- $argv[1] "
+    echo abbr -a -- $argv[1] (string escape -- $argv[2..-1]) >>$__fish_config_dir/abbreviations
 end
