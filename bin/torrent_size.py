9108d73e27f0a56a412c960f06e5db18b78b10f2
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-09-21 08:58:43 -0500

    pip_installed torrent_size

---
+++ bin/torrent_size.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+from pathlib import Path
+from torrentool.api import Torrent
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('paths', nargs='+', help='Path(s) to torrent files')
+parser.add_argument('-r', '--remove', action='store_true', help='Actually remove files')
+args = parser.parse_args()
+
+for torrent_file in args.paths:
+    torrent_file = Path(torrent_file)
+    if torrent_file.stem.endswith('_2'):
+        original_file = torrent_file.with_name(torrent_file.name.replace('_2', ''))
+        if original_file.exists():
+            torrent1 = Torrent.from_file(original_file)
+            torrent2 = Torrent.from_file(torrent_file)
+            if sum(f.length for f in torrent1.files) < sum(f.length for f in torrent2.files):
+                if args.remove:
+                    os.remove(torrent_file)
+                else:
+                    print(torrent_file)
+            else:
+                if args.remove:
+                    os.remove(original_file)
+                else:
+                    print(original_file)
