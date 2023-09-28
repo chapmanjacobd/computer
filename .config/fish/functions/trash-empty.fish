481eb4104bd4d7faefe5c47c0164bbefc9e7803a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-09-28 18:32:02 -0500

    trash-empty

--- .config/fish/functions/trash-empty.fish
+++ .config/fish/functions/trash-empty.fish
@@ -6,25 +6,25 @@ function trash-empty
     end
 
     trash-size
-    trash-list | tee -a ~/.local/share/trashed.txt
+    trash-list >> ~/.local/share/trashed.txt
 
-    for mnt in $argv
-        for d in $mnt/.Tras*/*/files/
-            NO_COLOR=1 ncdu "$d"
-        end
-    end
+    # for mnt in $argv
+    #    for d in $mnt/.Tras*/*/files/
+    #        NO_COLOR=1 ncdu "$d"
+    #    end
+    # end
 
     if gum confirm --default=no 'Empty trash?'
         command trash-empty -f
     end
 
-    if gum confirm --default=no 'Refresh snapshots?'
+    # if gum confirm --default=no 'Refresh snapshots?'
         for mnt in $argv
             btrfs_nocheck_delete_snapshot $mnt
             #kitty fish -c "sleep (random_thousanths 0 5000); btrfs_check_delete_snapshot $mnt" &
         end
         wait
-    end
+    # end
 
 
 end
