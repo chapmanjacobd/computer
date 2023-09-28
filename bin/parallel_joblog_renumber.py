eb606d479f8b9fde4024b71ffffb3f9f0dbeff03
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-09-28 15:57:00 -0500

    parallel_joblog_renumber

---
+++ bin/parallel_joblog_renumber.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import sys
+import argparse
+
+
+def pipe_lines(x) -> None:
+    try:
+        sys.stdout.writelines(x)
+    except BrokenPipeError:
+        sys.stdout = None
+        sys.exit(141)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("joblog", type=argparse.FileType("r"))
+parser.add_argument("argsfile", nargs="?", type=argparse.FileType("r"), default=sys.stdin)
+parser.add_argument("output_path", nargs="?")
+args = parser.parse_args()
+
+joblog = args.joblog.readlines()
+args.joblog.close()
+
+argsfile = args.argsfile.readlines()
+args.argsfile.close()
+
+
+new_joblog = ['Seq	Host	Starttime	JobRuntime	Send	Receive	Exitval	Signal	Command']
+seq = 1
+for argline in argsfile:
+    for jobline in joblog:
+        if argline.strip() in jobline:
+            parts = jobline.split('\t')
+            parts[0] = str(seq)
+            new_joblog.append("\t".join(parts))
+    seq += 1
+
+if args.output_path:
+    with open(args.output_path, "w") as output_fd:
+        output_fd.writelines(new_joblog)
+else:
+    pipe_lines(new_joblog)
