edac99b570030e062001e244e4d435087602d9fb
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-09-29 20:40:24 -0500

    ls input cargo_installed dnf_installed

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,10 +1,10 @@
 # Defined interactively
-function ls --description 'An alias for exa with some defaults'
+function ls -w eza
     if status --is-interactive
         if test -d .git
-            exa --git --long --header --classify --sort=type --group-directories-first $argv
+            eza --git --long --header --classify --sort=type --group-directories-first $argv
         else
-            exa --long --header --classify --sort=none --group-directories-first $argv
+            eza --long --header --classify --sort=none --group-directories-first $argv
         end
     else
         /usr/bin/ls --sort=none $argv
