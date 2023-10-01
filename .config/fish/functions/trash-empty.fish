601208b42d8f1e7daa1ef57c60d952343f9871f6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-01 08:10:05 -0500

    bass trash-empty gitignore

--- .config/fish/functions/trash-empty.fish
+++ .config/fish/functions/trash-empty.fish
@@ -6,7 +6,7 @@ function trash-empty
     end
 
     trash-size
-    trash-list >> ~/.local/share/trashed.txt
+    trash-list >>~/.local/share/trashed.txt
 
     # for mnt in $argv
     #    for d in $mnt/.Tras*/*/files/
@@ -17,13 +17,13 @@ function trash-empty
     if gum confirm --default=no 'Empty trash?'
         command trash-empty -f
 
-    # if gum confirm --default=no 'Refresh snapshots?'
+        # if gum confirm --default=no 'Refresh snapshots?'
         for mnt in $argv
             btrfs_nocheck_delete_snapshot $mnt
             #kitty fish -c "sleep (random_thousanths 0 5000); btrfs_check_delete_snapshot $mnt" &
         end
         wait
-    # end
+        # end
     end
 
 end
