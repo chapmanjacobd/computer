e0c7fff447e266b81326c29f5cce832cf07d9b1b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-02 23:14:12 -0500

    sleepmusic catt_default_night

--- .config/fish/functions/sleepmusic.fish
+++ .config/fish/functions/sleepmusic.fish
@@ -1,7 +1,7 @@
 # Defined interactively
 function sleepmusic
-    catt -d "Bedroom pair" set_default
-    fish -c '~/lb/ && lb listen -L 99 -cast -cast-to "Bedroom pair" -s relax' &
+    catt -d "Bedroom" set_default
+    fish -c '~/lb/ && lb listen -L 99 -cast -cast-to "Bedroom" -s relax' &
     sleep 8
     catt volume 25
     sleep 2700
