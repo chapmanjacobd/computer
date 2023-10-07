1064344f7c734a903911e9e185a82ca17d676922
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-06 22:10:38 -0500

    monthly

--- .config/fish/functions/monthly.fish
+++ .config/fish/functions/monthly.fish
@@ -2,6 +2,6 @@ function monthly
     git gc --prune=now
     sudo btrfs filesystem defragment -r ~/lb/ ~/m/ ~/mc/ ~/j/ ~/.git/
     noswap fish -c forganize
-    noswap fish -c fdupe
+    # noswap fish -c fdupe
     sudo btrfs filesystem defragment -r ~/lb/
 end
