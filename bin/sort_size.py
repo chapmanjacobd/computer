999878a97bd3a0d213ec2bf0bea97a3030ae79ad
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-12 08:35:57 -0500

    sort_size

---
+++ bin/sort_size.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+import os
+
+
+def pipe_lines(x) -> None:
+    try:
+        sys.stdout.writelines(x)
+    except BrokenPipeError:
+        sys.stdout = None
+        sys.exit(141)
+
+
+def main():
+    paths = [line.strip() for line in sys.stdin]
+    filtered_paths = filter(os.path.exists, paths)
+    sorted_paths = sorted(filtered_paths, key=os.path.getsize, reverse=True)
+    pipe_lines('\n'.join(sorted_paths))
+
+
+if __name__ == "__main__":
+    main()
