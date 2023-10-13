564ef7dfbd63f4c2bbaf35778afffb57dfce8482
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-13 16:08:09 -0500

    copy_to_pakon

--- .config/fish/functions/copy_to_pakon.fish
+++ .config/fish/functions/copy_to_pakon.fish
@@ -3,6 +3,7 @@ function copy_to_pakon
     rsync -auh --info=progress2 --no-inc-recursive --remove-sent-files backup:d/_tixati/ ~/d/75_Moviequeue/from_backup/
 
     ~/d/75_Moviequeue/from_backup/
-    fd '[a-zA-Z]{3,5}-?[0-9]{3}' -x mv {} /mnt/d/69_Taxes/onejav/
+    fd '[a-zA-Z]{2,5}-?[0-9]{3}' -x mv {} /mnt/d/69_Taxes/onejav/
     fd '^[a-zA-Z]{3,}-[0-9]{3,5}' -x mv {} /mnt/d/69_Taxes/onejav/
+    remove_empty_directories .
 end
