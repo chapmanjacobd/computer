00991fb8cf8636708a84ee1b6a8270d3d33d5af3
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-14 15:54:39 -0500

    gitignore

--- .config/fish/functions/gitignore.fish
+++ .config/fish/functions/gitignore.fish
@@ -1,5 +1,9 @@
 # Defined via `source`
 function gitignore
     echo "$argv" | cat - .gitignore | sponge .gitignore
-    git reset :/"$argv"
+    git reset -- :/"$argv"
+    if gum confirm --default=no
+        git rm --cached "$argv"
+        git commit -m wip
+    end
 end
