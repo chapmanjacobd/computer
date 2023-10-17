b1700a31933345cd6363cc83e908c322e828c058
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-17 16:32:32 -0500

    lbrelease

--- .config/fish/functions/lbrelease.fish
+++ .config/fish/functions/lbrelease.fish
@@ -8,7 +8,7 @@ function lbrelease --argument oldver newver
     rg db.execute
 
     lbformat
-    python .github/readme.py >.github/README.md
+    python -m xklb.readme > .github/README.md
     git reset tests/cassettes/
     git restore tests/cassettes/
     if wipm $newver
