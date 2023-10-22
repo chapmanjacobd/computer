abf6f712dfb410b2a1fb6ac2ece13e5c91ca82c7
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-22 17:31:56 -0500

    wtm

---
+++ .config/fish/functions/wtm.fish
@@ -0,0 +1,6 @@
+# Defined interactively
+function wtm
+    focus_under_mouse
+    python -m xklb.lb watch --action ask_move_or_delete --keep-dir /home/xk/d/69_Taxes_Keep/ --prefetch 2 --local -u size desc -S+700M -C --sort-by 'deleted/played desc, deleted desc' --exit-code-confirm -i -m 4 $argv
+    focus_follows_mouse
+end
