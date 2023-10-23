053a96583e2f5ed69222e51ed3ba470886253dfd
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-23 14:14:44 -0500

    spongebob

--- .config/fish/functions/spongebob.fish
+++ .config/fish/functions/spongebob.fish
@@ -1,5 +1,5 @@
 # Defined interactively
 function spongebob --argument dest
     # the unique sponge -- har har
-    cat - $dest | sponge $dest
+    cat - $dest | awk '!seen[$0]++' | sponge $dest
 end
