092439ccc089f1997de31d65f5453d285e9da967
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-23 14:48:57 -0500

    copy_to_pakon stickysync_backup

---
+++ .config/fish/functions/stickysync_backup.fish
@@ -0,0 +1,10 @@
+# Defined via `source`
+function stickysync_backup --argument historyfile from to
+    set new_files (mktemp)
+    combine (ssh backup "cd $from && fd -S+1b --changed-before '24 hours'" | psub) not $historyfile >$new_files
+    cat $new_files | while read line
+        rsync -r --files-from=(echo $line | psub) backup:$from $to
+        and echo $line >>$historyfile
+        or echo failed $line
+    end
+end
