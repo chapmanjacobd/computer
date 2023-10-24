327d4639855104d6715f9510336f220db100d414
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-24 09:23:07 -0500

    copy_to_pakon

--- .config/fish/functions/copy_to_pakon.fish
+++ .config/fish/functions/copy_to_pakon.fish
@@ -5,8 +5,8 @@ function copy_to_pakon
     stickysync_backup ~/d/00_Metadata/stickysync_rtorrent_audiobooks d/_rtorrent/ /mnt/d/82_Audiobooks/rtorrent/
     rsync -auh --info=progress2 --no-inc-recursive --remove-sent-files ~/Downloads/*.torrent backup:.local/data/rtorrent/watch/start/
 
-    ~/d/75_Moviequeue/from_backup/
-    fd '[a-zA-Z]{2,5}-?[0-9]{3}' -x mv {} /mnt/d/69_Taxes/onejav/
-    fd '^[a-zA-Z]{3,}-[0-9]{3,5}' -x mv {} /mnt/d/69_Taxes/onejav/
-    remove_empty_directories .
+    # ~/d/75_Moviequeue/from_backup/
+    # fd '[a-zA-Z]{2,5}-?[0-9]{3}' -x mv {} /mnt/d/69_Taxes/onejav/
+    # fd '^[a-zA-Z]{3,}-[0-9]{3,5}' -x mv {} /mnt/d/69_Taxes/onejav/
+    # remove_empty_directories .
 end
