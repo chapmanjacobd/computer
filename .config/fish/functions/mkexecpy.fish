19b95c9448b318c4226d5a93e2936fa117ecbd95
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-25 11:48:57 -0500

    mkexecpy

--- .config/fish/functions/mkexecpy.fish
+++ .config/fish/functions/mkexecpy.fish
@@ -1,7 +1,7 @@
 # Defined via `source`
 function mkexecpy
     for f in $argv
-        if not grep -q '^#!/' $f
+        if not grep -q '^#!' $f
             echo "#!/usr/bin/python3" | cat - $f | sponge $f
         end
 
