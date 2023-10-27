c4fcf7748ed29bf95c6364f30f1345a4a7b4d761
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-27 01:53:09 -0500

    gitignore LICENSE README adb-channel adb-sync human_size_sum text_files_similarity

---
+++ bin/human_size_sum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+import humanize
+from xklb.utils import nums
+
+total = 0
+for line in sys.stdin:
+    total += float(nums.human_to_bytes(line))
+
+print(f"Total: {humanize.naturalsize(total)}")
--- bin/text_files_similarity.py
+++ bin/text_files_similarity.py
@@ -5,12 +5,13 @@ import math
 import sys
 from collections import Counter
 
+
 def get_cosine(vec1, vec2):
     intersection = set(vec1.keys()) & set(vec2.keys())
     numerator = sum([vec1[x] * vec2[x] for x in intersection])
 
-    sum1 = sum([vec1[x]**2 for x in vec1.keys()])
-    sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+    sum1 = sum([vec1[x] ** 2 for x in vec1.keys()])
+    sum2 = sum([vec2[x] ** 2 for x in vec2.keys()])
     denominator = math.sqrt(sum1) * math.sqrt(sum2)
 
     if not denominator:
@@ -20,12 +21,14 @@ def get_cosine(vec1, vec2):
 
     return returnval
 
+
 def file_words(a):
     text = []
     for line in open(a).readlines():
         for word in line.strip().split():
             text.append(word)
     return text
+
 
 for a, b in itertools.combinations(sys.argv[1:], 2):
     similarity = get_cosine(Counter(file_words(a)), Counter(file_words(b)))
