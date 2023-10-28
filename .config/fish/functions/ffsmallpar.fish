b398e8a0ad8252e9d2969e5b1a60f66982c39dd0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-27 23:14:23 -0500

    ffsmallpar

--- .config/fish/functions/ffsmallpar.fish
+++ .config/fish/functions/ffsmallpar.fish
@@ -1,4 +1,4 @@
-# Defined interactively
+# Defined in /home/xk/.config/fish/functions/ffsmallpar.fish @ line 2
 function ffsmallpar
     if not create_lock_file ffsmallpar
         echo "ffsmallpar already running..."
@@ -15,13 +15,12 @@ function ffsmallpar
     ssh -fN pulse15 # workaround for VisualHostKey
     ssh -fN backup
     cat $tmpfile | timeout -s HUP 18h parallel --sshloginfile ~/.parallel/sshloginfile.ffmpeg --transfer "ffmpeg -nostdin -hide_banner -dn -y -i {} -vf 'scale=-2:min(ih\,1440)' -vcodec libx265 -preset 4 -acodec libopus -b:a 96k {.}.small.mkv && rm {} && rsync -auh --remove-sent-files {.}.small.mkv" $hostname:(pwd)
-    and cat $tmpfile | parallel rm || true
-    or for f in (cat $tmpfile)
+
+    for f in (cat $tmpfile)
         if test -e $f -a -e (path change-extension small.mkv $f)
             trash-put $f
         end
     end
-
     ssh backup fd -eMKV -eMP4 -d1 -x trash
     ssh pulse15 fd -eMKV -eMP4 -d1 -x trash
 
