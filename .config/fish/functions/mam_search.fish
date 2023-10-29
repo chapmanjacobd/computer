a686a74ea83f80075e32f0076c16c48fa6af9b04
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-29 14:24:31 -0500

    mam_search

--- .config/fish/functions/mam_search.fish
+++ .config/fish/functions/mam_search.fish
@@ -1,10 +1,12 @@
 # Defined via `source`
 function mam_search
+    filter_opts $argv
+
     load_env_mam
-    set db ~/d/23_Linkmining/mam/(string replace -a ' ' _ $argv).db
+    set db ~/d/23_Linkmining/mam/(string replace -a ' ' _ $args).db
 
     ~/lb/
-    python -m xklb.scratch.mam_search --description --audiobooks --books --comics --radio --cookie $MAM_COOKIE "$db" $argv
+    python -m xklb.scratch.mam_search --audiobooks --books --comics --radio $opts --cookie $MAM_COOKIE "$db" $args
 
     sqlite --no-headers --raw-lines "$db" 'select id from media where vip=1 and seeders>0' | sed "s|^|curl -OJs -b mam_id=$MAM_COOKIE https://www.myanonamouse.net/tor/download.php?tid=|" | parallel -S backup -j4 '.local/data/rtorrent/watch/vip_new/; eval {}'
     sqlite --no-headers --raw-lines "$db" 'select id from media where vip=0 and seeders>0' | sed "s|^|curl -OJs -b mam_id=$MAM_COOKIE https://www.myanonamouse.net/tor/download.php?tid=|" | parallel -S backup -j4 '.local/data/rtorrent/watch/nonvip_new/; eval {}'
