901399be2d81e31615846900363673dfd60d98e2
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-29 09:14:52 -0500

    relaxvid relaxvidd wt-dev wt

--- .config/fish/functions/wt-dev.fish
+++ .config/fish/functions/wt-dev.fish
@@ -1,5 +1,5 @@
 # Defined interactively
 function wt-dev
     ~/lb/
-    python -m xklb.lb watch --keep-dir /mnt/d/70_Now_Watching/keep/ -k delete video.db -v $argv
+    python -m xklb.lb watch --keep-dir /mnt/d/77_Library/ -k delete video.db -v $argv
 end
