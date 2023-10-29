2e1868f4f0fdc8a10765d91bb89af6846b9b3b4d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-29 14:22:56 -0500

    filter_opts

--- .config/fish/functions/filter_opts.fish
+++ .config/fish/functions/filter_opts.fish
@@ -1,16 +1,5 @@
-# Defined interactively
-function filter_opts
-    set -l filtered_opts
-    set -l new_argv
-
-    for opt in $argv
-        if string match -r -- '-.*' $opt
-            set -a filtered_opts $opt
-        else
-            set -a new_argv $opt
-        end
-    end
-
-    set -g argv $new_argv
-    echo $filtered_opts
+# Defined via `source`
+function filter_opts --no-scope-shadowing
+    set opts (string match  -- '-*' $argv)
+    set args (string match --invert -- '-*' $argv)
 end
