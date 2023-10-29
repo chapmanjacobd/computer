3894cd1e287bf76fde7e3102b3ba5ed3a0573885
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-29 09:47:40 -0500

    near_duplicates

---
+++ bin/near_duplicates.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+import difflib
+from typing import List
+
+prev_lines: List[str] = []
+for curr in sys.stdin:
+    curr = curr.strip()
+
+    is_duplicate = False
+    for prev in prev_lines:
+        if difflib.SequenceMatcher(None, curr, prev).ratio() > 0.8:
+            print(prev)
+            print(curr)
+            is_duplicate = True
+            break
+
+    if not is_duplicate:
+        prev_lines.append(curr)
