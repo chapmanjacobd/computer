a951c030f02ee8f2cba856c75669ae2059216a1c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-30 14:53:23 -0500

    torrent_promote

--- bin/torrent_promote.py
+++ bin/torrent_promote.py
@@ -3,16 +3,23 @@
 import argparse
 import shutil
 from pathlib import Path
+from typing import List, Tuple
 
 import humanize
 from torrentool.api import Torrent
 
 
 def sort_and_move_torrents(args):
-    torrent_folder = Path(args.torrent_folder)
+    paths: List[Path] = []
+    for p in args.paths:
+        p = Path(p)
+        if p.is_dir():
+            paths.extend(p.glob("*.torrent"))
+        else:
+            paths.append(p)
 
-    torrent_data = []
-    for torrent_file in (Path(s) for s in args.paths) or torrent_folder.glob('*.torrent'):
+    torrent_data: List[Tuple[Path, int]] = []
+    for torrent_file in paths:
         torrent = Torrent.from_file(torrent_file)
         total_size = sum(f.length for f in torrent.files)
         torrent_data.append((torrent_file, total_size))
@@ -20,7 +27,7 @@ def sort_and_move_torrents(args):
     sorted_torrents = sorted(torrent_data, key=lambda x: x[1], reverse=args.reverse)
 
     for torrent_file, size in sorted_torrents[: args.n]:
-        destination_path = torrent_folder / '..' / 'start' / torrent_file.name
+        destination_path = torrent_file.parent / '..' / 'start' / torrent_file.name
 
         if args.dry_run:
             print('mv', torrent_file, ' ', destination_path, '# ', humanize.naturalsize(size, binary=True))
@@ -30,11 +37,11 @@ def sort_and_move_torrents(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('torrent_folder', help='Folder containing torrent files')
     parser.add_argument('-n', type=int, default=20, help='Number of torrents to move')
     parser.add_argument('--reverse', '-r', action='store_true')
     parser.add_argument('--dry-run', '-p', action='store_true')
-    parser.add_argument('paths', nargs='*')
-    args = parser.parse_intermixed_args()
+
+    parser.add_argument('paths', nargs='+')
+    args = parser.parse_args()
 
     sort_and_move_torrents(args)
