70b8d8bb6398d55c5017ac103d1e1a8fbaf117e2
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-10-30 15:44:28 -0500

    copy_to_pakon

--- .config/fish/functions/copy_to_pakon.fish
+++ .config/fish/functions/copy_to_pakon.fish
@@ -5,6 +5,7 @@ function copy_to_pakon
     stickysync_backup ~/d/00_Metadata/stickysync_rtorrent_audiobooks d/_rtorrent/ /mnt/d/82_Audiobooks/rtorrent/
     rsync -auh --info=progress2 --no-inc-recursive --remove-sent-files ~/Downloads/*.torrent backup:.local/data/rtorrent/watch/new/
     ssh backup torrent_promote.py .local/data/rtorrent/watch/vip_bbc/ --reverse -n 50
+    ssh backup 'systemctl --user stop vpn_oracle.service; sleep (minutes 20); systemctl --user restart vpn_oracle.service'
 
     # ~/d/75_Moviequeue/from_backup/
     # fd '[a-zA-Z]{2,5}-?[0-9]{3}' -x mv {} /mnt/d/69_Taxes/onejav/
