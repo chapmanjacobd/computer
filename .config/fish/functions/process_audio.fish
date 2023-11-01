b24570b05a7f0f34120ce7c47bf35075c894bc01
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-01 15:13:57 -0500

    process_audio

--- .config/fish/functions/process_audio.fish
+++ .config/fish/functions/process_audio.fish
@@ -1,5 +1,5 @@
 # Defined interactively
 function process_audio
-    fd . $argv -H -tf -eDTS -eAAC -eWAV -eAIF -eAIFF -eFLAC -eAIFF -eM4A -eMP3 -eOGG -eMP4 -eWMA -j6 -x process_audio.py 
+    fd . $argv -H -tf -eDTS -eAAC -eWAV -eAIF -eAIFF -eFLAC -eAIFF -eM4A -eMP3 -eOGG -eMP4 -eWMA -j6 -x process_audio.py
     #if string match -q --ignore-case "*album*" "{.}"; and test (duration "{.}.opus") -gt 2280; split_by_silence "{.}.opus"; end;
 end
