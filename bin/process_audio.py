7eed9bc092a53691cc761305e24feffa20a900f1
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-01 14:50:16 -0500

    process_audio

---
+++ bin/process_audio.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+import argparse
+import json
+import os
+import shlex
+import subprocess
+from pathlib import Path
+from typing import List
+
+parser = argparse.ArgumentParser()
+parser.add_argument('paths', nargs='+')
+args = parser.parse_args()
+
+for path in args.paths:
+    ffprobe_cmd = ['ffprobe', '-v', 'error', '-print_format', 'json', '-show_format', '-show_streams', path]
+    result = subprocess.run(ffprobe_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    info = json.loads(result.stdout)
+
+    channels = info['streams'][0]['channels']
+    bitrate = int(info['format']['bit_rate'])
+    source_rate = int(info['streams'][0]['sample_rate'])
+
+    assert bitrate > 0
+    assert channels > 0
+    assert source_rate > 0
+
+    ff_opts: List[str] = []
+    if channels == 1:
+        opus_channels = '1'
+        ff_opts.extend(['-ac 1'])
+    else:
+        ff_opts.extend(['-ac 2'])
+
+    if bitrate >= 256000:
+        ff_opts.extend(['-b:a 128k'])
+    else:
+        ff_opts.extend(['-b:a 64k', '-frame_duration 40'])
+
+    if source_rate >= 44100:
+        opus_rate = 48000
+    elif source_rate >= 22050:
+        opus_rate = 24000
+    else:
+        opus_rate = 16000
+    ff_opts.extend([f"-ar {opus_rate}"])
+
+    new_path = str(Path(path).with_suffix('.opus'))
+    cmd = f'ffmpeg -nostdin -hide_banner -loglevel warning -y -i {shlex.quote(path)} -c:a libopus {" ".join(ff_opts)} -vbr constrained -filter:a loudnorm=i=-18:lra=17 {shlex.quote(new_path)}'
+
+    subprocess.check_call(cmd, shell=True)
+
+    # Remove original
+    os.remove(path)
