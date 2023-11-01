080eaeaf2fdd00d4ec5740e8c9a9e44105d3440e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-01 16:18:03 -0500

    copy_to_pakon

--- .config/fish/functions/copy_to_pakon.fish
+++ .config/fish/functions/copy_to_pakon.fish
@@ -4,11 +4,12 @@ function copy_to_pakon
 
     stickysync_backup ~/d/00_Metadata/stickysync_rtorrent_audiobooks d/_rtorrent/ /mnt/d/82_Audiobooks/rtorrent/
     rsync -auh --info=progress2 --no-inc-recursive --remove-sent-files ~/Downloads/*.torrent backup:.local/data/rtorrent/watch/new/
-    ssh backup torrent_promote.py .local/data/rtorrent/watch/vip_bbc/ --reverse -n 50
-    ssh backup 'systemctl --user stop vpn_oracle.service; sleep (minutes 45); systemctl --user restart vpn_oracle.service'
 
-    # ~/d/75_Moviequeue/from_backup/
-    # fd '[a-zA-Z]{2,5}-?[0-9]{3}' -x mv {} /mnt/d/69_Taxes/onejav/
-    # fd '^[a-zA-Z]{3,}-[0-9]{3,5}' -x mv {} /mnt/d/69_Taxes/onejav/
-    # remove_empty_directories .
+    set max 50
+    for dir in new/ vip_new/ vip_bbc/ nonvip_new/ bbc_nonfree/ lowseeds_small/
+        set filled (ssh backup torrent_promote.py .local/data/rtorrent/watch/$dir --reverse -n $max | count)
+        set max (math $max-$filled)
+        echo $dir $filled $max
+    end
+    ssh backup 'systemctl --user stop vpn_oracle.service; sleep (minutes 45); systemctl --user restart vpn_oracle.service'
 end
