372bf3238991f9b31d0badec112c0a22d26fe1cd
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-03 16:50:25 -0500

    archiveurl

--- .config/fish/functions/archiveurl.fish
+++ .config/fish/functions/archiveurl.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function archiveurl
     for url in $argv
-        waybackpy -s --archive-url --url $url
+        waybackpy -s --archive-url --url $url | grep http
     end
 end
