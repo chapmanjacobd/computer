240d6a0b6e34d545b13235e66aa69f54cf0626a6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-05 20:05:47 -0600

    hourly

--- .config/fish/functions/hourly.fish
+++ .config/fish/functions/hourly.fish
@@ -7,13 +7,15 @@ function hourly
 
     ~/lb/
     set max (python -m xklb.scratch.mam_slots --cookie $MAM_COOKIE)
-    for dir in new/ vip_new/ vip_bbc/
-        set filled (ssh backup torrent_promote.py .local/data/rtorrent/watch/$dir --reverse -n $max | count)
-        set max (math $max-$filled)
-    end
-    for dir in nonvip_new/ bbc_nonfree/ lowseeds_small/
-        set filled (ssh backup torrent_promote.py .local/data/rtorrent/watch/$dir -n $max | count)
-        set max (math $max-$filled)
+    if test $max -gt 0
+        for dir in new/ vip_new/ vip_bbc/
+            set filled (ssh backup torrent_promote.py .local/data/rtorrent/watch/$dir --reverse -n $max | count)
+            set max (math $max-$filled)
+        end
+        for dir in nonvip_new/ bbc_nonfree/ lowseeds_small/
+            set filled (ssh backup torrent_promote.py .local/data/rtorrent/watch/$dir -n $max | count)
+            set max (math $max-$filled)
+        end
     end
 
 end
