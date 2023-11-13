3ec1a914a1838bbec59cc323a5cb92227da59163
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-13 00:08:34 -0600

    pchildren

--- .config/fish/functions/pchildren.fish
+++ .config/fish/functions/pchildren.fish
@@ -1,4 +1,10 @@
-# Defined via `source`
+# Defined interactively
 function pchildren --wraps=pgrep
-    pstree -lap (pgrep -n $argv)
+    if test (count (string match -r '^[0-9]+$' -- $argv)) -eq (count $argv)
+        set pids $argv
+    else
+        set pids (pgrep -n $argv)
+    end
+
+    pstree -lap $pids
 end
