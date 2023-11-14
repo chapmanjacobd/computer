1530fe2bff28bfc8e63460b0df19f2b1720b2de2
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-14 01:27:12 -0600

    vpn_jp vpn_nl

---
+++ .config/fish/functions/vpn_jp.fish
@@ -0,0 +1,8 @@
+# Defined interactively
+function vpn_jp
+    sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+    wg-quick up protovpn-jp
+    if confirm close?
+        wg-quick down protovpn-jp
+    end
+end
---
+++ .config/fish/functions/vpn_nl.fish
@@ -0,0 +1,8 @@
+# Defined interactively
+function vpn_nl
+    sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+    wg-quick up protovpn-nl
+    if confirm close?
+        wg-quick down protovpn-nl
+    end
+end
