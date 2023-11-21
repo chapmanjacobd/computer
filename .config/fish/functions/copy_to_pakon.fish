8eb7f906f44bf6eb93edf3dde51b092f713a2ac8
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-20 19:18:22 -0600

    copy_to_pakon

--- .config/fish/functions/copy_to_pakon.fish
+++ .config/fish/functions/copy_to_pakon.fish
@@ -4,6 +4,7 @@ function copy_to_pakon
     ssh backup remove_empty_directories /home/xk/d/_tixati/
 
     stickysync_backup ~/d/00_Metadata/stickysync_rtorrent_audiobooks d/_rtorrent/ /mnt/d/82_Audiobooks/rtorrent/
+    stickysync_backup ~/d/00_Metadata/stickysync_qbittorrent d/_qbittorrent/ /mnt/d/75_Moviequeue/From_Backup/
 
     # ssh backup 'pkill tixati; systemctl --user stop vpn_oracle.service; sleep (minutes 45); systemctl --user restart vpn_oracle.service; sleep 20; run tixati'
 end
