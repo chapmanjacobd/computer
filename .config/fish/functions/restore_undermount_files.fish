72d5a46db82d6f13504cae19cc46381911ec1efa
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-23 22:13:35 -0600

    restore_undermount_files

--- .config/fish/functions/restore_undermount_files.fish
+++ .config/fish/functions/restore_undermount_files.fish
@@ -1,11 +1,11 @@
-# Defined interactively
+# Defined via `source`
 function restore_undermount_files
     sudo mkdir /orig_mnt/
     sudo mount --bind /mnt /orig_mnt/
     /orig_mnt/
     for dir in (fd -td -d1)
         # check if src and dest are the same...
-        # may be wise to do manually, if one is wise enough
+        # may be wise to do manually, if one is wise enough--or prevent this to begin with via conventional mountpoint dir permissions
         NO_COLOR=1 ncdu /orig_mnt/$dir
         if confirm
             rclone move /orig_mnt/$dir /mnt/$dir
