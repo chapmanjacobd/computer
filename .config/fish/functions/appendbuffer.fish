e13892823a5347ef3e8067fb4473b529fb5e33be
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-24 15:26:57 -0600

    appendbuffer appendbuffer_html

--- .config/fish/functions/appendbuffer.fish
+++ .config/fish/functions/appendbuffer.fish
@@ -2,8 +2,6 @@
 function appendbuffer
     set out (mktemp)
     echo $out
-    while sleep 0.2
-        cat $out (cbfile) | dedupe | sponge $out
-    end
+    watch.py -- cb $argv >>$out
     cat $out | cb
 end
--- .config/fish/functions/appendbuffer_html.fish
+++
@@ -1,8 +0,0 @@
-# Defined interactively
-function appendbuffer_html
-    set out (mktemp)
-    echo $out
-    while sleep 0.2
-        cat $out (cb_htmlfile) | sponge $out
-    end
-end
