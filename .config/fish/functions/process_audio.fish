450ebc9eccb5071488baf6beab1d933224004335
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-25 07:27:54 -0600

    process_audio wtm

--- .config/fish/functions/process_audio.fish
+++ .config/fish/functions/process_audio.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function process_audio
-    fd . $argv -H -tf -eDTS -eAAC -eWAV -eAIF -eAIFF -eFLAC -eAIFF -eM4A -eMP3 -eOGG -eMP4 -eWMA -j6 -x process_audio.py
+    fd . $argv -H -tf -eDTS -eAAC -eWAV -eAIF -eAIFF -eFLAC -eAIFF -eM4A -eMP3 -eOGG -eMP4 -eWMA -j6 -x library process-audio --split-longer-than 36mins
 end
--- .config/fish/functions/wtm.fish
+++ .config/fish/functions/wtm.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function wtm
     focus_under_mouse
-    python -m xklb.lb watch --move-replace --action ask_move_or_delete --keep-dir /home/xk/d/69_Taxes_Keep/ --prefetch 2 --local -u size desc --sort-by 'deleted/played desc, deleted desc' --loop --exit-code-confirm -i --cmd5 ~/bin/process_audio.py --cmd6 'mv {} /mnt/d/68_Vr_Tv/' -m 4 $argv
+    python -m xklb.lb watch --move-replace --action ask_move_or_delete --keep-dir /home/xk/d/69_Taxes_Keep/ --prefetch 2 --local -u size desc --sort-by 'deleted/played desc, deleted desc' --loop --exit-code-confirm -i --cmd5 'python -m xklb.lb process-audio --split-longer-than 36mins' --cmd6 'mv {} /mnt/d/68_Vr_Tv/' -m 4 $argv
     focus_follows_mouse
 end
