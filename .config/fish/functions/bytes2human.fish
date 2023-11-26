8f667282e0743079f60bc1b5f4dd0980cf18534d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-25 18:34:47 -0600

    abbreviations bytes2human dnf_installed gitignore

---
+++ .config/fish/functions/bytes2human.fish
@@ -0,0 +1,8 @@
+# Defined interactively
+function bytes2human
+    if test (count $argv) -eq 0
+        python -c "import sys; from humanize import naturalsize; [print(naturalsize(float(l), binary=True)) for l in sys.stdin]"
+    else
+        python -c "from humanize import naturalsize; print(naturalsize(float('$argv'), binary=True))"
+    end
+end
