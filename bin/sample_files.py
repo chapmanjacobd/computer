441b9fc18005b10942bd6c9bbff166cf90ea245c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-26 14:08:05 -0600

    sample_files

--- bin/sample_files.py
+++ bin/sample_files.py
@@ -6,9 +6,8 @@ import sys
 from typing import Dict, List
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--files', nargs='?', type=int, const=1, help='Print N random files per directory instead of directories'
-)
+parser.add_argument('--files', type=int, default=1, help='Print N random files per directory')
+parser.add_argument('--folders', '--dirs', action='store_true', help='Only print unique directories')
 args = parser.parse_args()
 
 files_per_dir: Dict[str, List[str]] = {}
@@ -19,15 +18,17 @@ for line in sys.stdin:
 
     if dir_name not in files_per_dir:
         files_per_dir[dir_name] = []
+    if line.endswith(os.sep):
+        continue
     files_per_dir[dir_name].append(line)
 
-if args.files:
-    for dir_name in files_per_dir:
-        files = files_per_dir[dir_name]
-        n = min(len(files), args.files)
-        print('\n'.join(random.sample(files, n)))
-else:
+if args.folders:
     for dir_name in files_per_dir:
         if not dir_name:
             continue
         print(dir_name + os.sep)
+else:
+    for dir_name in files_per_dir:
+        files = files_per_dir[dir_name]
+        n = min(len(files), args.files)
+        print('\n'.join(random.sample(files, n)))
