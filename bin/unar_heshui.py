45b42702936334bd8bd20d2fb42cb92db6d3588a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-26 21:48:02 -0600

    dnf_installed pip_installed unar_heshui

--- bin/unar_heshui.py
+++ bin/unar_heshui.py
@@ -1,1 +1,50 @@
 #!/usr/bin/python3
+import os
+from collections import defaultdict
+from pathlib import Path
+
+import rarfile
+
+cwd = Path().cwd()
+
+destination_base = Path('..', os.path.basename(cwd) + '_out')
+destination_base.mkdir(exist_ok=True)  # TODO: remove
+
+
+def check_archive(input_path: Path):
+    files = defaultdict(list)
+    with rarfile.RarFile(input_path, errors='strict') as rf:
+        rf.setpassword('heshui')
+
+        for f in rf.infolist():
+            if f.file_size == 0:
+                if not f.filename.endswith(os.sep):
+                    print(f.filename)
+                continue
+            elif f.filename.endswith('.jpg'):
+                continue
+
+            files[os.path.splitext(f.filename)[1]].append((f.filename, f.mtime))
+
+        if len(files.keys()) == 0:
+            print('No files:', input_path)
+        elif len(files.keys()) == 1:
+            for _, files_arr in files.items():
+                for member, mtime in files_arr:
+                    print(member, mtime)
+                    # rf.extract(member, destination_base)
+        else:
+            print(files)
+
+
+for input_path in cwd.rglob('*'):
+    if input_path.is_file():
+        try:
+            check_archive(input_path)
+        except (rarfile.BadRarFile, rarfile.NotRarFile):
+            print('Corrupt file:', input_path)
+        except rarfile.NeedFirstVolume:
+            print('NeedFirstVolume', input_path)
+        except Exception as e:
+            print(input_path)
+            raise
