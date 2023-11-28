6193bd11baca974963aaee7b5bdef4a69a49c18b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-28 09:20:31 -0600

    the_empire_dl

--- .config/fish/functions/the_empire_dl.fish
+++ .config/fish/functions/the_empire_dl.fish
@@ -2,7 +2,7 @@
 function the_empire_dl
     load_env_the_empire
 
-    for url in (cb)
+    for url in $argv
         if not grep -i $url ~/.local/data/qbittorrent/the_empire_click.urls >/dev/null
             echo $url
             curl_retry.py --move ~/.local/data/qbittorrent/the_empire_click/ --filetype bittorrent "curl -OJs -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0' -H 'Referer: https://theempire.click/details.php' -H 'Cookie: $THE_EMPIRE_COOKIE'" $url
