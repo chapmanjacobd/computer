399398c163a92e579f01800c3e28e2ca4c3a4548
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-11-29 14:20:55 -0600

    lt-dev lt lt_args

--- .config/fish/functions/lt-dev.fish
+++ .config/fish/functions/lt-dev.fish
@@ -1,5 +1,5 @@
 # Defined interactively
 function lt-dev
     ~/lb/
-    python -m xklb.lb listen -w 'play_count=0' -k delete-if-audiobook -v audio.db $argv
+    python -m xklb.lb (lt_args) -v $argv
 end
---
+++ .config/fish/functions/lt_args.fish
@@ -0,0 +1,13 @@
+# Defined via `source`
+function lt_args
+    echo "listen
+-w
+play_count=0
+-r
+-k
+delete-if-audiobook
+--local-media-only
+-O
+folder
+/home/xk/lb/audio.db"
+end
