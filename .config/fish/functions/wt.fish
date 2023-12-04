447e900756e1c8937b9dee15333b99294d35ac87
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-12-04 11:34:06 -0600

    wt

--- .config/fish/functions/wt.fish
+++ .config/fish/functions/wt.fish
@@ -1,3 +1,3 @@
 function wt
-    /home/xk/.local/bin/lb wt --keep-dir /mnt/d/77_Library/ -k delete --local-media-only ~/lb/video.db $argv
+    /home/xk/.local/bin/lb wt --keep-dir /mnt/d/77_Library/ -k delete --local-media-only ~/lb/video.db --cmd5 'echo skip' $argv
 end
