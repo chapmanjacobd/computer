d5a9cde2e731a100efb3caf96fa30e677818353d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-12-10 20:09:58 -0600

    dupreplace mkdir

--- .config/fish/functions/dupreplace.fish
+++ .config/fish/functions/dupreplace.fish
@@ -1,11 +1,13 @@
-# Defined interactively
+# Defined via `source`
 function dupreplace --argument duplicate original
     rm "$duplicate"
 
     for n in (seq 1 10)
         set src (echo "$original" | sed "s|/mnt/d/|/mnt/d$n/|")
         if test -e $src
-            cp --reflink=always $src (echo "$duplicate" | sed "s|/mnt/d/|/mnt/d$n/|")
+            set dest (echo "$duplicate" | sed "s|/mnt/d/|/mnt/d$n/|")
+            command mkdir -p (path dirname "$dest")
+            cp --reflink=always $src "$dest"
         end
     end
 end
--- .config/fish/functions/mkdir.fish
+++ .config/fish/functions/mkdir.fish
@@ -1,8 +1,4 @@
 # Defined interactively
 function mkdir
-    if path extension $argv >/dev/null
-        command mkdir -p (path dirname $argv)
-    else
-        command mkdir -p $argv
-    end
+    command mkdir -p $argv
 end
