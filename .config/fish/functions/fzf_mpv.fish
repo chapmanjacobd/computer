f4a51b1a8f1a0b23c90a60c1245d720862748f4c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-12-18 21:40:47 -0600

    fzf_mpv fzf_mpv_preview

---
+++ .config/fish/functions/fzf_mpv.fish
@@ -0,0 +1,22 @@
+# Defined interactively
+function fzf_mpv
+    set -l videos
+    while read -l line
+        set videos $videos $line
+    end
+
+    while test (count $videos) -gt 0
+        set selected_url (printf "%s\n" $videos | fzf)
+
+        if test -n "$selected_url"
+            mpv $selected_url
+        else
+            break
+        end
+
+        set -l index (contains -i $videos $selected_url)
+        if test $index -gt 0
+            set -e videos[$index]
+        end
+    end
+end
