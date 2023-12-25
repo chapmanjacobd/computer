1860205772f7dca5fcb57941cf5798a1e3b642c7
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-12-25 01:46:48 -0600

    process_audio

--- .config/fish/functions/process_audio.fish
+++ .config/fish/functions/process_audio.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function process_audio
-    fd . $argv -H -tf -eDTS -eAAC -eWAV -eAIF -eAIFF -eFLAC -eAIFF -eM4A -eMP3 -eOGG -eMP4 -eWMA -j6 -x library process-audio --split-longer-than 36mins
+    fd . $argv -H -tf -eDTS -eAAC -eWAV -eAIF -eAIFF -eFLAC -eAIFF -eM4A -eMP3 -eOGG -eMP4 -eM4B -eWMA -j6 -x library process-audio --split-longer-than 36mins
 end
