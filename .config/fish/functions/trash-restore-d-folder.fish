be9b11e176485a4caf5cec52ade02bd840546f88
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-12-29 10:20:15 -0600

    trash-restore-d-folder

--- .config/fish/functions/trash-restore-d-folder.fish
+++ .config/fish/functions/trash-restore-d-folder.fish
@@ -2,6 +2,7 @@
 function trash-restore-d-folder
     for i in (seq 1 $MERGERFS_DISKS)
         /mnt/d$i/
-        trash-restore $argv
+        mkdir $argv
+        lb merge-folder .Trash/1000/files/$argv $argv
     end
 end
