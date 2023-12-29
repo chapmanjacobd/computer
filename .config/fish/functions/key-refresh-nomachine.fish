69ac33ab60a636c335c9fc7e432b96082856bcc1
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-12-29 13:03:09 -0600

    key-refresh-nomachine

--- .config/fish/functions/key-refresh-nomachine.fish
+++ .config/fish/functions/key-refresh-nomachine.fish
@@ -1,10 +1,10 @@
 # Defined via `source`
-function key-refresh-nomachine -a label
+function key-refresh-nomachine
     set oldkey (mktemp --tmpdir=/home/xk/.ssh/old/ id_rsa_nx_XXXXX)
     mv ~/.ssh/id_rsa_nx $oldkey
     mv ~/.ssh/id_rsa_nx.pub $oldkey.pub
 
-    ssh-keygen -t rsa -C $label -q -N '' -f ~/.ssh/id_rsa_nx </dev/zero || true
+    ssh-keygen -t rsa -C (hostname) -q -N '' -f ~/.ssh/id_rsa_nx </dev/zero || true
     cat ~/.ssh/id_rsa_nx.pub >>~/.nx/config/authorized.crt
     filterfile ~/.nx/config/authorized.crt (cat $oldkey.pub)
 end
