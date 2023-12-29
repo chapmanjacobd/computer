c299f70b058b5a27f9a1008bc393883b4b91ca2e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-12-29 10:29:54 -0600

    pymodule_path

---
+++ bin/pymodule_path.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+import importlib.util
+
+if len(sys.argv) < 2:
+    print("Please provide the module name as an argument.")
+else:
+    module_name = sys.argv[1]
+    try:
+        spec = importlib.util.find_spec(module_name)
+        if spec is not None:
+            module_location = spec.origin
+            print(module_location)
+        else:
+            print(f"Module '{module_name}' not found.")
+    except ImportError:
+        print(f"Unable to import '{module_name}'.")
+
