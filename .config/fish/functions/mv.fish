859ba6904c5c2ca93ff564608676877b6e39842e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-01-03 17:37:35 -0600

    mv mktree

--- .config/fish/functions/mv.fish
+++ .config/fish/functions/mv.fish
@@ -4,5 +4,9 @@ function mv
     if not path extension $argv[-1] >/dev/null; and not test -e "$parent"
         mkdir "$parent"
     end
+
+    if test (count $argv) -eq 2
+        rmdir "$argv[-1]"  # fast rename if possible
+    end
     command mv $argv
 end
--- bin/mktree.py
+++ bin/mktree.py
@@ -2,34 +2,37 @@
 import argparse
 import os
 
-tasks = ['sync', 'dump', 'check', 'library', 'archive']
+tasks = ['sync', 'dump', 'pending', 'check', 'library', 'archive']
 categories = {
-    'self': [],
-    'world': [],
-    'datasets': [],
-    'programs': [],
-    'games': ['VR'],
-    'text': [],
-    'porn': ['video', 'VR', 'audio', 'image'],
-    'video': [],
-    'audio': [],
-    'image': [],
+    'self': ['metadata', 'home', 'finances', 'secure', 'private', 'credentials', 'portraits', 'other'],
+    'world': ['consulting', 'downloads', 'seeding', 'other'],
+    'datasets': ['raster', 'vector', 'links', 'file-lists', 'social', 'other'],
+    'programs': ['linux', 'windows', 'repos', 'other'],
+    'games': ['8bit', 'gamecube', 'wii', 'switch', 'vr', 'linux', 'other'],
+    'text': ['ebooks', 'manga', 'receipts', 'web', 'recipes', 'theory', 'other'],
+    'porn': ['video', 'vr', 'audio', 'image', 'other'],
+    'video': ['other'],
+    'audio': ['music', 'midi', 'patterns', 'recordings', 'other'],
+    'image': ['art', 'memes', 'patterns', 'other'],
+    'internals': ['text', 'video', 'audio', 'image', 'other'],
+    'projects': ['art', 'cinematograph', 'music', 'other'],
 }
 
 
 def create_directory_structure(base_dir):
     for task in tasks:
         for category, subcategories in categories.items():
-            path = os.path.join(base_dir,task, category)
+            path = os.path.join(base_dir, task, category)
             os.makedirs(path, exist_ok=True)
 
             for subcategory in subcategories:
                 path = os.path.join(base_dir, task, category, subcategory)
                 os.makedirs(path, exist_ok=True)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create directory hierarchy.')
-    parser.add_argument('base_dir', nargs='?', default=os.getcwd(), help='Base directory path (default: current working directory)')
+    parser.add_argument('base_dir', nargs='?', default=os.getcwd())
     args = parser.parse_args()
 
     create_directory_structure(args.base_dir)
