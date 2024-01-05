d8e3a8503d73ad992f0c6ec389b3a00709a98916
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-01-04 22:38:36 -0600

    daily dbs dlunsafe lb-maintainence lb-refresh library-stats mrrelax tubeupdate weekly

--- .config/fish/functions/lb-maintainence.fish
+++ .config/fish/functions/lb-maintainence.fish
@@ -1,7 +1,7 @@
 # Defined via `source`
 function lb-maintainence
     ~/lb/
-    for db in video.db tax.db audio.db tax_Sounds.db
+    for db in video.db tax.db audio.db tax_sounds.db
         sqlite3 $db 'delete from media where time_deleted>0'
         b lb optimize --fts $db
     end
--- .config/fish/functions/tubeupdate.fish
+++ .config/fish/functions/tubeupdate.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function tubeupdate
-    for db in ~/lb/video.db ~/lb/audio.db ~/lb/tax_Sounds.db ~/lb/tax.db
+    for db in ~/lb/video.db ~/lb/audio.db ~/lb/tax_sounds.db ~/lb/tax.db
         lb dedupe-db $db playlists --bk extractor_playlist_id
         lb tubeupdate $db
     end
