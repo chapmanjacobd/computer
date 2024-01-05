31152cb1c8642fdb17701ab36a20363858da00b2
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-01-05 02:48:52 -0600

    hourly

--- .config/fish/functions/hourly.fish
+++ .config/fish/functions/hourly.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function hourly
-    # rsync -auh --info=progress2 --no-inc-recursive --remove-sent-files ~/Downloads/*.torrent backup:.local/data/rtorrent/watch/new/
+    ssh backup torrent_promote.py --out .local/data/qbittorrent/jptvclub/ .local/data/jptv.club/ -n1
 
     load_env_mam
     ~/.local/bin/mam_upload_credit.sh
