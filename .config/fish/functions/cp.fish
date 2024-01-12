94bc27e7cb7782ffd0e4adb9a5b16f271611c2ac
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-01-12 11:19:59 -0600

    cp

--- .config/fish/functions/cp.fish
+++ .config/fish/functions/cp.fish
@@ -1,6 +1,6 @@
 # Defined interactively
-function cp
-    set parent (path dirname "$argv[-1]")
+function cp --wraps cp
+    set parent (path dirname $argv[-1])
     if not path extension $argv[-1] >/dev/null; and not test -e "$parent"
         mkdir "$parent"
     end
