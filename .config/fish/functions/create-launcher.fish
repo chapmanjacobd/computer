d40b0a0cbce201f30d7af8b5c29c5e865b799469
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-01-12 12:02:08 -0600

    create-launcher

---
+++ .config/fish/functions/create-launcher.fish
@@ -0,0 +1,16 @@
+# Defined via `source`
+function create-launcher
+    set command_name (string replace -r ' ' '_' "$argv")
+    set launcher_path $HOME/.local/share/applications/$command_name.desktop
+
+    echo "[Desktop Entry]" >$launcher_path
+    echo "Version=1.0" >>$launcher_path
+    echo "Type=Application" >>$launcher_path
+    echo "Name=$command_name" >>$launcher_path
+    echo "Exec=setsid -f fish -c '$argv'" >>$launcher_path
+    echo "Terminal=false" >>$launcher_path
+    echo "Categories=Utility;" >>$launcher_path
+
+    echo Launcher created:
+    echo "$launcher_path"
+end
