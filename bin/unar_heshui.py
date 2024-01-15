db18d63c5ba74639dbb73f7a07b1cd950ab7b75c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-01-14 19:25:43 -0600

    unar_heshui

--- bin/unar_heshui.py
+++ bin/unar_heshui.py
@@ -3,13 +3,16 @@ import argparse
 import os
 from collections import Counter
 from pathlib import Path
+import tempfile
+
+import rarfile
 from xklb.utils import objects
 from xklb.utils.log_utils import log
-import rarfile
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
+    parser.add_argument("--unlink", action="store_true")
     parser.add_argument("--dry-run", action="store_true")
     parser.add_argument("--verbose", "-v", action="count", default=0)
 
@@ -31,25 +34,58 @@ def check_archive(args, input_path: Path, output_prefix: Path):
                 if not f.filename.endswith(os.sep):
                     log.warning('Ignoring non-directory zero size file: %s', f.filename)
                 continue
-            elif f.filename.lower().endswith(('.txt', '.pdf', '.docx', '.url', '.html', '.htm', '.jpg', '.png', '.bmp', '.gif', '.m3u', 'Thumbs.db')):
+            elif f.filename.lower().endswith(
+                ('.txt', '.pdf', '.docx', '.url', '.html', '.htm', '.jpg', '.png', '.bmp', '.gif', '.m3u', 'Thumbs.db', 'desktop.ini', '.DS_Store')
+            ):
+                continue
+            if any(s in f.filename.lower() for s in ('左右反転バージョン',)):
                 continue
 
             files.append(f.filename)
 
-        files, nested_archives = [s for s in files if not s.endswith(('.exe', '.rar'))], [s for s in files if s.endswith(('.exe', '.rar'))]
+        files, nested_archives = [s for s in files if not s.endswith(('.exe', '.rar'))], [
+            s for s in files if s.endswith(('.exe', '.rar'))
+        ]
         if nested_archives:
             log.error('nested archives: %s', nested_archives)
+            with tempfile.TemporaryDirectory() as temp_prefix:
+                rf.extractall(temp_prefix)
+                for nested_archive in nested_archives:
+                    temp_path = Path(temp_prefix) / nested_archive
+                    try:
+                        count_extracted += check_archive(args, temp_path, output_prefix)
+                    except (rarfile.BadRarFile, rarfile.NotRarFile):
+                        log.info('Corrupt file: %s', temp_path)
+                    except rarfile.NeedFirstVolume:
+                        log.debug('NeedFirstVolume: %s', temp_path)
+                    except Exception:
+                        log.exception(input_path)
+                        raise
+            if not files:
+                return count_extracted
 
         extensions = Counter(Path(s).suffix.lower() for s in files)
+
+        for extra in ['.lrc', '.rtf']:
+            if extra in extensions:
+                extra_files = [s for s in files if s.endswith(extra)]
+                log.info('Extracting %s extras %s', extra, extra_files)
+                for member in extra_files:
+                    rf.extract(member, output_prefix)
+                del extensions[extra]
 
         if len(files) == 0:
             log.error('No files found in: %s', input_path)
         elif len(extensions) == 1:
-            for member in files:
+            key, _value = extensions.popitem()
+            for member in [s for s in files if s.endswith(key)]:
                 log.debug('One type of extension: %s', member)
                 if not args.dry_run:
                     rf.extract(member, output_prefix)
                 count_extracted += 1
+
+            if args.unlink:
+                input_path.unlink()
         elif len(extensions) == 2:
             fist_ext, second_ext = extensions
 
@@ -69,6 +105,9 @@ def check_archive(args, input_path: Path, output_prefix: Path):
                     if not args.dry_run:
                         rf.extract(member, output_prefix)
                     count_extracted += 1
+
+                if args.unlink:
+                    input_path.unlink()
         else:
             log.error('Directory structure not recognized: %s', files)
     return count_extracted
@@ -77,20 +116,20 @@ def check_archive(args, input_path: Path, output_prefix: Path):
 if __name__ == "__main__":
     args = parse_args()
 
-    src = args.source_path
-    output_prefix = src.parent / (src.parent.name + '_out')
+    src = Path(args.source_path)
+    output_prefix = src.parent / (src.parent.name + 'out')
     output_prefix.mkdir(exist_ok=args.dry_run)
 
-    count_skipped=0
-    count_processed=0
-    count_files=0
+    count_skipped = 0
+    count_processed = 0
+    count_files = 0
     for input_path in src.rglob('*'):
         if input_path.is_file():
             try:
                 ar_extracted = check_archive(args, input_path, output_prefix)
                 if ar_extracted > 0:
+                    count_files += ar_extracted
                     count_processed += 1
-                    count_files += check_archive(args, input_path, output_prefix)
                 else:
                     count_skipped += 1
             except (rarfile.BadRarFile, rarfile.NotRarFile):
@@ -99,5 +138,7 @@ if __name__ == "__main__":
                 log.debug('NeedFirstVolume: %s', input_path)
             except Exception as e:
                 log.exception(input_path)
+                if args.dry_run:
+                    raise
 
-    print('Skipped:',  count_processed, 'Processed:',  count_processed, 'Files:', count_files)
+    print('Skipped:', count_skipped, 'Processed:', count_processed, 'Files:', count_files)
