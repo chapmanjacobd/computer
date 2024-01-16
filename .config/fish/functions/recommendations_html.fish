406c1c263e9c60434ddfa3d76bb6b8632b4fd9f1
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-01-15 22:37:56 -0600

    recommendations_html

--- .config/fish/functions/recommendations_html.fish
+++ .config/fish/functions/recommendations_html.fish
@@ -1,4 +1,4 @@
 # Defined interactively
-function reccomendations_html
+function recommendations_html
     lb extract-text --skip-links --local-file (cb_htmlfile) | lb cs --groups | jq -r '.[] | .grouped_paths | "\n" + join("\n")'
 end
