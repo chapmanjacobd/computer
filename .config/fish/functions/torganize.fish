8348b661587a7d21bf71b55984ddffa695a31d43
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-01-19 11:23:00 -0600

    torganize

--- .config/fish/functions/torganize.fish
+++ .config/fish/functions/torganize.fish
@@ -4,6 +4,7 @@ function torganize
     rsync -auh --remove-sent-files ~/Downloads/*jptv.club*.torrent backup:.local/data/jptv.club/
 
     rsync -auh --remove-sent-files ~/Downloads/\[OldToons*.torrent backup:.local/data/qbittorrent/seed/oldtoons_world/
+    rsync -auh --remove-sent-files ~/Downloads/\[CarPT*.torrent backup:.local/data/qbittorrent/seed/carpt.net/
 
     rsync -auh --remove-sent-files ~/Downloads/\[HappyFappy\]*.torrent backup:.local/data/qbittorrent/porn/happyfappy/
     rsync -auh --remove-sent-files ~/Downloads/\[pornolab.net\]*.torrent backup:.local/data/qbittorrent/porn/pornolab.net/
