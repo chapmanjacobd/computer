b04ea74aea4facb44d25ef1aed8b7e8c9ef49f1e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-01-21 19:35:38 -0600

    wdl

--- .config/fish/functions/wdl.fish
+++ .config/fish/functions/wdl.fish
@@ -1,6 +1,10 @@
 # Defined interactively
 function wdl
     for url in $argv
+        set parent (string unescape --style=url (path basename $url))
+        mkdir $parent/
+        ./$parent/
         rclone copy :http: --http-url "$url" .
+        ..
     end
 end
