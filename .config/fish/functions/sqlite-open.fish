4118320cbe7ca109310f1537f6c6729059d81129
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-01-23 03:30:53 -0600

    sqlite-open

--- .config/fish/functions/sqlite-open.fish
+++ .config/fish/functions/sqlite-open.fish
@@ -1,6 +1,9 @@
 # Defined interactively
 function sqlite-open
-    for f in $argv
+    run dbeaver-ce -con 'driver=SQLite|database='(path resolve $argv[1])
+    sleep 2
+
+    for f in $argv[2..-1]
         dbeaver-ce -con 'driver=SQLite|database='(path resolve $f)
     end
 end
