4daed23e22689cdbe58d05a5a7ddc1430c300324
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-02-03 13:25:57 +0800

    setup-fedora

--- .github/setup-fedora.sh
+++ .github/setup-fedora.sh
@@ -26,7 +26,7 @@ sudo dnf install -y lame\* --exclude=lame-devel
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-cat "${BASH_SOURCE%/vscode_extensions.txt}" | xargs -I{} -n1 code --install-extension {} --force
+cat .github/vscode_extensions.txt | xargs -I{} -n1 code --install-extension {} --force
 
 sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
 [google-cloud-sdk]
