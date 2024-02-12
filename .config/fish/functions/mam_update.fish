575d6f93be5ca9e431b47d7b521220825ee9b216
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-02-11 20:26:06 -0600

    mam_update

--- .config/fish/functions/mam_update.fish
+++ .config/fish/functions/mam_update.fish
@@ -10,6 +10,10 @@ function mam_update
         python -m xklb.scratch.mam_search --audiobooks --books --radio --no-title --narrator --cookie $MAM_COOKIE ~/d/library/datasets/links/mam/mam.db $q
     end
 
+    for l in (cat ~/j/private/mam_custom_args.txt)
+        python -m xklb.scratch.mam_search --cookie $MAM_COOKIE ~/d/library/datasets/links/mam/mam.db (string split ' ' -- $l) ''
+    end
+
     for db in ~/d/library/datasets/links/mam/mam.db
         sqlite --no-headers --raw-lines "$db" "select id from media where added > datetime('now', '-7 days') and vip=1 and seeders>0" | sed "s|^|curl -OJs -b mam_id=$MAM_COOKIE https://www.myanonamouse.net/tor/download.php?tid=|" | parallel -S backup -j4 ".local/data/rtorrent/watch/vip_new/; eval {}"
         sqlite --no-headers --raw-lines "$db" "select id from media where added > datetime('now', '-7 days') and vip=0 and seeders>0" | sed "s|^|curl -OJs -b mam_id=$MAM_COOKIE https://www.myanonamouse.net/tor/download.php?tid=|" | parallel -S backup -j4 ".local/data/rtorrent/watch/nonvip_new/; eval {}"
