f948266508820aabfc667c1806c4b46228c769db
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-02-13 13:33:13 +0800

    lb-refresh

--- .config/fish/functions/lb-refresh.fish
+++ .config/fish/functions/lb-refresh.fish
@@ -5,8 +5,8 @@ function lb-refresh
     end
 
     lb fsadd ~/lb/tax.db --hash --delete-unplayable --check-corrupt --full-scan-if-corrupt 15% --delete-corrupt 20% --move ~/d/check/porn/video/ ~/d/dump/porn/video/ -v
-    fd . /mnt/d/dump/porn/video/ -ejpg -ejpeg -epng -x lb relmv {} /mnt/d/dump/porn/image/from_video/
-    fd . /mnt/d/dump/porn/video/ -emka -x lb relmv {} /mnt/d/dump/porn/audio/from_video/
+    fd . /mnt/d/dump/porn/video/ -ejpg -ejpeg -epng -epdf -x lb relmv {} /mnt/d/dump/porn/image/from_video/
+    fd . /mnt/d/dump/porn/video/ -emka -em4a -x lb relmv {} /mnt/d/dump/porn/audio/from_video/
 
     # lb fsadd ~/lb/audio.db --audio --delete-unplayable --process --move ~/d/check/audio/music/ ~/d/dump/audio/music/ -v
     lb fsadd ~/lb/tax_sounds.db --audio --delete-unplayable --process --move ~/d/check/porn/audio/ ~/d/dump/porn/audio/ -v
