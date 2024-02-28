db991f300b6ad7e9508eed4ddf0710d415510fd6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-02-28 20:09:41 +0800

    vnc

--- .config/fish/functions/vnc.fish
+++ .config/fish/functions/vnc.fish
@@ -3,7 +3,7 @@ function vnc
     sync_history
     if contains pakon $argv
         # ssh -fNT -R localhost:7070:localhost:22 pakon
-        ssh xk@pakon -f -L 4102:localhost:4000 'sleep 20' &
+        ssh -4 xk@pakon -f -L 4102:localhost:4000 'sleep 20' &
         sleep 5
         /usr/NX/bin/nxplayer --session ~/.ssh/pakon.nxs
     end
