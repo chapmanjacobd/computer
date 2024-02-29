3374eace1bd399fcf91f2dde74c212a73fbfbbce
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-02-29 04:38:26 -0600

    lb-refresh

--- .config/fish/functions/lb-refresh.fish
+++ .config/fish/functions/lb-refresh.fish
@@ -4,15 +4,16 @@ function lb-refresh
         unar $file
     end
 
-    lb fsadd ~/lb/tax.db --hash --delete-unplayable --check-corrupt --full-scan-if-corrupt 15% --delete-corrupt 20% --move ~/d/check/porn/video/ ~/d/dump/porn/video/ -v
+    lb fsadd ~/lb/tax.db --video --hash --delete-unplayable --check-corrupt --full-scan-if-corrupt 15% --delete-corrupt 20% --move ~/d/check/porn/video/ ~/d/dump/porn/video/ -v
     fd . /mnt/d/dump/porn/video/ -ejpg -ejpeg -epng -epdf -x lb relmv {} /mnt/d/dump/porn/image/from_video/
     fd . /mnt/d/dump/porn/video/ -emka -em4a -x lb relmv {} /mnt/d/dump/porn/audio/from_video/
 
-    # lb fsadd ~/lb/audio.db --audio --delete-unplayable --process --move ~/d/check/audio/music/ ~/d/dump/audio/music/ -v
     lb fsadd ~/lb/tax_sounds.db --audio --delete-unplayable --process --move ~/d/check/porn/audio/ ~/d/dump/porn/audio/ -v
 
-    lb fsadd ~/lb/video.db --hash --delete-unplayable --check-corrupt --full-scan-if-corrupt 15% --delete-corrupt 20% --move ~/d/check/video/ ~/d/dump/video/ -v
-    fd . /mnt/d/dump/video/ -emp3 -x lb relmv {} /mnt/d/dump/audio/from_video/
+    lb fsadd ~/lb/video.db --video --hash --delete-unplayable --check-corrupt --full-scan-if-corrupt 15% --delete-corrupt 20% --move ~/d/check/video/ ~/d/dump/video/ -v
+    lb fsadd ~/lb/audio.db --audio --delete-unplayable --process --move ~/d/check/audio/from_video/ ~/d/dump/video/
+
+    lb fsadd ~/lb/audio.db --audio --delete-unplayable --process --move ~/d/check/audio/ ~/d/dump/audio/ -v
 
     ~/lb/
     for db in video.db tax.db audio.db tax_sounds.db
