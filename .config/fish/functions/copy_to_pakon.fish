e765d5467e66c8cea8a9883bad67ab8d74db3cbc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-03-03 05:49:09 -0600

    copy_to_pakon

--- .config/fish/functions/copy_to_pakon.fish
+++ .config/fish/functions/copy_to_pakon.fish
@@ -1,9 +1,5 @@
 # Defined interactively
 function copy_to_pakon
-    #rsync -auh --info=progress2 --no-inc-recursive --remove-sent-files backup:d/_tixati/ ~/d/dump/video/other/from_backup/
-    #ssh backup remove_empty_directories /home/xk/d/_tixati/
-
-    stickysync_backup ~/d/sync/datasets/file-lists/stickysync_rtorrent_audiobooks d/_rtorrent/ /mnt/d/dump/audio/audiobooks/rtorrent/
 
     for d in ~/d/check/world/seeding/*
         switch $d
@@ -14,6 +10,11 @@ function copy_to_pakon
         end
     end
     stickysync_local ~/d/sync/datasets/file-lists/stickysync_qbittorrent ~/d/check/world/seeding-porn/ ~/d/dump/porn/video/
+
+    #rsync -auh --info=progress2 --no-inc-recursive --remove-sent-files backup:d/_tixati/ ~/d/dump/video/other/from_backup/
+    #ssh backup remove_empty_directories /home/xk/d/_tixati/
+
+    stickysync_backup ~/d/sync/datasets/file-lists/stickysync_rtorrent_audiobooks d/_rtorrent/ /mnt/d/dump/audio/audiobooks/rtorrent/
 
     lb-refresh
 end
