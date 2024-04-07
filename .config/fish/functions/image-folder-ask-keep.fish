1ca9f563e70b95714f3a0ee0c0209b7bdf9fe2d1
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-04-07 10:13:23 +0800

    image-folder-ask-keep

--- .config/fish/functions/image-folder-ask-keep.fish
+++ .config/fish/functions/image-folder-ask-keep.fish
@@ -3,7 +3,9 @@ function image-folder-ask-keep
     for dir in $argv
         for d in (fd . -td "$dir")
             feh -q -F --hide-pointer --sort filename --on-last-slide resume -G --auto-zoom --zoom max --draw-tinted --image-bg black --scale-down -D -3 "$d"
-            if not confirm keep
+            if confirm keep
+                lb relmv "$d" ~/d/library/porn/image/
+            else
                 trash-put "$d"
             end
         end
