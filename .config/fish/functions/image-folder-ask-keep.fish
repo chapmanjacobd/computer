caea83cce3fd0a9d0cfe73bdf16bfc70c0a778c3
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-04-07 09:46:40 +0800

    image-folder-ask-keep

--- .config/fish/functions/image-folder-ask-keep.fish
+++ .config/fish/functions/image-folder-ask-keep.fish
@@ -1,6 +1,6 @@
-# Defined interactively
+# Defined via `source`
 function image-folder-ask-keep
-    for d in (fd -td $argv)
+    for d in (fd . -td "$argv")
         feh -q -F --hide-pointer --sort filename --on-last-slide resume -G --auto-zoom --zoom max --draw-tinted --image-bg black --scale-down -D -3 $d
         if not confirm keep
             trash-put $d
