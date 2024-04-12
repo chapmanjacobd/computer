a2ac2b79182effa184c61f6aefc65ae200ef89df
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-04-12 14:48:08 +0800

    forganize lb-refresh

--- .config/fish/functions/forganize.fish
+++ .config/fish/functions/forganize.fish
@@ -25,11 +25,26 @@ function forganize
     set audio_dirs ~/d/dump/porn/audio/ ~/d/dump/audio/
     set photo_dirs ~/d/dump/porn/image/ ~/d/dump/image/other/ ~/d/dump/image/other/ ~/d/dump/image/other/
 
-    ~/d/dump/audio/midi/ && fd -tf -eMID -x mv "{}" "{.}.mid"
+    # ~/d/dump/audio/midi/ && fd -tf -eMID -x mv "{}" "{.}.mid"
 
     fd . $audio_dirs -epng -ejpg -x rm "{}"
     # fd . $audio_dirs -H -tf -eWEBM -j8 -x fish -c 'mkvextract "{}" tracks 0:"{.}".oga && rm "{}"'
     process_audio $audio_dirs
+
+    for file in (fd -tf -eZIP -eRAR -eEXE -er00 . ~/d/dump/porn/video/ ~/d/dump/video/)
+        unar $file
+    end
+
+    lb fsadd ~/lb/tax.db --video --hash --delete-unplayable --check-corrupt --full-scan-if-corrupt 15% --delete-corrupt 20% --move ~/d/check/porn/video/ ~/d/dump/porn/video/ -v
+    fd . /mnt/d/dump/porn/video/ -ejpg -ejpeg -epng -epdf -x lb relmv {} /mnt/d/dump/porn/image/from_video/
+    fd . /mnt/d/dump/porn/video/ -emka -em4a -x lb relmv {} /mnt/d/dump/porn/audio/from_video/
+
+    lb fsadd ~/lb/tax_sounds.db --audio --delete-unplayable --process --move ~/d/check/porn/audio/ ~/d/dump/porn/audio/
+
+    lb fsadd ~/lb/video.db --video --hash --delete-unplayable --check-corrupt --full-scan-if-corrupt 15% --delete-corrupt 20% --move ~/d/check/video/ ~/d/dump/video/
+    lb fsadd ~/lb/audio.db --audio --delete-unplayable --process --move ~/d/check/audio/from_video/ ~/d/dump/video/
+
+    lb fsadd ~/lb/audio.db --audio --delete-unplayable --process --move ~/d/check/audio/ ~/d/dump/audio/
 
     lb fsadd (tempdb) --move ~/d/check/image/ --process --image ~/d/dump/image/ --delete-unplayable
     lb fsadd (tempdb) --move ~/d/check/video/image/ --process ~/d/dump/image/ --delete-unplayable --io-multiplier 0.2
--- .config/fish/functions/lb-refresh.fish
+++ .config/fish/functions/lb-refresh.fish
@@ -1,19 +1,10 @@
 # Defined interactively
 function lb-refresh
-    for file in (fd -tf -eZIP -eRAR -eEXE -er00 . ~/d/dump/porn/video/ ~/d/dump/video/)
-        unar $file
-    end
+    lb fsadd ~/lb/video.db --video ~/d/sync/video/
+    lb fsadd ~/lb/audio.db --audio ~/d/sync/audio/
 
-    lb fsadd ~/lb/tax.db --video --hash --delete-unplayable --check-corrupt --full-scan-if-corrupt 15% --delete-corrupt 20% --move ~/d/check/porn/video/ ~/d/dump/porn/video/ -v
-    fd . /mnt/d/dump/porn/video/ -ejpg -ejpeg -epng -epdf -x lb relmv {} /mnt/d/dump/porn/image/from_video/
-    fd . /mnt/d/dump/porn/video/ -emka -em4a -x lb relmv {} /mnt/d/dump/porn/audio/from_video/
-
-    lb fsadd ~/lb/tax_sounds.db --audio --delete-unplayable --process --move ~/d/check/porn/audio/ ~/d/dump/porn/audio/ -v
-
-    lb fsadd ~/lb/video.db --video --hash --delete-unplayable --check-corrupt --full-scan-if-corrupt 15% --delete-corrupt 20% --move ~/d/check/video/ ~/d/dump/video/ -v
-    lb fsadd ~/lb/audio.db --audio --delete-unplayable --process --move ~/d/check/audio/from_video/ ~/d/dump/video/
-
-    lb fsadd ~/lb/audio.db --audio --delete-unplayable --process --move ~/d/check/audio/ ~/d/dump/audio/ -v
+    lb fsadd ~/lb/tax.db --video ~/d/sync/porn/video/
+    lb fsadd ~/lb/tax_sounds.db --audio ~/d/sync/porn/audio/
 
     ~/lb/
     for db in video.db tax.db audio.db tax_sounds.db
