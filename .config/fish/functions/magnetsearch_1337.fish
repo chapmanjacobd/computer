9578cca0c7c813d1b8f4cb05dd13b01740519950
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-04-15 00:53:30 -0500

    magnetsearch_1337 magnetsearch_1337_categories

--- .config/fish/functions/magnetsearch_1337.fish
+++ .config/fish/functions/magnetsearch_1337.fish
@@ -1,8 +1,4 @@
 # Defined interactively
-function magnetsearch_1337 --argument search
-    for cat in TV Documentaries Movies XXX
-        firefox --new-tab https://1337x.to/category-search/$search/$cat/1/
-        read last_page_number
-        lb links --file (lb links https://1337x.to/category-search/$search/$cat/(seq 1 $last_page_number)/ --path-include /torrent/ | psub) --path-include magnet: | ssh backup tee -a ~/.local/magnets
-    end
+function magnetsearch_1337 --argument search -a last_page_number
+    lb links https://1337x.to/search/$search/(seq 1 $last_page_number)/ --path-include /torrent/ | lb links --path-include magnet: | ssh backup tee -a ~/.local/magnets
 end
