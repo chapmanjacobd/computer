7fa1700e2854275223f921b78dfca6bcf3a8f1c9
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-04-18 02:14:54 -0500

    mrmusic

--- .config/fish/functions/mrmusic.fish
+++ .config/fish/functions/mrmusic.fish
@@ -2,10 +2,10 @@ function mrmusic
     rclone move /mnt/d/sync/audio/weekly/ /mnt/d/check/audio/
 
     lb relmv (
-        lt --db ~/lb/audio.db --local-media-only -w 'play_count=0' -u random --fetch-siblings if-audiobook -L 600 -p f
+        lb lt ~/lb/audio.db --local -w 'play_count=0' -u random --fetch-siblings if-audiobook -L 600 -p f
     ) /mnt/d/sync/audio/weekly/
 
     lb relmv (
-        lb listen ~/lb/audio.db -E /sync/audio/music/ --local --lower 4 --upper 16 -p bf | shuf | head -n 8
+        lb lt ~/lb/audio.db --local -E /sync/audio/ -w 'play_count=0' --lower 4 --upper 16 -p bf | shuf | head -n 8
     ) /mnt/d/sync/audio/weekly/
 end
