e057f5524836b76174593329b9fe551286ea4984
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-04-18 12:09:27 +0800

    watts

--- .config/fish/functions/watts.fish
+++ .config/fish/functions/watts.fish
@@ -2,6 +2,6 @@
 function watts
     set PERCENT (apcaccess -pLOADPCT | sed 's/Percent//')
     set UPSMAXWATTS (apcaccess -pNOMPOWER | sed 's/Watts//')
-    set watts (math "$PERCENT * $UPSMAXWATTS / 100")
-    echo $watts
+
+    echo (math "$PERCENT * $UPSMAXWATTS / 100")
 end
