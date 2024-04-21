184469c57c74de449497a7388be3d84a16f343fb
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-04-20 21:17:51 -0500

    log

--- .config/fish/functions/log.fish
+++ .config/fish/functions/log.fish
@@ -1,3 +1,3 @@
 function log -w journalctl
-    journalctl --no-hostname --reverse -a $argv | grep -v -E 'postfix|Firewall|pam|libva|kioslave|ddcutil|sshd|audit' | less -FSRXc
+    journalctl --no-hostname --reverse -a $argv | grep -v -E 'postfix|Firewall|pam|libva|kioslave|ddcutil|sshd|audit|kwin_core: Applying KScreen config' | less -FSRXc
 end
