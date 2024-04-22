72b83a23bc18b3faf0d9ffea126d952c880123f6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-04-22 22:58:25 +0800

    lb-load

--- .config/fish/functions/lb-load.fish
+++ .config/fish/functions/lb-load.fish
@@ -1,6 +1,5 @@
 # Defined interactively
 function lb-load
-
     lb fsadd ~/lb/tax.db --video --hash --delete-unplayable --check-corrupt --full-scan-if-corrupt 15% --delete-corrupt 20% --move ~/d/check/porn/video/ ~/d/dump/porn/video/ -v
     fd . /mnt/d/dump/porn/video/ -ejpg -ejpeg -epng -epdf -x lb relmv {} /mnt/d/dump/porn/image/from_video/
     fd . /mnt/d/dump/porn/video/ -emka -em4a -x lb relmv {} /mnt/d/dump/porn/audio/from_video/
@@ -8,7 +7,8 @@ function lb-load
     lb fsadd ~/lb/tax_sounds.db --audio --delete-unplayable --process --move ~/d/check/porn/audio/ ~/d/dump/porn/audio/
 
     lb fsadd ~/lb/video.db --video --hash --delete-unplayable --check-corrupt --full-scan-if-corrupt 15% --delete-corrupt 20% --move ~/d/check/video/ ~/d/dump/video/
-    lb fsadd ~/lb/audio.db --audio --delete-unplayable --process --move ~/d/check/audio/from_video/ ~/d/dump/video/
+    lb relmv --ext srt,vtt,ass ~/d/dump/video/ ~/d/check/video/
 
+    lb fsadd ~/lb/audio.db --audio --delete-unplayable --process --move ~/d/check/audio/from_video/ ~/d/dump/video/
     lb fsadd ~/lb/audio.db --audio --delete-unplayable --process --move ~/d/check/audio/ ~/d/dump/audio/
 end
