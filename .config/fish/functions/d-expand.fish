42b27169d417c58af8706cee7cab006304489c51
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-04-22 01:43:22 -0500

    d-expand

---
+++ .config/fish/functions/d-expand.fish
@@ -0,0 +1,16 @@
+# Defined via `source`
+function d-expand
+    set path (path resolve "$argv")
+    set -l tasks sync/ dump/ pending/ check/ library/ archive/
+
+    for r in $tasks
+        if string match -r -- $r $path >/dev/null
+            for rn in $tasks
+                set np (string replace -r $r $rn $path)
+                if test -d $np
+                    echo $np
+                end
+            end
+        end
+    end
+end
