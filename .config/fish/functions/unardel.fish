e0deda40a5b3b6fc3e5cf33e80e6cfd00da025a5
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-04-24 01:28:05 -0500

    unardel

--- .config/fish/functions/unardel.fish
+++ .config/fish/functions/unardel.fish
@@ -1,6 +1,6 @@
 function unardel
     for f in $argv
-        /usr/bin/unardel -q -o (path dirname "$f") -d "$f"
+        /usr/bin/unar -q -o (path dirname "$f") -d "$f"
         and rm "$f"
     end
 end
