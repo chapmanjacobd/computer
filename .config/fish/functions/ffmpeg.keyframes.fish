ddb2c0a61c94c87443d07676f84963cbd8f0b3f0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-05-16 13:44:34 +0800

    ffmpeg

--- .config/fish/functions/ffmpeg.keyframes.fish
+++ .config/fish/functions/ffmpeg.keyframes.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function ffmpeg.keyframes
-    ffmpeg -i "$argv" -filter:v "select='eq(pict_type,PICT_TYPE_I)',setpts=N/FRAME_RATE/TB" (path change-extension .keys.mkv "$argv")
+    ffmpeg -i "$argv" -an -sn -filter:v "select='eq(pict_type,PICT_TYPE_I)',setpts=N/FRAME_RATE/TB" (path change-extension .keys.mkv "$argv")
 end
