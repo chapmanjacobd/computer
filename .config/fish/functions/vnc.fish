14e0bcfb77d100db0f06e04b8d2036ac30f8a07e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-05-25 22:21:38 +0800

    vnc

--- .config/fish/functions/vnc.fish
+++ .config/fish/functions/vnc.fish
@@ -1,6 +1,8 @@
 # Defined interactively
 function vnc
     # sync_history
+
+    #vopono -v exec --custom ./custom_openvpn.ovpn --protocol openvpn "firefox"
     if contains pakon $argv
         # ssh -fNT -R localhost:7070:localhost:22 pakon
         ssh -4 xk@pakon -f -L 4102:localhost:4000 'sleep 20'
