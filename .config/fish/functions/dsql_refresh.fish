3fb085034ce5931416fabbf0989a637571de1071
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-05-25 23:16:21 -0500

    dsql_refresh forganize

---
+++ .config/fish/functions/dsql_refresh.fish
@@ -0,0 +1,10 @@
+# Defined interactively
+function dsql_refresh
+    set joblog (mktemp)
+    for i in (seq 1 $MERGERFS_DISKS)
+        for m in /mnt/d$i/*
+            echo lb fsadd --filesystem ~/lb/fs/d$i.db "$m"
+        end
+    end | parallel --shuf --joblog $joblog
+    parallel --retry-failed --joblog $joblog -j1
+end
--- .config/fish/functions/forganize.fish
+++ .config/fish/functions/forganize.fish
@@ -36,11 +36,5 @@ function forganize
     end
     mktree.py ~/d/
 
-    set joblog (mktemp)
-    for i in (seq 1 $MERGERFS_DISKS)
-        for m in /mnt/d$i/*
-            echo lb fsadd --filesystem ~/lb/fs/d$i.db "$m"
-        end
-    end | parallel --shuf --joblog $joblog
-    parallel --retry-failed --joblog $joblog -j1
+    dsql_refresh
 end
