0a44f371419460a1e7e7e424f61f128bda15dfaf
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-05-28 00:56:37 +0800

    dupreplace

--- bin/dupreplace.fish
+++ bin/dupreplace.fish
@@ -6,12 +6,4 @@ set duplicate $argv[1]
 set original $argv[2]
 
 rm "$duplicate"
-
-for n in (seq 1 $MERGERFS_DISKS)
-    set src (echo "$original" | sed "s|/mnt/d/|/mnt/d$n/|")
-    if test -e "$src"
-        set dest (echo "$duplicate" | sed "s|/mnt/d/|/mnt/d$n/|")
-        mkdir (path dirname "$dest")
-        cp --reflink=always "$src" "$dest"
-    end
-end
+lb mergerfs-cp "$original" "$duplicate"
