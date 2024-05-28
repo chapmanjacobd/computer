e24419f8b88909b3f59f3d1d7ac3e875cae52526
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-05-28 23:05:44 +0800

    lbupgrade

--- .config/fish/functions/lbupgrade.fish
+++ .config/fish/functions/lbupgrade.fish
@@ -1,6 +1,7 @@
-# Defined interactively
+# Defined via `source`
 function lbupgrade
     for pc in pakon backup pulse15
         ssh $pc pip install --upgrade xklb
+        or pip install --upgrade xklb
     end
 end
