45ffca3ef447eb917acae0c647d2f446904dedee
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-06-01 04:23:11 -0500

    torganize

--- .config/fish/functions/torganize.fish
+++ .config/fish/functions/torganize.fish
@@ -6,9 +6,9 @@ function torganize
     rsync -auh --remove-sent-files ~/Downloads/\[OldToons*.torrent backup:.local/data/qbittorrent/seed/oldtoons_world/
     rsync -auh --remove-sent-files ~/Downloads/\[CarPT*.torrent backup:.local/data/qbittorrent/seed/carpt.net/
 
-    rsync -auh --remove-sent-files ~/Downloads/\[HappyFappy*.torrent backup:.local/data/qbittorrent/porn/happyfappy/
-    rsync -auh --remove-sent-files ~/Downloads/\[pornolab*.torrent backup:.local/data/qbittorrent/porn/pornolab/
-    rsync -auh --remove-sent-files ~/Downloads/\[exoticaz*.torrent backup:.local/data/qbittorrent/porn/exoticaz/
+    mv ~/Downloads/\[HappyFappy*.torrent ~/.local/data/qbittorrent/porn/happyfappy/
+    mv ~/Downloads/\[pornolab*.torrent ~/.local/data/qbittorrent/porn/pornolab/
+    mv ~/Downloads/\[exoticaz*.torrent ~/.local/data/qbittorrent/porn/exoticaz/
 
     rsync -auh --remove-sent-files ~/Downloads/*.torrent backup:.local/data/qbittorrent/seed_pause/others/
 end
