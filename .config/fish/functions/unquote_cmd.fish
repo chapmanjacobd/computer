42b691b0f1d22262f7da7188655a3c70dada4e43
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-06-04 11:46:24 +0800

    unquote_cmd

---
+++ .config/fish/functions/unquote_cmd.fish
@@ -0,0 +1,7 @@
+function unquote_cmd
+    if test (count $argv) -eq 0
+        python -c "from shlex import join; from ast import literal_eval; import sys; print(join(literal_eval(sys.stdin.read())))"
+    else
+        python -c "from shlex import join; from ast import literal_eval; import sys; print(join(literal_eval('$argv')))"
+    end
+end
