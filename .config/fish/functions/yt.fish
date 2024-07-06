960705531a25a89f14cdfc40ea3c40b7280af418
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-07-06 11:10:52 +0800

    yt

--- .config/fish/functions/yt.fish
+++ .config/fish/functions/yt.fish
@@ -1,5 +1,5 @@
 function yt -w yt-dlp
-    ytdl -i -f bestvideo[height<=576]+bestaudio/best[height<=576]/best --write-sub --write-auto-sub \
+    ytdl -i --format-sort res:576 --check-formats --format '(bestvideo[filesize<2G]+bestaudio/best[filesize<2G]/bestvideo*+bestaudio/best)[format_id!$=-drc][dynamic_range!^=HDR]/bestvideo[filesize<2G]+bestaudio/best[filesize<2G]/bestvideo*+bestaudio/best' --write-sub --write-auto-sub \
         --sub-lang 'en,EN,en.*,en-*,EN.*,EN-*eng,ENG,english,English,ENGLISH' \
         --embed-subs --compat-options no-keep-subs \
         --match-filter "duration >? 59 & duration <? 14399 & live_status=?not_live" \
