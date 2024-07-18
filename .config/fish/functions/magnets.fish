c71165784ecdc85df1e062f822078ae0626209c2
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-07-18 07:44:55 -0500

    magnets

--- .config/fish/functions/magnets.fish
+++ .config/fish/functions/magnets.fish
@@ -5,19 +5,19 @@ function magnets
     combine ~/.local/magnets not ~/.local/magnets_history | sponge ~/.local/magnets
     cat ~/.local/magnets >>~/.local/magnets_history
 
-        pgrep tixati >/dev/null
-        set tixati_is_running $status
-        if not test $tixati_is_running -eq 0
-            tixati &
-            sleep 5
-        end
+    pgrep tixati >/dev/null
+    set tixati_is_running $status
+    if not test $tixati_is_running -eq 0
+        tixati &
+        sleep 5
+    end
 
-        cat ~/.local/magnets | string escape | tee /dev/tty | xargs tixati
-        and true >~/.local/magnets
+    cat ~/.local/magnets | string escape | tee /dev/tty | xargs tixati
+    and true >~/.local/magnets
 
-        if not test $tixati_is_running -eq 0
-            sleep 5
-            pkill tixati
-        end
+    if not test $tixati_is_running -eq 0
+        sleep 5
+        pkill tixati
+    end
 
 end
