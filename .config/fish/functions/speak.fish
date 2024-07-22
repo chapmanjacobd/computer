58283339afdd01c888320fc04751a96c4c43849d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-07-22 22:29:07 +0800

    speak

--- .config/fish/functions/speak.fish
+++ .config/fish/functions/speak.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function speak
-    speak-ng -v gmw/en "$argv"
+    piper --model ~/.local/share/models/piper/en_US-joe-medium.onnx --output-raw | aplay -r 22050 -f S16_LE -t raw -
 end
