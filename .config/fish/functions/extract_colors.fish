b347ecb0638f45d948299caeff8b2c4410cba590
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-07-22 09:19:40 +0800

    extract_colors

---
+++ .config/fish/functions/extract_colors.fish
@@ -0,0 +1,14 @@
+# Defined via `source`
+function extract_colors
+    for space in sRGB RGB HSV LAB
+        set -l temp_file (mktemp)
+
+        magick "$argv" -quantize "$space" +dither -colors 6 "$temp_file.tiff"
+
+        echo "Histogram in $space colorspace:"
+        magick "$temp_file.tiff" -format %c histogram:info:- | tee "$temp_file.txt"
+        pastel color (sed -nre 's/.*(#[0-9A-F]{6,8}) .*/\1/p' "$temp_file.txt")
+
+        rm "$temp_file.tiff" "$temp_file.txt"
+    end
+end
