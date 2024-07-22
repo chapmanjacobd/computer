05177908f2c0751575c5c1cd23fada3b8a580a8c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-07-22 23:33:38 +0800

    speak

--- .config/fish/functions/speak.fish
+++ .config/fish/functions/speak.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function speak
-    piper --model ~/.local/share/models/piper/en_US-joe-medium.onnx --output-raw | mpv --demuxer=rawaudio --demuxer-rawaudio-rate=22050 --demuxer-rawaudio-channels=1 --demuxer-rawaudio-format=s16le -
+    piper --model ~/.local/share/models/piper/en_US-joe-medium.onnx --output-raw | mpv --demuxer=rawaudio --demuxer-rawaudio-rate=22050 --demuxer-rawaudio-channels=1 --demuxer-rawaudio-format=s16le --no-resume-playback -
 end
