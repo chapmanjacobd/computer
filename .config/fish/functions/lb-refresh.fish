ca74d9c26de12436f05afa8abcc317fc9a16f629
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-07-23 18:25:02 +0800

    lb-refresh

--- .config/fish/functions/lb-refresh.fish
+++ .config/fish/functions/lb-refresh.fish
@@ -1,7 +1,7 @@
 # Defined interactively
 function lb-refresh
-    lb fsadd ~/lb/video.db --video ~/d/sync/video/
     lb fsadd ~/lb/audio.db --audio ~/d/sync/audio/
+    lb fsadd ~/lb/video.db --video ~/d/sync/video/
 
     lb fsadd ~/lb/tax.db --video ~/d/sync/porn/video/
     lb fsadd ~/lb/tax_sounds.db --audio ~/d/sync/porn/audio/
