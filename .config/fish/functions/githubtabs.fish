8462c9fc43dabc86703acdffa7caddbb7285ebb8
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-07-23 13:46:14 +0800

    githubtabs

---
+++ .config/fish/functions/githubtabs.fish
@@ -0,0 +1,13 @@
+# Defined via `source`
+function githubtabs
+    for user in (cb | sed "s|https://github.com/\([^/?]*\)/\?.*|\1|")
+
+        for lang in rust c lua shell python c%2B%2B c%23 markdown html
+            printf '%s\n' https://github.com/$user?language=$lang&tab=stars
+        end
+
+        echo "https://github.com/$user?tab=repositories&type=source
+https://gist.github.com/$user
+https://gist.github.com/$user/starred"
+    end | lb linksdb ~/mc/links.db -c p1 --skip-extract -
+end
