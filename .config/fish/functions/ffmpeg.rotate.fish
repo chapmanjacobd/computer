c552d3c5fa4fd08586d13a62286b25dd0b1916c0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-07-24 15:15:28 +0800

    ffmpeg

---
+++ .config/fish/functions/ffmpeg.rotate.fish
@@ -0,0 +1,5 @@
+# Defined interactively
+function ffmpeg.rotate
+    set deg (coalesce $argv[2] -90)
+    ffmpeg -i $argv[1] -metadata:s:v rotate="$deg" -codec copy (path change-extension '' $argv[1]).rotated.(path extension $argv[1])
+end
