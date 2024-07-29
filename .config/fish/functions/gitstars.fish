e36942c46f00259e91f9ea621cd5b9a202910419
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-07-29 12:31:39 -0500

    gitstars

--- .config/fish/functions/gitstars.fish
+++ .config/fish/functions/gitstars.fish
@@ -1,6 +1,9 @@
 # Defined via `source`
 function gitstars
-    for lang in rust c lua shell python c%2B%2B c%23 markdown html
-        firefox --new-tab $argv?language=$lang&tab=stars
+    for user in (cb | sed "s|https://github.com/\([^/?]*\)/\?.*|\1|")
+
+        for lang in rust c lua shell python c%2B%2B c%23 markdown html
+            printf '%s\n' https://github.com/$user?language=$lang&tab=stars
+        end
     end
 end
