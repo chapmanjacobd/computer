d0a5492f28f9de061b5ec785f244e9db35590887
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-03 12:42:51 -0500

    mergerfs_readlink

--- .config/fish/functions/mergerfs_readlink.fish
+++ .config/fish/functions/mergerfs_readlink.fish
@@ -1,7 +1,7 @@
 # Defined interactively
 function mergerfs_readlink
     for n in (seq 1 $MERGERFS_DISKS)
-        set f (echo $argv | sed "s|/mnt/d/|/mnt/d$n/|")
+        set f (path resolve $argv | sed "s|/mnt/d/|/mnt/d$n/|")
         if test -e $f
             echo $f
         end
