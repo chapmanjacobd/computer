cb35c1126eabd8312b88e5aa49e3243597313545
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-08 07:13:13 +0800

    vnc

--- .config/fish/functions/vnc.fish
+++ .config/fish/functions/vnc.fish
@@ -5,7 +5,7 @@ function vnc
     #vopono -v exec --custom ./custom_openvpn.ovpn --protocol openvpn "firefox"
     if contains pakon $argv
         # ssh -fNT -R localhost:7070:localhost:22 pakon
-        ssh -4 xk@pakon -f -L 4102:localhost:4000 'sleep 20'
+        # ssh -4 xk@pakon -f -L 4102:localhost:4000 'sleep 20'
         /usr/NX/bin/nxplayer --session ~/.ssh/pakon.nxs
     end
     if contains backup $argv
