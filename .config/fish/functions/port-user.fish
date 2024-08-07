69077339e8b68706c94b8e2b2cbde51cb66ab823
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-07 18:17:21 -0500

    port-user vnc

--- .config/fish/functions/port-user.fish
+++ .config/fish/functions/port-user.fish
@@ -1,5 +1,5 @@
 function port-user
-    set -l pid (lsof -t -i:$argv[1])
+    set -l pid (sudo lsof -t -i:$argv[1])
     if count $pid >/dev/null
         set -l cmd (ps -p $pid -o command | tail -n 1)
         ps -p $pid -o command
