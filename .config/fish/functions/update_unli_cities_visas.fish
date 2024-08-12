e2785cdd63215732ef7f8082916e477270aee9a2
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-12 23:42:09 +0800

    update_unli_cities_visas

--- .config/fish/functions/update_unli_cities_visas.fish
+++ .config/fish/functions/update_unli_cities_visas.fish
@@ -1,5 +1,5 @@
 # Defined interactively
 function update_unli_cities_visas
-    ssh unli.xyz -t sudo su admin -c '"cd /home/production/admin/random-city-video-api/; pipenv run python ./update_visa.py"'
+    ssh unli.xyz -t sudo su admin -c '"cd /home/production/admin/random-city-video-api/; LC_ALL=en_US.UTF-8 ~/.local/bin/pipenv run python ./update_visa.py"'
     ssh unli.xyz -t sudo su -c '"systemctl restart scarlette"'
 end
