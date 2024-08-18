642600f2f234d1f964b697225f290a406898d65b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-18 23:12:02 +0800

    laptop2880

--- .config/fish/functions/laptop2880.fish
+++ .config/fish/functions/laptop2880.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function laptop2880
-    sudo xrandr --newmode "2880x1726_60.00" 165.25 1920 2040 2240 2560 1038 1041 1051 1077 -hsync +vsync
+    sudo xrandr --newmode "2880x1726_60.00" 423.75  2880 3104 3416 3952  1726 1729 1739 1789 -hsync +vsync
     sudo xrandr --addmode $argv "2880x1726_60.00"
     xrandr --output $argv --mode "2880x1726_60.00"
 end
