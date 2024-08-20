fdc7d663f57dd1657a654a1c0b1cfc200c272f54
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-20 23:35:28 +0800

    rdp

--- .config/fish/functions/rdp.fish
+++ .config/fish/functions/rdp.fish
@@ -1,4 +1,4 @@
-# Defined interactively
+# Defined via `source`
 function rdp
-    sdl-freerdp -authentication /v:localhost:35589 +clipboard /network:auto /rfx /gfx:rfx /floatbar:sticky:off +fonts /bpp:32 /audio-mode:0 +aero +window-drag /size:2880x1724 /tune:FreeRDP_HiDefRemoteApp:true,FreeRDP_GfxAVC444v2:true,FreeRDP_GfxH264:true
+    sdl-freerdp -authentication /v:localhost:35589 +clipboard /network:auto /rfx /gfx:rfx /floatbar:sticky:off +fonts /bpp:32 /audio-mode:0 +aero +window-drag /smart-sizing:2880x1724 /window-position:0x0 /tune:FreeRDP_HiDefRemoteApp:true,FreeRDP_GfxAVC444v2:true,FreeRDP_GfxH264:true
 end
