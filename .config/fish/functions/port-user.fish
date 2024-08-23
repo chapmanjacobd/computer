356024ef096de20e574d0fe1bebe1ea10d464861
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-23 21:39:13 +0800

    laptop2880 port-user

--- .config/fish/functions/port-user.fish
+++ .config/fish/functions/port-user.fish
@@ -2,8 +2,8 @@ function port-user
     set -l pid (sudo lsof -t -i:$argv[1])
     if count $pid >/dev/null
         for id in $pid
-        set -l cmd (ps -p $id -o command | tail -n 1)
-        printf "%s\t%s\n" $id $cmd
+            set -l cmd (ps -p $id -o command | tail -n 1)
+            printf "%s\t%s\n" $id $cmd
         end
     else
         echo nope
