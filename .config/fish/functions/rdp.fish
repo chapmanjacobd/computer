4c0d6b78662cb43528344b05520c9a2b3ca2b861
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-24 22:11:31 +0800

    rdp

--- .config/fish/functions/rdp.fish
+++ .config/fish/functions/rdp.fish
@@ -1,4 +1,12 @@
 # Defined via `source`
 function rdp
-    xfreerdp -authentication +clipboard +auto-reconnect -audio -decorations /v:/run/user/1000/rdp /network:broadband-low /clipboard:direction-to:all /smart-sizing /workarea /rfx /gfx:AVC420,thin-client,progressive,rfx /compression-level:2
+    expect -c '
+set timeout -1
+spawn xfreerdp -authentication +clipboard +auto-reconnect -audio -decorations /v:/run/user/1000/rdp /network:broadband-low /clipboard:direction-to:all /smart-sizing /workarea /rfx /gfx:AVC420,thin-client,progressive,rfx /compression-level:2
+expect "Domain:"
+send "\r"
+expect "Password:"
+send "\r"
+expect eof
+'
 end
