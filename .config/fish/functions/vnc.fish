d246e61196b907b23a33f236903b5549af2f926f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-24 14:05:43 +0800

    rdp vnc

--- .config/fish/functions/vnc.fish
+++ .config/fish/functions/vnc.fish
@@ -5,8 +5,8 @@ function vnc
     #vopono -v exec --custom ./custom_openvpn.ovpn --protocol openvpn "firefox"
     if contains pakon $argv
         # ssh -fNT -R localhost:7070:localhost:22 pakon
-        ssh -4 xk@pakon -L /run/user/1000/rdp:/run/user/1000/rdp 'sleep 1'
-        b ssh -4 xk@pakon -L /run/user/1000/rdp:/run/user/1000/rdp 'sleep 20'
+        ssh -4 xk@pakon -L 35589:localhost:35589 'sleep 1'
+        b ssh -4 xk@pakon -L 35589:localhost:35589 'sleep 20'
         sleep 1
         rdp
     end
