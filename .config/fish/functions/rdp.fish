2bae8125738365d11aa4cd4dfc6d202602c1b238
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-24 04:45:36 +0000

    use ipc socket

--- .config/fish/functions/rdp.fish
+++ .config/fish/functions/rdp.fish
@@ -1,4 +1,4 @@
 # Defined via `source`
 function rdp
-    sdl-freerdp -authentication /v:localhost:35589 +clipboard /network:broadband-low -audio /smart-sizing /workarea +auto-reconnect /rfx /gfx:AVC420,thin-client,progressive,rfx -encryption -decorations /compression-level:2
+    sdl-freerdp -authentication -encryption /v:/run/user/1000/rdp +clipboard /network:broadband-low -audio /smart-sizing /workarea +auto-reconnect /rfx /gfx:AVC420,thin-client,progressive,rfx -encryption -decorations /compression-level:2
 end
