12581093a5800195cdd462e0f3b6ddb4465570cf
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-24 01:04:30 -0500

    rdpserve

--- .config/fish/functions/rdpserve.fish
+++ .config/fish/functions/rdpserve.fish
@@ -1,5 +1,4 @@
 # Defined interactively
 function rdpserve
-    rm /run/user/1000/rdp
-    freerdp-shadow-cli -auth /ipc-socket:/run/user/1000/rdp
+    freerdp-shadow-cli -auth /port:35589 /bind-address:127.0.0.1
 end
