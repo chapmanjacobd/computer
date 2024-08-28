f62513a47df434f9dbe5827112938644a34b7532
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-28 22:21:06 +0800

    rdp

--- .config/fish/functions/rdp.fish
+++ .config/fish/functions/rdp.fish
@@ -2,7 +2,7 @@
 function rdp
     expect -c '
 set timeout -1
-spawn xfreerdp -authentication +clipboard +auto-reconnect -audio -decorations /v:/run/user/1000/rdp /network:broadband-low /clipboard:direction-to:all /smart-sizing /workarea /rfx /gfx:AVC420,thin-client,progressive,rfx /compression-level:2
+spawn xfreerdp -authentication +clipboard +auto-reconnect -audio -decorations /v:/run/user/1000/rdp /network:broadband-low /clipboard:direction-to:all /smart-sizing /workarea /rfx /gfx:AVC420,thin-client,progressive,rfx -mouse-motion # +mouse-relative
 expect "Domain:"
 send "\r"
 expect "Password:"
