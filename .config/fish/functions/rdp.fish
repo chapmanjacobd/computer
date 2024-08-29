7756f7f4eeacfb15e15500f56ce3f52d0937635e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-08-29 13:18:23 +0800

    rdp rdpserve vnc autossh@len

--- .config/fish/functions/rdp.fish
+++ .config/fish/functions/rdp.fish
@@ -2,7 +2,7 @@
 function rdp
     expect -c '
 set timeout -1
-spawn xfreerdp -authentication +clipboard +auto-reconnect -audio -decorations /v:/run/user/1000/rdp /network:broadband-low /clipboard:direction-to:all /smart-sizing /workarea /rfx /gfx:AVC420,thin-client,progressive,rfx -mouse-motion # +mouse-relative
+spawn xfreerdp -authentication +clipboard +auto-reconnect -audio -decorations /v:localhost:35589 /network:broadband-low /clipboard:direction-to:all /smart-sizing /workarea /rfx /gfx:AVC420,thin-client,progressive,rfx +mouse-relative
 expect "Domain:"
 send "\r"
 expect "Password:"
