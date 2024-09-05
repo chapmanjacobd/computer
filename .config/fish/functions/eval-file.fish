4265044df37a8d4a0da8965c21abea1852aa639a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-09-05 19:22:30 +0800

    eval-file

--- .config/fish/functions/eval-file.fish
+++ .config/fish/functions/eval-file.fish
@@ -1,6 +1,6 @@
 function eval-file
     for j in (cat $argv)
         echo $j
-        eval $j
+        systemd-run --user -p RuntimeMaxSec=10h -p MemoryMax=10G -p MemorySwapMax=20G --same-dir --wait --collect --service-type=exec -- fish -c "$j"
     end
 end
