59bb0457e1bfd8620c167aa881019622530a5e37
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-09-08 00:57:59 +0800

    brightness_set

--- .config/fish/functions/brightness_set.fish
+++ .config/fish/functions/brightness_set.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function brightness_set
-    set min 1
+    set min 2
     set max (qdbus local.org_kde_powerdevil /org/kde/Solid/PowerManagement/Actions/BrightnessControl brightnessMax)
     qdbus local.org_kde_powerdevil /org/kde/Solid/PowerManagement/Actions/BrightnessControl setBrightnessSilent (math min $max,(math max $min,$argv))
 end
