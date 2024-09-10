37718757de042f4109c5db4c5a25f4d7f8ff5378
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-09-10 21:12:54 +0800

    str_rgb

--- bin/str_rgb.py
+++ bin/str_rgb.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import colorsys
 import hashlib
 import sys
 
@@ -11,31 +12,8 @@ def string_to_rgb(input_string):
     saturation = 30 + (hash_int // 360) % 50  # Saturation is between 30 and 79
     lightness = 30 + (hash_int // (360 * 50)) % 50  # Lightness is between 30 and 79
 
-    r, g, b = hsl_to_rgb(hue, saturation, lightness)
+    r, g, b = colorsys.hls_to_rgb(hue / 360, lightness / 100, saturation / 100)
 
-    return r, g, b
-
-def hsl_to_rgb(h, s, l):
-    # Normalize HSL values
-    h = h / 360.0
-    s = s / 100.0
-    l = l / 100.0
-
-    def hue_to_rgb(p, q, t):
-        if t < 0: t += 1
-        if t > 1: t -= 1
-        if t < 1/6: return p + (q - p) * 6 * t
-        if t < 1/2: return q
-        if t < 2/3: return p + (q - p) * (2/3 - t) * 6
-        return p
-
-    q = l * (1 + s) if l < 0.5 else l + s - l * s
-    p = 2 * l - q
-    r = hue_to_rgb(p, q, h + 1/3)
-    g = hue_to_rgb(p, q, h)
-    b = hue_to_rgb(p, q, h - 1/3)
-
-    # Convert RGB values to integers in the range 0-255
     r = int(r * 255)
     g = int(g * 255)
     b = int(b * 255)
