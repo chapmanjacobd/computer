722f481b877f48a27ead275096cf4b756d630d89
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-09-15 00:36:12 -0500

    log gitignore

--- .config/fish/functions/log.fish
+++ .config/fish/functions/log.fish
@@ -1,3 +1,3 @@
 function log -w journalctl
-    journalctl --no-hostname --reverse -a $argv | grep -v -E 'postfix|Firewall|pam|libva|kioslave|ddcutil|sshd|audit|kwin_core: Applying KScreen config|vsce-sign|ETA: |/dev/tty: No such device or address|making sync progress|dnscrypt-proxy' | less -FSRXc
+    journalctl --no-hostname -n5000 -a $argv | grep -v -E 'postfix|Firewall|pam|libva|kioslave|ddcutil|sshd|audit|kwin_core: Applying KScreen config|vsce-sign|ETA: |/dev/tty: No such device or address|making sync progress|dnscrypt-proxy' | less -FSRXc +G
 end
