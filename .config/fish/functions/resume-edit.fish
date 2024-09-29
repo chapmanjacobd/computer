e18d2cb0b71d6b18f704e1c87223621659b56eda
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-09-29 15:28:59 +0800

    resume-edit

--- .config/fish/functions/resume-edit.fish
+++ .config/fish/functions/resume-edit.fish
@@ -2,7 +2,7 @@
 function resume-edit
     cd ~/github/o/resume/
     code .
-    while inotifywait -e modify inputs/resume.yaml
+    while inotifywait -e modify inputs/resume.yaml templates/*/*
         python generate.py
     end
 end
