c282eca6751fa68cea53b4df1718029bc618b00a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-10-02 22:14:34 +0800

    search_url

--- .config/fish/functions/search_url.fish
+++ .config/fish/functions/search_url.fish
@@ -1,8 +1,11 @@
-# Defined interactively
+# Defined via `source`
 function search_url --description 'Expand a search URL'
     set -l search_url $argv[1]
+
     for query in $argv[2..-1]
-        set -l encoded_query (string escape --style=url $query)
+        # Replace unwanted characters with an empty string
+        set -l cleaned_query (string replace -ra '[()\[\]'"',]+" '' $query)
+        set -l encoded_query (string escape --style=url $cleaned_query)
         echo (string replace "%s" $encoded_query $search_url)
     end
 end
