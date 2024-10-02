9348f79646cd05b65ca9f428507f9ce8ad7f02b8
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-10-03 00:14:58 +0800

    resume-edit with_unli

--- .config/fish/functions/resume-edit.fish
+++ .config/fish/functions/resume-edit.fish
@@ -4,5 +4,7 @@ function resume-edit
     code .
     while inotifywait -e modify inputs/resume.yaml templates/*/*
         python generate.py
+        and cp outputs/jchapman_resume.pdf ~/sync/self/credentials/Jacob_Chapman.pdf
     end
+    and with_unli cp ~/sync/self/credentials/Jacob_Chapman.pdf ~/.mnt/web/home/production/admin/public_html/resume.pdf
 end
