c22d01a884364191bf3481c07b4c1d90d6eefe56
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-10-13 16:52:03 +0800

    timers-status

--- .config/fish/functions/timers-status.fish
+++ .config/fish/functions/timers-status.fish
@@ -1,9 +1,11 @@
 # Defined interactively
 function timers-status
     for service in (systemctl --user list-timers --all --no-pager --no-legend --state=running | awk '{print $NF}')
-        begin
-            pstree -a -p (systemd-pid-u $service)
-            timer-status $service
-        end | less -FSRXc
+        if not contains $service -- firefox.service
+            begin
+                pstree -a -p (systemd-pid-u $service)
+                timer-status $service
+            end | less -FSRXc
+        end
     end
 end
