c4814dcb0134428989e13a794507b4e4c0d2250c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-10-14 22:25:30 -0500

    scatter

--- .config/fish/functions/scatter.fish
+++ .config/fish/functions/scatter.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function scatter
     for i in (seq 1 10)
-        lb mv (df | sort --human-numeric-sort -k4 | grep /mnt/d | awk '{print $6}' | head -1)$argv (df | sort --human-numeric-sort -k4 | grep /mnt/d | awk '{print $6}' | tail -1)$argv -TS 2Gi
+        python -m xklb.lb mv (df | sort --human-numeric-sort -k4 | grep /mnt/d | awk '{print $6}' | head -1)$argv (df | sort --human-numeric-sort -k4 | grep /mnt/d | awk '{print $6}' | tail -n 2 | head -1)$argv -v -l 1
     end
 end
