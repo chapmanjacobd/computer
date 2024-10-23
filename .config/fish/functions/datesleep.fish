0d5dd976c168139adab1bbb6d38082b6481d070f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-10-23 18:10:59 +0800

    datesleep

---
+++ .config/fish/functions/datesleep.fish
@@ -0,0 +1,8 @@
+# Defined interactively
+function datesleep --argument target_date
+    set current_date (date --iso-8601=seconds)
+
+    set sleep_seconds (math (date -d "$target_date" +%s) - (date -d "$current_date" +%s))
+    echo sleeping for $sleep_seconds
+    sleep $sleep_seconds
+end
