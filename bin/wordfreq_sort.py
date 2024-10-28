65d1469c8d24d7b00c9f5fc9497c93df34e354a3
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-10-28 22:40:02 +0800

    abbreviations wordfreq_sort

---
+++ bin/wordfreq_sort.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+from wordfreq import zipf_frequency
+
+
+def main():
+    input_words = sys.stdin.read().splitlines()
+
+    sorted_words = sorted(input_words, key=lambda word: zipf_frequency(word, 'en'), reverse=True)
+
+    for word in sorted_words:
+        print(word)
+
+
+if __name__ == "__main__":
+    main()
