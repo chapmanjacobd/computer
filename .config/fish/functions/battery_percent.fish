625f7d8408488a9520b9ef51bb0b63c2820ee622
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-10-30 12:01:06 +0800

    battery_percent low_battery_percent dnf_installed

--- .config/fish/functions/battery_percent.fish
+++ .config/fish/functions/battery_percent.fish
@@ -1,4 +1,8 @@
 # Defined interactively
 function battery_percent
+    if set -q CONTAINER_ID; set -q container
+        return
+    end
+
     upower -i (upower -e | grep 'BAT') | grep percentage | awk '{print $2}'
 end
--- .config/fish/functions/low_battery_percent.fish
+++ .config/fish/functions/low_battery_percent.fish
@@ -2,10 +2,10 @@
 function low_battery_percent
     set battery_percentage (battery_percent)
 
-    if test (echo $battery_percentage | sed 's/%//') -lt 60
-        echo $battery_percentage
-    else
-        echo ""
+    if test -n "$battery_percentage"
+        if test (echo $battery_percentage | sed 's/%//') -lt 60
+            echo $battery_percentage
+        end
     end
 
 end
