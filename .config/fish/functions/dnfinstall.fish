6bd04db82132e823f5e54026a2036284f7b254e0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-11-01 15:54:13 +0800

    battery_percent dnfinstall

--- .config/fish/functions/dnfinstall.fish
+++ .config/fish/functions/dnfinstall.fish
@@ -1,7 +1,11 @@
-# Defined interactively
 function dnfinstall --wraps='dnf install'
-    sudo dnf install $argv
-    and echo $argv | string split ' ' >>~/.github/dnf_installed
-
-    sort --unique --ignore-case ~/.github/dnf_installed | sponge ~/.github/dnf_installed
+    if command -v rpm-ostree > /dev/null
+        sudo rpm-ostree install --apply-live $argv
+        and echo $argv | string split ' ' >>~/.github/ostree_installed
+        sort --unique --ignore-case ~/.github/ostree_installed | sponge ~/.github/ostree_installed
+    else
+        sudo dnf install $argv
+        and echo $argv | string split ' ' >>~/.github/dnf_installed
+        sort --unique --ignore-case ~/.github/dnf_installed | sponge ~/.github/dnf_installed
+    end
 end
