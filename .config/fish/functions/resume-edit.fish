cca26afba478bfdcf2d52050823fa468d971d502
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-11-07 11:47:27 -0600

    resume-edit

--- .config/fish/functions/resume-edit.fish
+++ .config/fish/functions/resume-edit.fish
@@ -2,10 +2,11 @@
 function resume-edit
     cd ~/github/o/resume/
     code .
+
     open outputs/jchapman_resume.pdf
     while inotifywait -e modify inputs/resume.yaml templates/*/*
         python generate.py
+        gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$HOME/sync/self/credentials/Jacob_Chapman.pdf" ~/github/o/resume/outputs/jchapman_resume.pdf
+        with_unli cp ~/sync/self/credentials/Jacob_Chapman.pdf ~/.mnt/web/home/production/admin/public_html/resume.pdf
     end
-    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$HOME/sync/self/credentials/Jacob_Chapman.pdf" outputs/jchapman_resume.pdf
-    with_unli cp ~/sync/self/credentials/Jacob_Chapman.pdf ~/.mnt/web/home/production/admin/public_html/resume.pdf
 end
