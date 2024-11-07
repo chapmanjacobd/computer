6a1c5482e60100647a01cc36275301afc52ac126
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-11-07 09:51:44 -0600

    maximize_all

--- .config/fish/functions/maximize_all.fish
+++ .config/fish/functions/maximize_all.fish
@@ -4,4 +4,7 @@ function maximize_all
         wmctrl -i -r $win -b toggle,maximized_horz,maximized_vert
         wmctrl -i -r $win -b add,maximized_horz,maximized_vert
     end
+
+    krohnkite_off
+    krohnkite_on
 end
