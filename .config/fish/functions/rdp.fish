a2f009fc9452733a6e8f5045ef9e1a487d876c07
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-11-11 10:58:56 -0600

    maximize_all rdp

--- .config/fish/functions/rdp.fish
+++ .config/fish/functions/rdp.fish
@@ -2,7 +2,7 @@
 function rdp
     expect -c '
 set timeout -1
-spawn xfreerdp -authentication +auto-reconnect /network:auto -audio -decorations /v:'$argv' /smart-sizing /workarea /clipboard /gfx:avc420,mask:0x1e0
+spawn xfreerdp -authentication +auto-reconnect -audio -decorations /gdi:sw /network:auto /v:'$argv' /workarea /smart-sizing /clipboard
 expect "Domain:"
 send "\r"
 expect "Password:"
