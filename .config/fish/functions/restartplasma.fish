db93f9d314d8137e195fadbd8ed2246c25226b15
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-11-14 12:19:17 -0600

    restartplasma

--- .config/fish/functions/restartplasma.fish
+++ .config/fish/functions/restartplasma.fish
@@ -1,5 +1,5 @@
 # Defined interactively
 function restartplasma
-    bash -c 'kquitapp5 plasmashell || killall plasmashell; systemd-run --user kstart5 plasmashell'
+    bash -c 'kquitapp6 plasmashell || killall plasmashell; systemd-run --user kstart plasmashell'
     systemd-run --user kwin_x11 --replace
 end
