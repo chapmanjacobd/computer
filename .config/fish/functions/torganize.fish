a363134783b230d88be4a5422d19b67271549317
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-11-16 09:13:43 -0600

    torganize

--- .config/fish/functions/torganize.fish
+++ .config/fish/functions/torganize.fish
@@ -1,14 +1,18 @@
 # Defined interactively
 function torganize
     rsync -auh --remove-sent-files ~/Downloads/\[(seq 0 9)*.torrent backup:.local/data/rtorrent/watch/new/
-    rsync -auh --remove-sent-files ~/Downloads/*jptv.club*.torrent backup:.local/data/jptv.club/
 
-    rsync -auh --remove-sent-files ~/Downloads/\[OldToons*.torrent backup:.local/data/qbittorrent/seed/oldtoons_world/
-    rsync -auh --remove-sent-files ~/Downloads/\[CarPT*.torrent backup:.local/data/qbittorrent/seed/carpt.net/
+    #rsync -auh --remove-sent-files ~/Downloads/*jptv.club*.torrent backup:.local/data/jptv.club/
+    #rsync -auh --remove-sen-files ~/Downloads/\[OldToons*.torrent backup:.local/data/qbittorrent/seed/oldtoons_world/
+    #rsync -auh --remove-sent-files ~/Downloads/\[CarPT*.torrent backup:.local/data/qbittorrent/seed/carpt.net/
+    mv ~/Downloads/*jptv.club*.torrent ~/.local/data/qbittorrent/seed/jptv.club/
+    mv ~/Downloads/\[OldToons*.torrent ~/.local/data/qbittorrent/seed/OTW/
+    mv ~/Downloads/\[CarPT*.torrent ~/.local/data/qbittorrent/seed/carpt.net/
 
     mv ~/Downloads/\[HappyFappy*.torrent ~/.local/data/qbittorrent/porn/happyfappy/
     mv ~/Downloads/\[pornolab*.torrent ~/.local/data/qbittorrent/porn/pornolab/
     mv ~/Downloads/\[exoticaz*.torrent ~/.local/data/qbittorrent/porn/exoticaz/
 
-    rsync -auh --remove-sent-files ~/Downloads/*.torrent backup:.local/data/qbittorrent/seed_pause/others/
+    mv ~/Downloads/*.torrent ~/.local/data/qbittorrent/seed/others/
+    #rsync -auh --remove-sent-files ~/Downloads/*.torrent backup:.local/data/qbittorrent/seed_pause/others/
 end
