ba5ff7c0d481164f14ebfb72500205e8866512bb
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-11-17 14:21:16 -0600

    tubeupdate

--- .config/fish/functions/tubeupdate.fish
+++ .config/fish/functions/tubeupdate.fish
@@ -2,6 +2,6 @@
 function tubeupdate
     for db in ~/lb/video.db ~/lb/audio.db ~/lb/tax_sounds.db ~/lb/tax.db
         lb dedupe-db $db playlists --bk extractor_playlist_id
-        lb tubeupdate $db
+        sleepy_run.py -- python -u -m xklb.lb tubeupdate $db
     end
 end
