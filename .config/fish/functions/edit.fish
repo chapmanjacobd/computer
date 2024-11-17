46cabaccb750ad6a8598f6c207fd3dc279e290b0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-11-17 11:09:20 -0600

    edit

--- .config/fish/functions/edit.fish
+++ .config/fish/functions/edit.fish
@@ -2,7 +2,7 @@
 function edit
     set sp (path normalize ~/.github/"$argv")
 
-    sudo nano "$argv"
+    sudoedit "$argv"
 
     mkdir (path dirname "$sp")
     sudo rsync --chown=(stat -c '%U:%G' (path dirname "$sp")) "$argv" "$sp"
