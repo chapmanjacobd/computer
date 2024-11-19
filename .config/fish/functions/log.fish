917f57f07e214761c2eeac621ccff2e34e18e51c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-11-18 19:44:35 -0600

    log

--- .config/fish/functions/log.fish
+++ .config/fish/functions/log.fish
@@ -1,3 +1,3 @@
 function log -w journalctl
-    journalctl --no-hostname -n5000 -a $argv | grep -v -E 'postfix|Firewall|pam|libva|kioslave|ddcutil|sshd|audit|kwin_core: Applying KScreen config|vsce-sign|ETA: |/dev/tty: No such device or address|making sync progress|dnscrypt-proxy|]: Start' | less -FSRXc +G
+    journalctl --no-hostname -n5000 -a $argv | grep -v -E 'postfix|Firewall|pam|libva|kioslave|ddcutil|sshd|audit|kwin_core: Applying KScreen config|vsce-sign|ETA: |/dev/tty: No such device or address|making sync progress|dnscrypt-proxy|]: Start|tailscale' | less -FSRXc +G
 end
