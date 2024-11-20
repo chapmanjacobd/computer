f4ff3e598b7482885252b7d9451c490a27a9a81b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-11-20 04:14:39 +0000

    freerdp avc444_decompress is broken in Fedora 41

--- .config/fish/functions/vnc.fish
+++ .config/fish/functions/vnc.fish
@@ -4,8 +4,9 @@ function vnc
 
     #vopono -v exec --custom ./custom_openvpn.ovpn --protocol openvpn "firefox"
     if contains pakon $argv
-        ssh -t pakon setsid -f 'fish -c "rdpserve"'
-        rdp pakon.curl-amberjack.ts.net:35589
+        /usr/NX/bin/nxplayer --session ~/.ssh/pakon.nxs
+        # ssh -t pakon setsid -f 'fish -c "rdpserve"'
+        # rdp pakon.curl-amberjack.ts.net:35589
     end
     if contains backup $argv
         # rdp backup.curl-amberjack.ts.net:35589
