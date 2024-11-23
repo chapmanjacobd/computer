1b23aba556dccf083988919699879a9fef5b232b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-11-23 12:33:42 -0600

    torganize

--- .config/fish/functions/torganize.fish
+++ .config/fish/functions/torganize.fish
@@ -2,6 +2,9 @@
 function torganize
     rsync -auh --remove-sent-files ~/Downloads/\[(seq 0 9)*.torrent backup:.local/data/rtorrent/watch/new/
 
-    torrent_promote.py --trackers -n9999 -o ~/.local/data/qbittorrent/ ~/Downloads/
+    lb mv ~/Downloads/*.torrent ~/.local/data/qbittorrent/queue/
+
+    #torrent_promote.py --trackers -n9999 -o ~/.local/data/qbittorrent/ ~/Downloads/
+
     #rsync -auh --remove-sent-files ~/Downloads/*.torrent backup:.local/data/qbittorrent/seed_pause/others/
 end
