51d5a4a5ade1887f5d0ba60d097a0fc4f646d2ce
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-12-05 00:16:22 -0600

    torganize gitignore

--- .config/fish/functions/torganize.fish
+++ .config/fish/functions/torganize.fish
@@ -5,6 +5,13 @@ function torganize
     lb mv ~/Downloads/*.torrent ~/.local/data/qbittorrent/queue/
     lb-dev torrents-add ~/lb/torrents.db ~/.local/data/qbittorrent/queue/
 
+    b lb-dev computer-update computers.local.db
+    b lb-dev computer-update computers.remote.db
+    wait
+
+    # lb-dev allocate-torrents computers.local.db torrents.db -v -E XXX pussytorrents.org plab.site bitporn.eu scenetime.com superbits.org exoticaz.to happyfappy.org empornium.is empornium.sx
+    # lb-dev allocate-torrents computers.remote.db torrents.db -v --flex -s XXX pussytorrents.org plab.site bitporn.eu scenetime.com superbits.org exoticaz.to happyfappy.org empornium.is empornium.sx
+
     #torrent_promote.py --trackers -n9999 -o ~/.local/data/qbittorrent/ ~/Downloads/
 
     #rsync -auh --remove-sent-files ~/Downloads/*.torrent backup:.local/data/qbittorrent/seed_pause/others/
