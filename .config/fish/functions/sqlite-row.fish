7163e5c87bb1c0e2e2f1a9dcd2f19761d8cf47fd
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-12-04 23:28:50 -0600

    sqlite-media sqlite-row

--- .config/fish/functions/sqlite-row.fish
+++ .config/fish/functions/sqlite-row.fish
@@ -1,4 +1,4 @@
 # Defined interactively
-function sqlite-row --argument db key value
-    sqlite-utils rows "$db" media --limit 1 --where "$key='$value'" | jq -r '.[] | to_entries[] | select(.value != null) | "\(.key) \(.value)"' | sort
+function sqlite-row --argument db table key value
+    sqlite-utils rows "$db" "$table" --limit 1 --where "$key='$value'" | jq -r '.[] | to_entries[] | select(.value != null) | "\(.key) \(.value)"' | sort
 end
