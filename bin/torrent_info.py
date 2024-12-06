38fa61d220a5a8528d87f8b7cfd7513c4400ffdc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-12-06 15:36:35 -0600

    torrent_info

---
+++ bin/torrent_info.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from xklb.mediafiles import torrents_start
+from xklb.utils import arggroups, argparse_utils, printing, processes
+
+parser = argparse_utils.ArgumentParser()
+arggroups.qBittorrent(parser)
+arggroups.debug(parser)
+
+parser.add_argument('search_terms', nargs='+', help="The info_hash, name, or save_path substring to search for")
+args = parser.parse_args()
+
+qbt_client = torrents_start.start_qBittorrent(args)
+torrents = qbt_client.torrents_info()
+
+torrents = [
+    t
+    for t in torrents
+    if all(
+        (term in t.get('hash', '') or term in t.get('name', '') or term in t.get('save_path', ''))
+        for term in args.search_terms
+    )
+]
+
+
+if not torrents:
+    processes.no_media_found()
+
+torrents = sorted(torrents, key=lambda t: -t.time_active)
+printing.extended_view(torrents)
