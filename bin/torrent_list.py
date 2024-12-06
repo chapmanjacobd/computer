23919c61e5ce510aa0f405870943ef04d6574965
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-12-05 21:13:15 -0600

    torrent_list

--- bin/torrent_list.py
+++ bin/torrent_list.py
@@ -1,71 +1,108 @@
 #!/usr/bin/python3
-import qbittorrentapi
+from xklb.mediafiles.torrents_start import start_qBittorrent
 from xklb.utils import arggroups, argparse_utils, iterables, printing, strings
 
 parser = argparse_utils.ArgumentParser()
+arggroups.qBittorrent(parser)
 arggroups.debug(parser)
 args = parser.parse_args()
 
-qb = qbittorrentapi.Client(host='127.0.0.1', port=8080)
+qbt_client = start_qBittorrent(args)
+all_torrents = qbt_client.torrents_info()
+
+torrents_by_state = {}
+for torrent in all_torrents:
+    torrents_by_state.setdefault(torrent.state, []).append(torrent)
+
+interesting_states = [
+    ['stoppedUP', 'queuedUP'],
+    ['stoppedDL'],
+    ['forcedMetaDL', 'metaDL'],
+    ['forcedDL', 'stalledDL'],
+    # ['forcedUP', 'stalledUP'],  # not very interesting
+    # ['uploading'],
+    ['downloading'],
+    ['missingFiles'],
+    ['error'],
+]
+
+tbl = []
+for states in interesting_states:
+    torrents = [t for state in states for t in torrents_by_state.get(state) or []]
+    if not torrents:
+        continue
+
+    torrents = sorted(torrents, key=lambda t: (-t.seen_complete, t.time_active))
+
+    if args.verbose >= 1:
+        printing.extended_view(torrents)
+    else:
+        tbl.extend(
+            [
+                {
+                    'state': '+'.join(states),
+                    'name': printing.path_fill(t.name, width=76),
+                    'seen_complete': strings.relative_datetime(t.seen_complete) if t.seen_complete > 0 else None,
+                    'last_activity': strings.relative_datetime(t.last_activity),
+                    'time_active': strings.duration(t.time_active),
+                }
+                for t in torrents
+            ]
+        )
+printing.table(tbl)
+print()
+
+
+tbl = []
+for states in interesting_states:
+    torrents = [t for state in states for t in torrents_by_state.get(state) or []]
+    if not torrents:
+        continue
+
+    torrents = sorted(torrents, key=lambda t: (t.amount_left == t.total_size, t.eta, t.amount_left), reverse=True)
+
+    if args.verbose == 0:
+        tbl.extend(
+            [
+                {
+                    'state': '+'.join(states),
+                    'name': printing.path_fill(t.name, width=76),
+                    'progress': strings.safe_percent(t.progress),
+                    'eta': strings.duration(t.eta) if t.eta < 8640000 else None,
+                    'remaining': strings.file_size(t.amount_left),
+                    'num_seeds': t.num_seeds,
+                    # 'num_leechs': t.num_leechs,
+                    # 'comment': t.comment,
+                }
+                for t in torrents
+            ]
+        )
+printing.table(tbl)
+print()
 
 categories = []
-for category in [
-    "completed",
-    "seeding",
-    "stalled_uploading",
-    "stalled_downloading",
-    "downloading",
-    "checking",
-    "moving",
-    "stopped",
-    "errored",
-]:
-    torrents = qb.torrents_info(category)  # type: ignore
+for state, torrents in torrents_by_state.items():
     files = list(iterables.flatten(t.files for t in torrents))
     categories.append(
         {
-            'state': category,
+            'state': state,
             'count': len(torrents),
             'size': strings.file_size(sum(f.size for f in files)),
             'file_count': len(files),
         }
     )
-    if 0 < len(torrents) < 50:
-        print(category)
-        if args.verbose >= 1:
-            printing.extended_view(torrents)
-        else:
-            torrents = sorted(torrents, key=lambda t: (-t.seen_complete, t.time_active))
-            printing.table(
-                [
-                    {
-                        'name': t.name,
-                        'seen_complete': strings.relative_datetime(t.seen_complete) if t.seen_complete > 0 else None,
-                        'last_activity': strings.relative_datetime(t.last_activity),
-                        'time_active': strings.duration(t.time_active),
-                    }
-                    for t in torrents
-                ]
-            )
-            print()
-
-            torrents = sorted(
-                torrents, key=lambda t: (t.amount_left == t.total_size, t.eta, t.amount_left), reverse=True
-            )
-            printing.table(
-                [
-                    {
-                        'name': t.name,
-                        'progress': strings.safe_percent(t.progress),
-                        'eta': strings.duration(t.eta) if t.eta < 8640000 else None,
-                        'remaining': strings.file_size(t.amount_left),
-                        'num_seeds': t.num_seeds,
-                        # 'num_leechs': t.num_leechs,
-                        # 'comment': t.comment,
-                    }
-                    for t in torrents
-                ]
-            )
-        print()
-
 printing.table(categories)
+print()
+
+transfer = qbt_client.transfer_info()
+print(transfer.connection_status.upper())
+
+dl_speed = strings.file_size(transfer.dl_info_speed)
+dl_limit = f'[{strings.file_size(transfer.dl_rate_limit)}/s]' if transfer.dl_rate_limit > 0 else ''
+dl_d = strings.file_size(transfer.dl_info_data)
+print(f"DL {dl_speed}/s {dl_limit} ({dl_d})")
+
+up_speed = strings.file_size(transfer.up_info_speed)
+up_limit = f'[{strings.file_size(transfer.up_rate_limit)}/s]' if transfer.up_rate_limit > 0 else ''
+up_d = strings.file_size(transfer.up_info_data)
+print(f"UP {up_speed}/s {up_limit} ({up_d})")
