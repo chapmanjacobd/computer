b6847529f3febf9da077e72167dd4972c0172545
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-12-07 08:34:16 -0600

    copy_to_pakon

--- .config/fish/functions/copy_to_pakon.fish
+++ .config/fish/functions/copy_to_pakon.fish
@@ -14,7 +14,7 @@ function copy_to_pakon
     #rsync -auh --info=progress2 --no-inc-recursive --remove-sent-files backup:d/_tixati/ ~/d/dump/video/other/from_backup/
     #ssh backup remove_empty_directories /home/xk/d/_tixati/
 
-    stickysync_backup ~/sync/datasets/file-lists/stickysync_rtorrent_audiobooks d/_rtorrent/ /mnt/d/dump/audio/audiobooks/rtorrent/
+    # stickysync_backup ~/sync/datasets/file-lists/stickysync_rtorrent_audiobooks d/_rtorrent/ /mnt/d/dump/audio/audiobooks/rtorrent/
 
     lb-refresh
 end
