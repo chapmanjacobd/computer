77f5ce6cfbb3d2de0250f449c3309815488a4615
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-12-07 21:31:38 -0600

    mam_db_dl mam_search mam_update

--- .config/fish/functions/mam_search.fish
+++ .config/fish/functions/mam_search.fish
@@ -8,6 +8,5 @@ function mam_search
     ~/lb/
     python -m xklb.scratch.mam_search --audiobooks --books --comics --radio $opts --cookie $MAM_COOKIE "$db" $args
 
-    sqlite --no-headers --raw-lines "$db" "select id from media where vip=1 and seeders>0 and my_snatched=0" | parallel -S backup -j1 ".local/data/rtorrent/watch/vip_new/; curl -OJs -b mam_id=$MAM_COOKIE https://www.myanonamouse.net/tor/download.php?tid={}; sleep 2"
-    sqlite --no-headers --raw-lines "$db" "select id from media where vip=0 and seeders>0 and my_snatched=0" | parallel -S backup -j1 ".local/data/rtorrent/watch/nonvip_new/; curl -OJs -b mam_id=$MAM_COOKIE https://www.myanonamouse.net/tor/download.php?tid={}; sleep 2"
+    mam_db_dl "$db"
 end
--- .config/fish/functions/mam_update.fish
+++ .config/fish/functions/mam_update.fish
@@ -17,9 +17,5 @@ function mam_update
         python -m xklb.scratch.mam_search --cookie $MAM_COOKIE ~/lb/sites/mam/mam.db (string split ' ' -- $l) ''
     end
 
-    for db in ~/lb/sites/mam/mam.db
-        sqlite --no-headers --raw-lines "$db" "select id from media where unixepoch(added) > $(timestamp 1 week ago 0 -u) and vip=1 and seeders>0 and my_snatched=0" | parallel -S backup -j1 ".local/data/rtorrent/watch/vip_new/; curl -OJs -b mam_id=$MAM_COOKIE https://www.myanonamouse.net/tor/download.php?tid={}; sleep 2"
-        sqlite --no-headers --raw-lines "$db" "select id from media where unixepoch(added) > $(timestamp 1 week ago 0 -u) and vip=0 and seeders>0 and my_snatched=0" | parallel -S backup -j1 ".local/data/rtorrent/watch/nonvip_new/; curl -OJs -b mam_id=$MAM_COOKIE https://www.myanonamouse.net/tor/download.php?tid={}; sleep 2"
-    end
-
+    mam_db_dl ~/lb/sites/mam/mam.db
 end
