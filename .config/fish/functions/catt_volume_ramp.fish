0ef834829e17e83d50595ea65c7f61308779b164
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-12-09 08:38:01 -0600

    catt_volume_ramp gitignore

---
+++ .config/fish/functions/catt_volume_ramp.fish
@@ -0,0 +1,24 @@
+# Defined via `source`
+function catt_volume_ramp --argument target_volume
+    if not string match -qr '^[1-9][0-9]?$|^100$' $target_volume
+        echo "Error: Target volume must be an integer between 1 and 100."
+        return 1
+    end
+
+    set current_volume (catt info | grep 'volume_level:' | awk '{print $2}')
+    set current_volume (math -s 0 "$current_volume * 100")
+
+    if test $current_volume -lt $target_volume
+        while test $current_volume -lt $target_volume
+            set current_volume (math $current_volume + 1)
+            catt volume $current_volume
+            sleep 2
+        end
+    else
+        while test $current_volume -gt $target_volume
+            set current_volume (math $current_volume - 1)
+            catt volume $current_volume
+            sleep 2
+        end
+    end
+end
