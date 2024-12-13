1294ea24b9f7505c52396d3720f1ec6c89d0bb84
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-12-12 20:17:38 -0600

    unlidisk_admin

--- .config/fish/functions/unlidisk_admin.fish
+++ .config/fish/functions/unlidisk_admin.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 2
 function unlidisk_admin
-    sshfs -o reconnect,IdentityFile=/home/xk/.ssh/admin_rsa admin@unli.xyz:/ ~/.mnt/admin
+    sshfs -o reconnect,IdentityFile=/home/xk/.ssh/old/admin_rsa admin@unli.xyz:/ ~/.mnt/admin
 end
