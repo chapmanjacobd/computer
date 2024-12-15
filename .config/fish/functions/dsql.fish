ba83187d4061ea6165cfd8ac0d7a3f32f2179d53
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-12-15 14:57:35 -0600

    dsql

--- .config/fish/functions/dsql.fish
+++ .config/fish/functions/dsql.fish
@@ -1,6 +1,6 @@
 # Defined via `source`
 function dsql
     for db in ~/lb/fs/d*.db
-        sqlite-utils --no-headers --csv $db "$argv" | tr -d '\r'
+        lb fs "$db" -pf -s "$argv"
     end
 end
