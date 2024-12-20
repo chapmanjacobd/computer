68afead95dd2262685cc1c3728c5d84bab3699ca
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-12-20 02:21:34 -0500

    unlidisk_admin

--- .config/fish/functions/unlidisk_admin.fish
+++ .config/fish/functions/unlidisk_admin.fish
@@ -1,5 +1,6 @@
 # Defined in - @ line 2
 function unlidisk_admin
+    mkdir /home/xk/.mnt/admin
     sshfs -o reconnect,IdentityFile=/home/xk/.ssh/old/admin_rsa admin@unli.xyz:/ ~/.mnt/admin
     sleep 2
     code ~/.mnt/admin/home/production/admin/public_html/
