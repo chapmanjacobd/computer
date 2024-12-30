3b7d024c7fcc28167d3874e5ad76d966dc35b8ea
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-12-30 11:54:33 -0500

    sr

---
+++ .config/fish/functions/sr.fish
@@ -0,0 +1,8 @@
+# Defined interactively
+function sr --argument-names find --argument-names replace
+    rg -i --no-heading --no-line-number -j1 "$find"
+
+    if confirm
+        fd --type file --exec sd "$find" "$replace"
+    end
+end
