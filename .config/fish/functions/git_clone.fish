4ca95d78c84269e57b5bda91b002d6e196ff0f33
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-01-02 15:17:50 -0500

    git_clone

---
+++ .config/fish/functions/git_clone.fish
@@ -0,0 +1,14 @@
+# Defined via `source`
+function git_clone --argument-names url
+
+    switch $url
+        case 'https://github.com/*'
+            set url (string replace 'https://github.com/' 'git@github.com:' $url)
+        case 'https://gitlab.com/*'
+            set url (string replace 'https://gitlab.com/' 'git@gitlab.com:' $url)
+        case 'https://bitbucket.org/*'
+            set url (string replace 'https://bitbucket.org/' 'git@bitbucket.org:' $url)
+    end
+
+    git clone $url
+end
