d5e0ed96d9cf4096b8cce00ffd75d04cdbfbe487
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-01-02 15:20:06 -0500

    git_remote_use_ssh

---
+++ .config/fish/functions/git_remote_use_ssh.fish
@@ -0,0 +1,24 @@
+# Defined interactively
+function git_remote_use_ssh
+    if not git rev-parse --git-dir >/dev/null 2>&1
+        echo "Error: Not in a Git repository."
+        return 1
+    end
+
+    set current_url (git config --get remote.origin.url)
+
+    switch $current_url
+        case 'https://github.com/*'
+            set new_url (string replace 'https://github.com/' 'git@github.com:' $current_url)
+        case 'https://gitlab.com/*'
+            set new_url (string replace 'https://gitlab.com/' 'git@gitlab.com:' $current_url)
+        case 'https://bitbucket.org/*'
+            set new_url (string replace 'https://bitbucket.org/' 'git@bitbucket.org:' $current_url)
+        case '*'
+            echo "The origin URL is already in the correct format or is not supported."
+            return 0
+    end
+
+    git remote set-url origin $new_url
+    git remote -v
+end
