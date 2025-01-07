1b57b950c8554a75be5cb1a1d77af0e4264459b9
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-01-07 00:33:43 -0500

    unli

--- .config/fish/functions/unli.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /tmp/fish.yTspbW/unli.fish @ line 2
-function unli --description alias\ unli=ssh\ jacob@unli.xyz\ -t\ \'tmux\ a\ -t\ base\ \|\|\ tmux\ new\ -s\ base\'
-    TERM=xterm ssh $argv jacob@unli.xyz -t tmux new-session -A -s phone
-end
