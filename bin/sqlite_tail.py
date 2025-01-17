f133bc0714f51ff0d3683699d8dfb3b815fda4cb
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-01-16 22:43:24 -0600

    dnf_installed sqlite_tail

--- bin/sqlite_tail.py
+++ bin/sqlite_tail.py
@@ -3,54 +3,56 @@ import sqlite3
 import time
 
 import sqlite_utils
-from library.utils import printing
+from library.utils import argparse_utils, printing
 
 
-def get_max_rowids(db: sqlite_utils.Database):
-    tables = db.table_names()
+def get_initial_rowids(args):
+    tables = args.db.table_names()
 
     max_rowids = {}
     for table_name in tables:
-        cursor = db.execute(f"SELECT MAX(rowid) FROM {table_name};")
+        cursor = args.db.execute(f"SELECT MAX(rowid) FROM {table_name};")
         max_rowid = cursor.fetchone()[0]
-        max_rowids[table_name] = max_rowid
+        max_rowids[table_name] = [max_rowid]
 
     return max_rowids
 
 
-def print_new_rows(db, max_rowids):
-    for table_name, last_max_rowid in max_rowids.items():
+def print_new_rows(args, max_rowids):
+    for table_name, max_rowids in max_rowids.items():
+        last_max_rowid = max_rowids[-(args.delay + 1)]
+
         new_rows = list(
-            db.query(f"SELECT rowid as rowid, * FROM {table_name} WHERE rowid > ? ORDER BY rowid;", (last_max_rowid or 0,))
+            args.db.query(
+                f"SELECT rowid as rowid, * FROM {table_name} WHERE rowid > ? ORDER BY rowid;", (last_max_rowid or 0,)
+            )
         )
         if new_rows:
             print(f"{table_name}:")
             printing.table(new_rows)
 
-            max_rowids[table_name] = max(row['rowid'] for row in new_rows)
+            max_rowids[table_name].append(max(row['rowid'] for row in new_rows))
 
 
-def main(db_path):
-    db = sqlite_utils.Database(sqlite3.connect(f"file:{db_path}?mode=ro", uri=True))
+def sqlite_tail(db_path):
+    args.db = sqlite_utils.Database(sqlite3.connect(f"file:{db_path}?mode=ro", uri=True))
 
-    max_rowids = get_max_rowids(db)
+    max_rowids = get_initial_rowids(args)
 
     try:
         while True:
-            print_new_rows(db, max_rowids)
+            print_new_rows(args, max_rowids)
             time.sleep(1)
     except KeyboardInterrupt:
         print("Exiting...")
     finally:
-        db.close()
+        args.db.close()
 
 
 if __name__ == "__main__":
-    import sys
+    parser = argparse_utils.ArgumentParser()
+    parser.add_argument("--delay", type=int, default=0, help="Delay read until write-N")
+    parser.add_argument("database")
+    args = parser.parse_args()
 
-    if len(sys.argv) != 2:
-        print("Usage: python sqlite_tail.py <database_path>")
-        sys.exit(1)
-
-    db_path = sys.argv[1]
-    main(db_path)
+    sqlite_tail(args.database)
