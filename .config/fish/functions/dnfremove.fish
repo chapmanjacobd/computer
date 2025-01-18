b37a9472ed5e1c29194cfec6ef146d3e1de82c55
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-01-18 12:23:52 -0600

    dnfremove

--- .config/fish/functions/dnfremove.fish
+++ .config/fish/functions/dnfremove.fish
@@ -3,5 +3,10 @@ function dnfremove --wraps='dnf install'
     for arg in $argv
         filterfile ~/.github/dnf_installed $arg
     end
-    sudo dnf remove $argv
+    if command -v rpm-ostree >/dev/null
+        sudo rpm-ostree remove $argv
+        echo sudo rpm-ostree override remove
+    else
+        sudo dnf remove $argv
+    end
 end
