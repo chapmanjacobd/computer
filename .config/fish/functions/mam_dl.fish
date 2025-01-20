167d6d61252a56e40f5381d72d2b9a521e23bd1a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-01-20 10:33:30 -0600

    mam_dl

--- .config/fish/functions/mam_dl.fish
+++ .config/fish/functions/mam_dl.fish
@@ -5,6 +5,6 @@ function mam_dl
     for id in $argv
         set id (echo $id | grep -i '.net/t/' | sed 's|https://www.myanonamouse.net/t/||')
         curl -OJs -b mam_id=$MAM_COOKIE https://www.myanonamouse.net/tor/download.php?tid=$id
-        sleep 1
+        sleep 2
     end
 end
