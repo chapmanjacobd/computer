f98ed5d0b2d22c4b1c48135394cdd0c74098b19c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-01-25 19:32:17 -0600

    duckdb_compact

---
+++ .config/fish/functions/duckdb_compact.fish
@@ -0,0 +1,17 @@
+# Defined via `source`
+function duckdb_compact
+    if test (count $argv) -ne 1
+        echo "Usage: duckdb_compact <database_file>"
+        return 1
+    end
+
+    set -l db_file $argv[1]
+    set -l temp_file (mktemp -u).duckdb
+
+    duckdb -c "
+        ATTACH '$db_file' AS db1;
+        ATTACH '$temp_file' AS db2;
+        COPY FROM DATABASE db1 TO db2;
+    "
+    and mv $temp_file $db_file
+end
