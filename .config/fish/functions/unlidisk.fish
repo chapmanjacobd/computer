6ff5e66930f0805a9d5f7b1f8e763b544fd8c92a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-01-30 19:04:13 -0600

    unlidisk unlidisk_admin

--- .config/fish/functions/unlidisk.fish
+++ .config/fish/functions/unlidisk.fish
@@ -2,6 +2,6 @@
 function unlidisk
     sshfs -o reconnect,sftp_server="/usr/bin/sudo /usr/libexec/openssh/sftp-server" unli.xyz:/ ~/.mnt/web/
     sleep 2
-    code ~/.mnt/web/home/production/admin/public_html/
+    code ~/.mnt/web/var/www/
     echo fusermount -zu ~/.mnt/web/
 end
--- .config/fish/functions/unlidisk_admin.fish
+++
@@ -1,8 +0,0 @@
-# Defined in - @ line 2
-function unlidisk_admin
-    mkdir /home/xk/.mnt/admin
-    sshfs -o reconnect,IdentityFile=/home/xk/.ssh/old/admin_rsa admin@unli.xyz:/ ~/.mnt/admin
-    sleep 2
-    code ~/.mnt/admin/home/production/admin/public_html/
-    echo fusermount -zu ~/.mnt/admin/
-end
