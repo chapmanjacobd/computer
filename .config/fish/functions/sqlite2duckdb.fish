ba844ddc63072ed9da106c587519a3322df20387
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-03 22:03:45 -0600

    sqlite2duckdb

--- .config/fish/functions/sqlite2duckdb.fish
+++ .config/fish/functions/sqlite2duckdb.fish
@@ -1,6 +1,8 @@
 # Defined via `source`
 function sqlite2duckdb -a sqlite_db -a duckdb_db
     duckdb -c "
+        SET sqlite_all_varchar=true;
+
         ATTACH '$sqlite_db' AS db1;
         ATTACH '$duckdb_db' AS db2;
         COPY FROM DATABASE db1 TO db2;
