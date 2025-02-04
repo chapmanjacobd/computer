cef5f16344833353f8cb57cdaee111f037a0e417
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-03 23:09:18 -0600

    duckdb2parquet

--- .config/fish/functions/duckdb2parquet.fish
+++ .config/fish/functions/duckdb2parquet.fish
@@ -1,6 +1,6 @@
 # Defined via `source`
 function duckdb2parquet
     for s in $argv
-        duckdb $s -c "EXPORT DATABASE '$s.parquet' (FORMAT 'parquet', COMPRESSION 'zstd', COMPRESSION_LEVEL 5, ROW_GROUP_SIZE 50_000, ROW_GROUPS_PER_FILE 200);"
+        duckdb $s -c "SET sqlite_all_varchar=true;EXPORT DATABASE '$s.parquet' (FORMAT 'parquet', COMPRESSION 'zstd', COMPRESSION_LEVEL 5, ROW_GROUP_SIZE 50_000, ROW_GROUPS_PER_FILE 200);"
     end
 end
