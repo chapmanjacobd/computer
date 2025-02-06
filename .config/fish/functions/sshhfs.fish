68696670515e205de83384abb71cf917e94bf3f2
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-05 22:45:05 -0600

    sshhfs

--- .config/fish/functions/sshhfs.fish
+++ .config/fish/functions/sshhfs.fish
@@ -1,5 +1,6 @@
 # Defined interactively
 function sshhfs
     mkdir -p ~/.mnt/$argv/
-    sshfs -o noauto,noatime,_netdev,reconnect,ConnectTimeout=10,ServerAliveInterval=8,TCPKeepAlive=no,auto_cache,cache_timeout=1152,attr_timeout=1152,entry_timeout=1200 $argv: ~/.mnt/$argv/
+    sshfs -o follow_symlinks,noauto,noatime,_netdev,reconnect,ConnectTimeout=10,ServerAliveInterval=8,TCPKeepAlive=no,auto_cache,cache_timeout=1152,attr_timeout=1152,entry_timeout=1200 $argv: ~/.mnt/$argv/
+    echo fusermount -zu ~/.mnt/$argv/
 end
