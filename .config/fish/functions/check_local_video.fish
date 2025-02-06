94fcfbcd3cc057713e78e5ca2472c449d4ea6638
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-06 11:46:08 -0600

    check_local check_local_video funcmv r730xd

---
+++ .config/fish/functions/check_local_video.fish
@@ -0,0 +1,6 @@
+# Defined interactively
+function check_local_video
+    echo $argv
+    locate_remote_mv.py -v -E /sync/ --flex --hosts backup pakon r730xd -- /video/ $argv
+    echon pakon backup r730xd | parallel sshpc {} lb torrents -- $argv
+end
