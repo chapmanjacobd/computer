a91b4fbed26f6eefd833785224f683e5c0b4aa56
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-06 12:56:52 -0600

    ls home-xk-

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,7 +1,7 @@
 # Defined interactively
 function ls -w eza
     if status --is-interactive
-        if has_git_dir $argv; and not string match -q '*/.mnt/*' $PWD
+        if has_git_dir $argv; and not string match -q '*.mnt/*' $argv; and not string match -q '*.mnt/*' $PWD
             eza --git --long --header --classify --sort=type --group-directories-first $argv
         else
             eza --long --header --classify --sort=none --group-directories-first $argv
