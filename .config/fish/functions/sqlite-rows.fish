1325fde67a9d1180dfe3071e4a3373bdcfb4ac20
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-10 11:48:11 -0600

    sqlite-rows

--- .config/fish/functions/sqlite-rows.fish
+++ .config/fish/functions/sqlite-rows.fish
@@ -1,4 +1,4 @@
 # Defined interactively
-function sqlite-rows --argument db key value
-    sqlite-utils rows "$db" media --where "$key like '$value'" | jq -r '.[] | to_entries[] | select(.value != null) | "\(.key) \(.value)"' | sort --unique --ignore-case
+function sqlite-rows --argument db table key value
+    sqlite-utils rows "$db" $table --where "$key like '$value'" | jq -r '.[] | to_entries[] | select(.value != null) | "\(.key) \(.value)"' | sort --unique --ignore-case
 end
