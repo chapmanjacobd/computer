234d9f18c692a790061a6c55eea14790efd42190
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-13 20:47:26 -0600

    fish_remove_path

---
+++ .config/fish/functions/fish_remove_path.fish
@@ -0,0 +1,7 @@
+# Defined interactively
+function fish_remove_path
+    if set -l index (contains -i "$argv" $fish_user_paths)
+        set -e fish_user_paths[$index]
+        echo "Removed $argv from the path"
+    end
+end
