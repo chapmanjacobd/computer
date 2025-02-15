ccfe5387a964e0a719e2256ecd0531f8bb5f6932
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-15 09:43:05 -0600

    abbreviations rclone

--- .config/fish/functions/rclone.fish
+++
@@ -1,4 +0,0 @@
-# Defined interactively
-function rclone
-    command rclone --transfers 10 --multi-thread-streams 4 --progress --metadata --human-readable --fast-list --error-on-no-transfer --checkers 18 $argv
-end
