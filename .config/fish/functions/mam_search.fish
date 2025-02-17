04a09bc2979772a0f5288df3ad6dafd62df113a4
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-16 18:15:38 -0600

    mam_search

--- .config/fish/functions/mam_search.fish
+++ .config/fish/functions/mam_search.fish
@@ -3,8 +3,7 @@ function mam_search
     filter_opts $argv
 
     load_env_mam
-    set db ~/lb/sites/mam/(string replace -a ' ' _ $args).db
 
     ~/lb/
-    python -m library.scratch.mam_search --audiobooks --books --comics --radio $opts --cookie $MAM_COOKIE "$db" $args
+    python -m library.scratch.mam_search --audiobooks --radio $opts --cookie $MAM_COOKIE ~/lb/sites/mam/mam.db $args
 end
