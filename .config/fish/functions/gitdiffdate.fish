a7c5603fd8f33c8b5ff6747ea52a6c45c5441f0c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-19 11:25:35 -0600

    gitdiffdate

--- .config/fish/functions/gitdiffdate.fish
+++ .config/fish/functions/gitdiffdate.fish
@@ -1,5 +1,5 @@
 # Defined interactively
 function gitdiffdate
-    git log --pretty=format:"%ad %an %h %B%-C()" --since="$argv" | less
-    git diff (git rev-list -n 1 --before="$argv" HEAD)
+    git log --pretty=format:"%ad %an %h %B%-C()" --since="$argv" | less -S
+    git diff --no-textconv (git rev-list -n 1 --before="$argv" HEAD)
 end
