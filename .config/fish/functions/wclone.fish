3b286b54bf972730e616d9f82990f8c703d95603
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-21 23:25:21 -0600

    wclone

--- .config/fish/functions/wclone.fish
+++ .config/fish/functions/wclone.fish
@@ -2,4 +2,5 @@
 function wclone
     ~/d/dump/text/web/
     wget --force-directories --adjust-extension -e robots=off -np -nc -r -l inf -p --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36" --reject-regex '/tag/|/tags/|\?tag' $argv
+    # TODO: https://github.com/rockdaboot/wget2/blob/master/docs/wget2.md#download-options
 end
