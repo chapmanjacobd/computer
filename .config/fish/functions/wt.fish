120786fea42a6ef7b278f2527f27238928ca1261
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-23 21:22:50 -0600

    wt

--- .config/fish/functions/wt.fish
+++ .config/fish/functions/wt.fish
@@ -1,3 +1,5 @@
 function wt
+    focus_under_mouse
     /home/xk/.local/bin/lb wt --keep-dir /mnt/d/archive/video/other/ -k delete --local-media-only ~/lb/video.db --cmd5 'echo skip' $argv
+    focus_follows_mouse
 end
