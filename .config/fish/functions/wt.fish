131f4272b89d4a11301b21ca87c48b5bf9fa8c01
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-24 20:14:44 -0600

    wt

--- .config/fish/functions/wt.fish
+++ .config/fish/functions/wt.fish
@@ -1,5 +1,11 @@
 function wt
-    focus_under_mouse
+    if not string match -q -- '*-p*' "$argv"
+        focus_under_mouse
+    end
+
     /home/xk/.local/bin/lb wt --keep-dir /mnt/d/archive/video/other/ -k delete --local-media-only ~/lb/video.db --cmd5 'echo skip' $argv
-    focus_follows_mouse
+
+    if not string match -q -- '*-p*' "$argv"
+        focus_follows_mouse
+    end
 end
