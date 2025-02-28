8728eac97fe7f9af2d52854c72df2fea515a14b0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-02-27 18:00:51 -0600

    check_local check_local_video

--- .config/fish/functions/check_local.fish
+++ .config/fish/functions/check_local.fish
@@ -1,7 +1,7 @@
 # Defined interactively
 function check_local
     echo $argv
-    lb playlists ~/lb/torrents.db -s $argv -pf --cols title | lb rs
+    lb playlists ~/lb/torrents.db -s $argv -pf --cols title 2>/dev/null | lb rs 2>/dev/null
     locate_remote_mv.py -v --flex --hosts backup pakon r730xd -- $argv
     echon pakon backup r730xd | parallel sshpc {} lb torrents -- $argv
 end
