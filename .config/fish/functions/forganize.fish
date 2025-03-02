cc0cdcd78800927f3d72e2893374f604abd01706
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-03-02 16:40:07 -0600

    forganize

--- .config/fish/functions/forganize.fish
+++ .config/fish/functions/forganize.fish
@@ -3,7 +3,7 @@ function forganize
     dorganize
 
     set processing_dir processing_(datestamp)
-    allpc library torrents --complete --seeders=+4 --time-stalled=+3days --time-active=+60days --stop --move $processing_dir --delete-rows -v
+    allpc library torrents --complete --seeders=+4 --time-stalled=+3days --time-seeding=+60days --stop --move $processing_dir --delete-rows -v
     # stop incomplete
     allpc library torrents --incomplete --time-unseeded=+90days --time-active=+60days --time-stalled=+30days --delete-incomplete --stop --move $processing_dir --tag library-delete -v
     allpc library torrents --incomplete --time-active=+60days --inactive --progress=0 --stop --tag library-no-seed -v
