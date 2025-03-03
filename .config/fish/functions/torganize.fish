bba8edc5ac8b8fac51511589bbbc71da2b7edd45
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-03-02 21:03:08 -0600

    torganize torrent-maintenance

--- .config/fish/functions/torganize.fish
+++ .config/fish/functions/torganize.fish
@@ -1,15 +1,11 @@
 # Defined interactively
 function torganize
     parallel sshpc {} qbt_prioritize.py ::: pakon backup r730xd len hk
-    # ~/bin/qbt_file_sizes.py -v 127.0.0.1:8080 backup:8888 r730xd:8888 hk:8888 len:8888
 
     rsync -auh --remove-sent-files ~/Downloads/\[(seq 0 9)*.torrent backup:.local/data/rtorrent/watch/new/
 
     lb mv ~/Downloads/*.torrent ~/.local/data/qbittorrent/queue/
     lb torrents-add ~/lb/torrents.db ~/.local/data/qbittorrent/queue/ -v --delete-files
-
-    qbt_torrents_exfoliate.py
-    qbt_torrents_trumped.py
 
     lb computer-add ~/lb/computers.local.db pakon backup r730xd len -v
     lb computer-add ~/lb/computers.remote.db hk -v
@@ -18,4 +14,5 @@ function torganize
     qbt_computers_remaining.py ~/lb/computers.remote.db
 
     tor-refresh
+    echo torrent-maintenance
 end
---
+++ .config/fish/functions/torrent-maintenance.fish
@@ -0,0 +1,9 @@
+# Defined interactively
+function torrent-maintenance
+    ~/bin/qbt_file_sizes.py -v 127.0.0.1:8080 backup:8888 r730xd:8888 hk:8888 len:8888
+
+    for s in 127.0.0.1:8080 backup:8888 r730xd:8888 hk:8888 len:8888
+        qbt_torrents_exfoliate.py --host $s
+        qbt_torrents_trumped.py --host $s
+    end
+end
