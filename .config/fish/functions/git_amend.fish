67d75b2d1dd8b0af0eaa64bdb848bdd96829f119
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-03-08 15:11:28 -0600

    gitconfig

---
+++ .config/fish/functions/git_amend.fish
@@ -0,0 +1,23 @@
+# Defined via `source`
+function git_amend
+    set tags (git tag --points-at HEAD)
+    if test -n "$tags"
+        if not confirm "Tags found pointing to HEAD: $tags. Continue? (y/N)"
+            return 1
+        end
+    end
+
+    git commit --amend --no-edit
+
+    if test -n "$tags"
+        for tag in $tags
+            git tag -f "$tag" HEAD
+        end
+    end
+
+    git push -f --force-with-lease --force-if-includes
+
+    if test -n "$tags"
+        git push -f --tags
+    end
+end
