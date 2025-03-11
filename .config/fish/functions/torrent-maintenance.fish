b6cfd0c8abdcf517f684794b9c0eae75250595b3
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-03-11 00:13:43 -0500

    torrent-maintenance

--- .config/fish/functions/torrent-maintenance.fish
+++ .config/fish/functions/torrent-maintenance.fish
@@ -1,5 +1,7 @@
 # Defined interactively
 function torrent-maintenance
+    combine-ssh --hosts pakon backup r730xd len hk --dups -v qbt_hashes.py
+
     ~/bin/qbt_file_sizes.py -v 127.0.0.1:8080 backup:8888 r730xd:8888 hk:8888 len:8888
 
     for s in 127.0.0.1:8080 backup:8888 r730xd:8888 hk:8888 len:8888
