70bbbba6ff124f4e280d49e6b6d195a04631991b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-03-16 17:41:40 -0500

    qbt_tracker_rename

--- bin/qbt_tracker_rename.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-for torrent in torrent_list:
-    for x in torrent.trackers:
-        if OLD_TRACKER in x.url:
-            newurl = x.url.replace(OLD_TRACKER, NEW_TRACKER)
-            print(f"torrent name: {torrent.name}, original url: {x.url}, modified url: {newurl}\n")
-            torrent.remove_trackers(urls=x.url)
-            torrent.add_trackers(urls=newurl)
