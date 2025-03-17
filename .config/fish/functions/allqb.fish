c0145bea139b3172dc8fd75677b935efd4e503da
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-03-17 15:27:27 -0500

    allqb

--- .config/fish/functions/allqb.fish
+++ .config/fish/functions/allqb.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function allqb
-    for s in 127.0.0.1:8080 backup:8888 r730xd:8888 hk:8888 len:8888
+    for s in 127.0.0.1:8080 backup:8888 r730xd:8888 len:8888 hk:8888
         $argv --host $s
     end
 end
