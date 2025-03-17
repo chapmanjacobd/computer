d93686c1f46087b1cae9f5eaf5752a86cc228448
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-03-17 09:42:55 -0500

    process_media

--- .config/fish/functions/process_media.fish
+++ .config/fish/functions/process_media.fish
@@ -1,4 +1,4 @@
 # Defined via `source`
 function process_media
-    lb shrink -vy (fd -td -d2 processing /mnt/) (fd -td -d1 processing /home/xk/) --move ../processed/ --move-broken ../processed-broken/
+    lb shrink -vy (fd -td -d2 processing /mnt/ | grep -v /mnt/d/) (fd -td -d1 processing /home/xk/) --move ../processed/ --move-broken ../processed-broken/
 end
