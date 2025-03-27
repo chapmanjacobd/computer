b0bd6ae6884ed52e1c6caefc2e196325c2b1e59c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-03-27 15:00:36 -0500

    torunqueue

--- .config/fish/functions/torunqueue.fish
+++ .config/fish/functions/torunqueue.fish
@@ -4,7 +4,7 @@ function torunqueue
 
     ~/.local/data/qbittorrent/queue/
     for s in r730xd:8888 backup:8888 127.0.0.1:8080
-        lb torrents --dl --time-active=-1hour --progress=0 --stop --export --delete-files --host $s
+        lb torrents --dl --time-active=-1hour --downloaded=0 --stop --export --delete-files --host $s
     end
     lb torrents-add ~/lb/torrents.db ~/.local/data/qbittorrent/queue/ --force -v
 end
