59a73289e8310f25dc94d5e7ae9381c610520c76
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-04-01 21:29:29 -0500

    cb2pdf

--- .config/fish/functions/cb2pdf.fish
+++ .config/fish/functions/cb2pdf.fish
@@ -1,6 +1,6 @@
-# Defined interactively
+# Defined via `source`
 function cb2pdf
-    set outpath (mktemp --suffix .pdf)
+    set outpath (mktemp /tmp/(datestamp)_XXX --suffix=.pdf --dry-run)
 
     pandoc -o $outpath (cb | psub -s md) -V pagestyle=empty
     dragondrop --on-top --all-compact $outpath
