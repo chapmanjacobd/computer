f8cce86b029de5a6888b1921685ba85e60c01951
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-04-07 15:25:17 -0500

    tor-refresh

--- .config/fish/functions/tor-refresh.fish
+++ .config/fish/functions/tor-refresh.fish
@@ -2,8 +2,11 @@
 function tor-refresh
     tempdir
     for s in pakon backup r730xd len hk
-        lb links --cookies-from-browser firefox --path-include /download/torrent/ --download (ssh -t $s qbt_torrents_refresh.py)
-        ssh -t $s lb torrents --tagged library-refresh --untag library-refresh --start
+        set urls (ssh -t $s qbt_torrents_refresh.py)
+        if test -n "$urls"
+            lb links --cookies-from-browser firefox --path-include /download/torrent/ --download $urls
+            ssh -t $s lb torrents --tagged library-refresh --untag library-refresh --start
+        end
     end
     trash-put (pwd)
 end
