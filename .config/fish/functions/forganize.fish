cde16d6ad7f374b8e84a701133993b5e651af12a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-04-17 01:34:28 -0500

    forganize qbt-unseed

--- .config/fish/functions/forganize.fish
+++ .config/fish/functions/forganize.fish
@@ -2,11 +2,7 @@ function forganize
     morganize
     dorganize
 
-    set processing_dir processing_(datestamp)
-    allpc library torrents --complete --seeders=+4 --time-stalled=+3days --time-seeding=+60days --stop --move $processing_dir --delete-rows -v
-    # stop incomplete
-    allpc library torrents --incomplete --time-active=+60days --time-unseeded=+90days --delete-incomplete --stop --move $processing_dir --tag library-delete -v
-    allpc library torrents --incomplete --time-active=+60days --inactive --downloaded=0 --stop --tag library-no-seed -v
+    qbt-unseed
 
     #trash-size
     #trash-empty
