b1eb6150a28972dfa56daa9829ae2e479d419ff4
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-05-05 00:26:59 -0500

    git-recreate

---
+++ .config/fish/functions/git-recreate.fish
@@ -0,0 +1,9 @@
+# Defined via `source`
+function git-recreate
+    set tmp_fifo (mktemp -u)
+    mkfifo $tmp_fifo
+    git fast-export --signed-tags=strip --all >$tmp_fifo &
+    mkcd ../newrepo/
+    git fast-import <$tmp_fifo
+    rm $tmp_fifo
+end
