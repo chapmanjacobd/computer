2d22615812c1bab7f17761aae535621976553ba4
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-05-07 15:41:54 -0500

    battery_percent

--- .config/fish/functions/battery_percent.fish
+++ .config/fish/functions/battery_percent.fish
@@ -7,6 +7,6 @@ function battery_percent
     if command -vq upower
         upower -i (upower -e | grep 'BAT') | grep percentage | awk '{print $2}'
     else
-        acpi -b | awk '{print $4}' | sed 's/%,//g'
+        acpi -b | awk -F '[:, %]' '{for (i=1; i<=NF; i++) if ($i ~ /^[0-9]+$/) {print $i; exit}}'
     end
 end
