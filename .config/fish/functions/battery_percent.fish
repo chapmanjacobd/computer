81675c174f00073e6562fdc965aa8f3bfd357fcc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-05-07 15:36:18 -0500

    battery_percent

--- .config/fish/functions/battery_percent.fish
+++ .config/fish/functions/battery_percent.fish
@@ -1,8 +1,12 @@
-# Defined interactively
+# Defined via `source`
 function battery_percent
     if set -q CONTAINER_ID; or set -q container
         return
     end
 
-    upower -i (upower -e | grep 'BAT') | grep percentage | awk '{print $2}'
+    if command -vq upower
+        upower -i (upower -e | grep 'BAT') | grep percentage | awk '{print $2}'
+    else
+        acpi -b | awk '{print $4}' | sed 's/%,//g'
+    end
 end
