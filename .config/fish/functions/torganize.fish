741eb46912c245f9de6e6fb63df7d57b2d7efbec
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-05-25 18:35:48 -0500

    torganize torrent-maintenance

--- .config/fish/functions/torganize.fish
+++ .config/fish/functions/torganize.fish
@@ -15,8 +15,6 @@ function torganize
     qbt_computers_remaining.py ~/lb/computers.local.db
     qbt_computers_remaining.py ~/lb/computers.remote.db
 
-    tor-refresh
-
     lb playlists ~/lb/torrents.db -pa
     duckdb ~/lb/torrents.db "select tracker, count(*), format_bytes(sum(size)::BIGINT) size from playlists WHERE time_deleted=0 group by 1 order by sum(size)"
     echo torrent-maintenance
