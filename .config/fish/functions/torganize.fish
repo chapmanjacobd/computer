bc3b3d691794cdb97a102611557c2c0bbf57c62a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-06-12 22:37:37 -0500

    torganize

--- .config/fish/functions/torganize.fish
+++ .config/fish/functions/torganize.fish
@@ -17,5 +17,6 @@ function torganize
 
     lb playlists ~/lb/torrents.db -pa
     duckdb ~/lb/torrents.db "select tracker, count(*), format_bytes(sum(size)::BIGINT) size from playlists WHERE time_deleted=0 group by 1 order by sum(size)"
+    python ~/bin/lowcharts_size_hist.py (lb playlists ~/lb/torrents.db -pf --cols size | psub -s csv)
     echo torrent-maintenance
 end
