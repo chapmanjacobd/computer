43d339b6a6c856cb734f3f4e7a2d97da1f73b939
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-06-13 00:04:01 -0500

    mam-promote

--- .config/fish/functions/mam-promote.fish
+++ .config/fish/functions/mam-promote.fish
@@ -5,14 +5,17 @@ function mam-promote -a max
     if test $max -gt 0
         for dir in new/
             set filled (ssh backup torrent_promote.py .local/data/rtorrent/watch/$dir -n $max | count)
+            echo Moved $filled from new/
             set max (math $max-$filled)
         end
         for dir in vip_new/
             set filled (ssh backup torrent_promote.py .local/data/rtorrent/watch/$dir --reverse -n $max | count)
+            echo Moved $filled from vip_new/
             set max (math $max-$filled)
         end
         for dir in nonvip_new/
             set filled (ssh backup torrent_promote.py .local/data/rtorrent/watch/$dir -n $max | count)
+            echo Moved $filled from nonvip_new/
             set max (math $max-$filled)
         end
     end
