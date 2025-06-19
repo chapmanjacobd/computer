317591160383804f52ea6dde4bc6d56f0da036a7
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-06-19 17:34:22 -0500

    pretty_num

--- bin/pretty_num.py
+++ bin/pretty_num.py
@@ -1,7 +1,16 @@
-#!/usr/bin/env -S python -S
+#!/usr/bin/env -S python
 
-import sys
+import argparse
+from library.utils import argparse_utils, nums
 
-n = sys.stdin.read()
-s = f'{n:,}'.replace(',', '$sep')
-print(s)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sep", "-s", default=",")
+    parser.add_argument(
+        "numbers", nargs="*", default=argparse_utils.STDIN_DASH, action=argparse_utils.ArgparseArgsOrStdin
+    )
+    args = parser.parse_args()
+
+    for n in args.numbers:
+        n = nums.safe_int_float_str(n)
+        print(f'{n:,}'.replace(',', args.sep))
