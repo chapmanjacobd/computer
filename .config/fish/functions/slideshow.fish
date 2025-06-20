03c17e71379cd52cbe6a49daf31ac2a8e811f204
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-06-20 14:58:25 -0500

    slideshow

--- .config/fish/functions/slideshow.fish
+++ .config/fish/functions/slideshow.fish
@@ -1,4 +1,8 @@
 # Defined interactively
 function slideshow
-    python -m library.lb view --action ask_move_or_delete --keep-dir /home/xk/d/library/image/ --prefetch 4 --loop --exit-code-confirm -i --cmd130 exit_multiple_playback -m 2 -u time_modified
+    focus_under_mouse
+
+    python -m library.lb view --action ask_move_or_delete --keep-dir /home/xk/d/library/image/ --prefetch 4 --loop --exit-code-confirm -i --cmd130 exit_multiple_playback -m 2 -u time_modified $argv
+
+    focus_follows_mouse
 end
