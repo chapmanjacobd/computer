667520f68bd6aaf17b04d3f05c68e245ecf99967
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-06-20 14:57:34 -0500

    slideshow

---
+++ .config/fish/functions/slideshow.fish
@@ -0,0 +1,4 @@
+# Defined interactively
+function slideshow
+    python -m library.lb view --action ask_move_or_delete --keep-dir /home/xk/d/library/image/ --prefetch 4 --loop --exit-code-confirm -i --cmd130 exit_multiple_playback -m 2 -u time_modified
+end
