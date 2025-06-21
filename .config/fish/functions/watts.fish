b55539652959656378f27142e789e85c11e434f0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-06-21 18:35:15 -0500

    watts

--- .config/fish/functions/watts.fish
+++ .config/fish/functions/watts.fish
@@ -1,7 +1,24 @@
-# Defined via `source`
 function watts
-    set PERCENT (apcaccess -pLOADPCT | sed 's/Percent//')
-    set UPSMAXWATTS (apcaccess -pNOMPOWER | sed 's/Watts//')
+    set -l apcupsd_was_inactive false
 
-    echo (math "$PERCENT * $UPSMAXWATTS / 100")
+    if not systemctl is-active --quiet apcupsd
+        set apcupsd_was_inactive true
+        sudo systemctl start apcupsd
+        if test $status -ne 0
+            return 1
+        end
+    end
+
+    set -l PERCENT (apcaccess -pLOADPCT | sed 's/Percent//')
+    set -l UPSMAXWATTS (apcaccess -pNOMPOWER | sed 's/Watts//')
+
+    if $apcupsd_was_inactive
+        sudo systemctl stop apcupsd
+    end
+
+    if test -n "$PERCENT" -a -n "$UPSMAXWATTS"
+        echo (math "$PERCENT * $UPSMAXWATTS / 100")
+    else
+        return 2
+    end
 end
