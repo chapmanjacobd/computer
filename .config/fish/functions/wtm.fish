6f6021072fe0c9e7290cfc14c1960cd76a7a876f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-06-29 22:00:28 -0500

    wtm

--- .config/fish/functions/wtm.fish
+++ .config/fish/functions/wtm.fish
@@ -1,8 +1,12 @@
 # Defined interactively
 function wtm
-    focus_under_mouse
+    if not string match -q -- '*-p*' "$argv"
+        focus_under_mouse
+    end
 
     python -m library.lb watch --action ask_move_or_delete --keep-dir /home/xk/d/library/porn/video/ --prefetch 2 --loop --exit-code-confirm -i --cmd130 exit_multiple_playback --cmd5 'python -m library.lb process-audio --no-preserve-video' --cmd6 'mv {} /mnt/d/library/porn/vr/' -m 4 $argv
 
-    focus_follows_mouse
+    if not string match -q -- '*-p*' "$argv"
+        focus_follows_mouse
+    end
 end
