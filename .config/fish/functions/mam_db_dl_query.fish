850b813004d623be1cb6c298ceba1c1c6f59a2ed
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-07-01 18:42:20 -0500

    html_details_code

--- .config/fish/functions/mam_db_dl_query.fish
+++ .config/fish/functions/mam_db_dl_query.fish
@@ -2,7 +2,7 @@
 function mam_db_dl_query --argument db
     load_env_mam
 
-    lb fs "$db" -w 'my_snatched=0' 'seeders>0' $argv[2..-1] -pa
+    lb-dev fs "$db" -w 'my_snatched=0' 'seeders>0' $argv[2..-1] -pa
 
     if confirm
         lb-dev fs "$db" -w 'my_snatched=0' 'seeders>0' 'vip=1' $argv[2..-1] -pf | unique | parallel -S backup -j1 ".local/data/rtorrent/watch/vip_new/; curl -OJs -b mam_id=$MAM_COOKIE https://www.myanonamouse.net/tor/download.php?tid={}; sleep 2"
