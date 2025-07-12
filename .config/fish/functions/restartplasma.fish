6bac3dfe2a7592d8be7106eda18fbde1ce22d627
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-07-12 18:41:50 -0500

    kglobalshortcutsrc

--- .config/fish/functions/restartplasma.fish
+++ .config/fish/functions/restartplasma.fish
@@ -3,5 +3,7 @@ function restartplasma
     bash -c 'kquitapp6 plasmashell || killall plasmashell; systemd-run --user kstart plasmashell'
     systemd-run --user kwin_x11 --replace
     systemctl --user restart pipewire-pulse.socket pipewire-pulse.service pipewire.service wireplumber.service
+
+    sleep 3
     maximize_all
 end
