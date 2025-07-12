f2885ea219b3ff102ed58e941b5663ed04445ebe
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-07-12 17:10:32 -0500

    brightness_step

--- .config/fish/functions/brightness_step.fish
+++ .config/fish/functions/brightness_step.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function brightness_step
-    if test (hostname) = len
+    if test (hostname) = len; and not set -q WAYLAND_DISPLAY
         echo 1
     else
         qdbus org.kde.Solid.PowerManagement /org/kde/Solid/PowerManagement/Actions/BrightnessControl org.kde.Solid.PowerManagement.Actions.BrightnessControl.brightnessSteps
