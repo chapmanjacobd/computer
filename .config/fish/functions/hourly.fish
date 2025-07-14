ea03ef3bb84eaa59c1f805bafde989bb7d050e5c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-07-13 21:00:00 -0500

    hourly

--- .config/fish/functions/hourly.fish
+++ .config/fish/functions/hourly.fish
@@ -5,7 +5,7 @@ function hourly
 
     mam-promote (mam_slots.py --max 5 --cookie $MAM_COOKIE)
 
-    if test (uptime_secs) -gt 3600; and not pgrep -fa torrents
+    if test (uptime_secs) -gt 3600; and not pgrep -fa torrents; and not pgrep -a mv; and not pgrep -fa 'lb mv'
         torganize
         priv-allocate-torrents
         torrent-maintenance
