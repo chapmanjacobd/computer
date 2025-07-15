f29438dd4a552a8d51b1c03ad9b5605a49c4bcb6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-07-14 20:38:31 -0500

    _ytdl

--- .config/fish/functions/_ytdl.fish
+++ .config/fish/functions/_ytdl.fish
@@ -5,6 +5,6 @@ function _ytdl
         --download-archive ~/.local/share/yt_archive.txt --retries 13 --extractor-retries 13 \
         --extractor-args youtubetab:skip=authcheck --embed-metadata \
         --no-progress --sleep-requests 1 \
-        --reject-title "Trailer|Sneak Peek|Preview|Teaser|Promo|Live |Stream|Twitch|Crypto|Meetup|Montage|Bitcoin|Makeup|Apology|Clip |Top 10|Top Ten|Panel|Red Carpet Premiere|Now Playing|In Theaters| Scene |Announcement|First Look|Final Look|Outtakes|360|World Premiere|Event|TV Spot|Advert|campaign|Horror|Graphics Card| GPU | Terror| AWARD|Ceremony" \
+        --reject-title "Trailer|Sneak Peek|Preview|Review|Teaser|Promo|Live |Stream|Twitch|Crypto|Meetup|Montage|Bitcoin|Makeup|Apology|Clip |Top 10|Top Ten|Panel|Red Carpet Premiere|Now Playing|In Theaters| Scene |Announcement|First Look|Final Look|Outtakes|360|World Premiere|Event|TV Spot|Advert|campaign|Horror|Graphics Card| GPU | Terror| AWARD|Ceremony" \
         $argv
 end
