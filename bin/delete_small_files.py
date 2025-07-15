882833fb0b156b9e8a361f7b7130dafe0c998f0a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-07-15 12:26:53 -0500

    delete_small_files

--- bin/delete_small_files.py
+++ bin/delete_small_files.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import argparse
 import os
-import sys
 from pathlib import Path
 
 from library.utils import processes, strings
@@ -25,12 +24,8 @@ def print_info(p):
     print()
 
 
-def clean_directory(args):
-    if not os.path.isdir(args.directory):
-        print(f"Error: Directory '{args.directory}' not found.")
-        sys.exit(1)
-
-    for root, _dirs, files in os.walk(args.directory):
+def clean_directory(args, directory):
+    for root, _dirs, files in os.walk(directory):
         for file in files:
             p = os.path.join(root, file)
             file_size_mb = os.path.getsize(p) / (1024 * 1024)
@@ -49,16 +44,31 @@ def clean_directory(args):
                         print(f"Skipping: {p}")
 
 
+def get_dir_size(path):
+    total_size = 0
+    for dirpath, _dirnames, filenames in os.walk(path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Remove empty folders and small files, with confirmation for non-whitelisted files."
     )
-    parser.add_argument("directory", type=str, help="The target directory to clean.")
+    parser.add_argument("directories", metavar="DIRECTORY", type=str, nargs='+', help="The target directory to clean.")
     parser.add_argument(
         "--max-size-mb",
         type=float,
         default=1.0,
         help="Maximum size in MB for files to be considered 'small' (default: 1.0MB).",
+    )
+    parser.add_argument(
+        "--max-dir-size-mb",
+        type=float,
+        default=5.0,
+        help="Maximum size in MB for a directory to be processed. Directories larger than this will be skipped.",
     )
     parser.add_argument(
         "--small-file-extensions",
@@ -72,7 +82,20 @@ def main():
 
     args.small_file_extensions = [ext.lower() for ext in args.small_file_extensions]
 
-    clean_directory(args)
+    for directory in args.directories:
+        if not os.path.isdir(directory):
+            print(f"Error: Directory '{directory}' not found.")
+            continue
+
+        dir_size_mb = get_dir_size(directory) / (1024 * 1024)
+        if args.max_dir_size_mb and dir_size_mb > args.max_dir_size_mb:
+            print(
+                f"Skipping directory '{directory}' as its size ({dir_size_mb:.2f} MB) "
+                f"exceeds the maximum allowed ({args.max_dir_size_mb:.2f} MB)."
+            )
+            continue
+
+        clean_directory(args, directory)
 
 
 if __name__ == "__main__":
