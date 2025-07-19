501d244c22fef8e2a19816e13767a2714ddecc4e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-07-19 15:17:10 -0500

    fzf-choose-multi fzf-choose locate_folders

---
+++ .config/fish/functions/fzf-choose-multi.fish
@@ -0,0 +1,4 @@
+# Defined interactively
+function fzf-choose-multi
+    fzf-choose --multi --bind ctrl-a:toggle-all
+end
--- .config/fish/functions/locate_folders.fish
+++ .config/fish/functions/locate_folders.fish
@@ -1,4 +1,4 @@
 # Defined via `source`
 function locate_folders
-    plocate $argv | folder_exists.py --all | unique
+    plocate $argv | folder_exists.py --all | unique | fzf-choose-multi
 end
