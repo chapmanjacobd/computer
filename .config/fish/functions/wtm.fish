a203cdf30152500b8ab31718745c7a4c5e725f69
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-07-20 11:41:40 -0500

    wtm

--- .config/fish/functions/wtm.fish
+++ .config/fish/functions/wtm.fish
@@ -4,7 +4,7 @@ function wtm
         focus_under_mouse
     end
 
-    python -m library.lb watch --prefetch 2 --loop --cmd130 exit_multiple_playback --cmd5 'python -m library.lb process-audio --no-preserve-video' --cmd6 'mv {} /mnt/d/library/porn/vr/' -m 4 $argv
+    python -m library.lb watch --prefetch 2 --loop --cmd130 exit_multiple_playback --cmd5 'python -m library.lb process-audio --no-preserve-video' -m 4 --fstack $argv
 
     if not string match -q -- '*-p*' "$argv"
         focus_follows_mouse
