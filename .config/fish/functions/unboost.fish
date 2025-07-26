294f644161c0267f9e9126a5e2f0b58af11b5e9c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-07-26 12:14:28 -0500

    unboost

--- .config/fish/functions/unboost.fish
+++ .config/fish/functions/unboost.fish
@@ -1,4 +1,6 @@
 # Defined interactively
 function unboost
     echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+    echo 0 | sudo tee /sys/devices/system/cpu/cpufreq/boost
+    echo "power" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
 end
