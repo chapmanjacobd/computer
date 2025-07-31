039fb3e8d4d96485c6992109baf6d12e3a80aef8
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-07-30 22:34:13 -0500

    abbreviations wt wtm

--- .config/fish/functions/wt.fish
+++ .config/fish/functions/wt.fish
@@ -3,7 +3,7 @@ function wt
         focus_under_mouse
     end
 
-    /home/xk/.local/bin/lb wt --keep-dir /home/xk/d/library/video/ --action ask_move --exit-code-confirm --ignore-errors --cmd5 'echo skip' --cmd130 exit_multiple_playback --local-media-only ~/lb/video.db $argv
+    /home/xk/.local/bin/lb wt --keep-dir /home/xk/d/library/video/ --action ask_move --exit-code-confirm --ignore-errors --cmd5 'echo skip' --cmd130 exit_multiple_playback --local-media-only --loop ~/lb/video.db $argv
 
     if not string match -q -- '*-p*' "$argv"
         focus_follows_mouse
--- .config/fish/functions/wtm.fish
+++
@@ -1,4 +0,0 @@
-# Defined interactively
-function wtm
-    wt --loop -m 4 --fstack $argv
-end
