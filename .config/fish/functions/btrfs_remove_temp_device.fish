446202b698d7794852727a8c1013a1cfb9ea9b01
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-02 13:14:36 -0500

    btrfs_remove_temp_device

--- .config/fish/functions/btrfs_remove_temp_device.fish
+++ .config/fish/functions/btrfs_remove_temp_device.fish
@@ -1,16 +1,24 @@
-# Defined interactively
+# Defined via `source`
 function btrfs_remove_temp_device --argument-names btrfs_mount
+    if test (count $argv) -ne 1
+        echo "Usage: btrfs_remove_temp_device <btrfs_mountpoint>"
+        return 1
+    end
 
-    for s in (sudo btrfs filesystem show --raw "$btrfs_mount" | grep -oP 'path\s+(\S+)' | cut -d' ' -f2)
+    if not test -d "$btrfs_mount"
+        echo "Error: Btrfs mountpoint '$btrfs_mount' does not exist."
+        return 1
+    end
 
-        if contains -- "$s" (losetup -O NAME --noheadings)
-            while not sudo btrfs device remove --enqueue "$s" "$btrfs_mount"
+    for dev_lo in (sudo btrfs filesystem show --raw "$btrfs_mount" | grep -oP 'path\s+(\S+)' | cut -d' ' -f2)
+        if contains -- "$dev_lo" (losetup -O NAME --noheadings)
+            while not sudo btrfs device remove --enqueue "$dev_lo" "$btrfs_mount"
                 sleep 10
             end
 
-            set -l back_files (losetup "$s" -O BACK-FILE --noheadings)
-            losetup --detach "$s"
-            rm $back_files
+            set -l back_files (losetup "$dev_lo" -O BACK-FILE --noheadings)
+            losetup --detach "$dev_lo"
+            and rm $back_files
         end
     end
 end
