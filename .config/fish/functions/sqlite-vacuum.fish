172b2a92334273d2af0a4ce3f93198dd17c0d54b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-02 18:35:27 -0500

    sqlite-vacuum

--- .config/fish/functions/sqlite-vacuum.fish
+++ .config/fish/functions/sqlite-vacuum.fish
@@ -2,5 +2,5 @@
 function sqlite-vacuum --argument db
     set tmp_bak (mktemp -d)/(path basename $db)
     sqlite3 "$db" "VACUUM main INTO '$tmp_bak'" # or .backup
-    mv "$tmp_bak" $db
+    and mv "$tmp_bak" $db
 end
