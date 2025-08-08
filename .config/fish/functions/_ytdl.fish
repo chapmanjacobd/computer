c5937a4efa1c687f2138d5f48d87b764525abf81
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-08 09:58:47 -0500

    _ytdl.fish: Added text '--cookies-from-browser firefox \'; Deleted text '# --cookies-from-browser firefox \'

--- .config/fish/functions/_ytdl.fish
+++ .config/fish/functions/_ytdl.fish
@@ -5,7 +5,7 @@ function _ytdl
         --extractor-args youtubetab:skip=authcheck --embed-metadata \
         --no-progress --sleep-requests 1 \
         --reject-title "Trailer|Sneak Peek|Preview|Review|Teaser|Promo|Live |Stream|Twitch|Crypto|Meetup|Montage|Bitcoin|Makeup|Apology|Clip |Top 10|Top Ten|Panel|Red Carpet Premiere|Now Playing|In Theaters| Scene |Announcement|First Look|Final Look|Outtakes|360|World Premiere|Event|TV Spot|Advert|campaign|Horror|Graphics Card| GPU | Terror| AWARD|Ceremony" \
-        # --cookies-from-browser firefox \
+        --cookies-from-browser firefox \
         # --user-agent "Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0" \
         $argv
 end
