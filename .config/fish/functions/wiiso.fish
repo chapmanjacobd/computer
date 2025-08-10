c99431f0940319d1ef04ba45a74be9bb7374dd42
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-10 11:47:03 -0500

    wget2rc: Deleted text 'check-certificate = off'

--- .config/fish/functions/wiiso.fish
+++ .config/fish/functions/wiiso.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function wiiso
-    dolphin-tool convert -i "$argv" -f rvz --block_size=131072 --compression=zstd -l 3 -o (path extension rvz "$argv")
+    dolphin-tool convert -i "$argv" -f rvz --block_size=131072 --compression=zstd -l 3 -o (path change-extension rvz "$argv")
 end
