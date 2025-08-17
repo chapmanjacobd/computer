e32d0bf7f593afb72206577fbf720a0a5955be1c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-17 08:19:19 -0500

    Renamed ar.fish

--- .config/fish/functions/ar.fish
+++
@@ -1,6 +0,0 @@
-# Defined in /tmp/fish.sUt5Y8/ar.fish @ line 2
-function ar
-    set target (string replace '/' '' $argv)
-    tar cf "$target".tzst -I zstd "$target"
-    and trash "$target"
-end
---
+++ .config/fish/functions/tzst.fish
@@ -0,0 +1,6 @@
+# Defined in /tmp/fish.sUt5Y8/tzst.fish @ line 2
+function tzst
+    set target (string replace --regex '/$' '' $argv)
+    tar cf "$target".tzst -I zstd "$target"
+    and trash "$target"
+end
