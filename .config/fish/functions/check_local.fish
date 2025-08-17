014e83fbd7339482f0f6740e920468075f470f13
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-17 18:45:49 -0500

    Modified check_local.fish, check_local_video.fish

--- .config/fish/functions/check_local.fish
+++ .config/fish/functions/check_local.fish
@@ -2,6 +2,8 @@
 function check_local
     echo $argv
     lb playlists ~/lb/torrents.db -s $argv -pf --cols title 2>/dev/null | lb rs 2>/dev/null
-    locate_remote_mv.py -v --flex --hosts backup pakon r730xd -- $argv
-    print pakon backup r730xd | parallel sshpc {} lb torrents -- $argv
+
+    set hosts (connectable-ssh backup pakon r730xd)
+    locate_remote_mv.py -v --flex --hosts $hosts -- $argv
+    print $hosts | parallel sshpc {} lb torrents -- $argv
 end
--- .config/fish/functions/check_local_video.fish
+++ .config/fish/functions/check_local_video.fish
@@ -2,6 +2,8 @@
 function check_local_video
     echo $argv
     lb playlists ~/lb/torrents.db -s $argv -p --cols title,size 2>/dev/null
-    locate_remote_mv.py -v -E /sync/ -E /archive/ --flex --hosts backup pakon r730xd -- /video/ $argv
-    print pakon backup r730xd | parallel sshpc {} lb torrents -- $argv
+
+    set hosts (connectable-ssh backup pakon r730xd)
+    locate_remote_mv.py -v -E /sync/ -E /archive/ --flex --hosts $hosts -- /video/ $argv
+    print $hosts | parallel sshpc {} lb torrents -- $argv
 end
