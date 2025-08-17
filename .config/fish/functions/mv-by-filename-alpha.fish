c8118b0190096f4d9409fd061516bff1e55e72ef
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-17 12:26:32 -0500

    mv-by-filename-alpha.fish: Added 12 lines

---
+++ .config/fish/functions/mv-by-filename-alpha.fish
@@ -0,0 +1,14 @@
+# Defined interactively
+function mv-by-filename-alpha
+    for p in (fd -tf --exact-depth 1 . $argv)
+        set first_char (string sub --length 1 "$p")
+
+        if string match --quiet --regex '[a-zA-Z]' -- "$first_char"
+            set target_dir (string upper "$first_char")
+        else
+            set target_dir nonalpha
+        end
+
+        mv "$p" "$target_dir/"
+    end
+end
