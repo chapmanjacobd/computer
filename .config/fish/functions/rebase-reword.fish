d02c3038a7963b8b22a9c1feb4ec4c7dbe7a793a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-23 12:12:14 -0500

    rebase-reword.fish: Added 1 lines; Deleted 1 lines

--- .config/fish/functions/rebase-reword.fish
+++ .config/fish/functions/rebase-reword.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function rebase-reword
-    git rebase -i (git rev-list --max-parents=0 HEAD)^ --exec 'git commit --amend -m "$(wip_message.py HEAD)"'
+    git rebase -i (git log --reverse --pretty=%H | sed -n '2p') --exec 'git commit --amend -m "$(wip_message.py HEAD)"'
 end
