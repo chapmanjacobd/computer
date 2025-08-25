93bcfaf3837bc6f499849474bda001b08ce6a811
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-24 22:04:59 -0500

    check_local_video.fish: Modified 3 line

--- .config/fish/functions/check_local_video.fish
+++ .config/fish/functions/check_local_video.fish
@@ -1,9 +1,9 @@
 # Defined interactively
 function check_local_video
     echo $argv
-    lb playlists ~/lb/torrents.db -s $argv -p --cols title,size 2>/dev/null
+    lb playlists ~/lb/torrents.db -s "$argv" -p --cols title,size 2>/dev/null
 
     set hosts (connectable-ssh backup pakon r730xd)
-    locate_remote_mv.py -v -E /sync/ -E /archive/ --flex --hosts $hosts -- /video/ $argv
-    print $hosts | parallel sshpc {} lb torrents -- $argv
+    locate_remote_mv.py -v -E /sync/ -E /archive/ --flex --hosts $hosts -- /video/ "$argv"
+    print $hosts | parallel sshpc {} lb torrents -- "$argv"
 end
