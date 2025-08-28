099dd5e7ea9c1433339b12155be32804136e26b8
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-28 07:25:19 -0500

    wclone.fish: Added text '# --max-threads=1 --wait=1 --random-wait --https-enforce=none'; Deleted 1 line

--- .config/fish/functions/wclone.fish
+++ .config/fish/functions/wclone.fish
@@ -2,7 +2,7 @@
 function wclone -w wget
     ~/d/dump/text/web/
     wget -np -r -l inf --reject-regex '/tag/|/tags/|\?tag' --continue $argv
+    # --max-threads=1 --wait=1 --random-wait --https-enforce=none
     # --retry-connrefused
     # --ignore-length
-    # TODO: https://github.com/rockdaboot/wget2/blob/master/docs/wget2.md#download-options
 end
