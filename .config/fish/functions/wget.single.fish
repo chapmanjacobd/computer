7e8de049ca6a11f7c4ef66739ec5b74d370f90f9
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-28 11:09:48 -0500

    wget.single.fish: Modified text to 'timeout 5m wget2 --user-agent="$(useragent)" --span-hosts -v "$url"'

--- .config/fish/functions/wget.single.fish
+++ .config/fish/functions/wget.single.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function wget.single
     for url in $argv
-        timeout 5m wget2 --user-agent="$(useragent)" --span-hosts "$url"
+        timeout 5m wget2 --user-agent="$(useragent)" --span-hosts -v "$url"
     end
 end
