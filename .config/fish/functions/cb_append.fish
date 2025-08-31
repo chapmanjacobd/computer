48fd64c2763379d81e4736e9c3a9bdb7fd32fd64
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-31 12:57:14 -0500

    Modified cb_append.fish, cb_append_html.fish

--- .config/fish/functions/cb_append.fish
+++ .config/fish/functions/cb_append.fish
@@ -1,14 +1,19 @@
 # Defined via `source`
 function cb_append --argument-names suffix
     set temp_file (mktemp -t "cb_append_XXXXXX$suffix")
+    echo "$temp_file" >&2
 
     while true
+        cb | head -n 1 >&2
+        cb | tail -n 1 >&2
+        cb >>"$temp_file"
+        echo '' >>"$temp_file"
+
+        echo '' >&2  # This prevents the shell's default 'read>' prompt from overwriting our output in stderr
         read __dummy__
         if test $status != 0
             break
         end
-        cb >>"$temp_file"
-        echo "" >>"$temp_file"
     end
     echo "$temp_file"
 end
--- .config/fish/functions/cb_append_html.fish
+++ .config/fish/functions/cb_append_html.fish
@@ -1,14 +1,19 @@
 # Defined via `source`
 function cb_append_html
     set temp_file (mktemp -t 'cb_append_XXXXXX.html')
+    echo "$temp_file" >&2
 
     while true
+        cb | head -n 1 >&2
+        cb | tail -n 1 >&2
+        cb -t text/html >>"$temp_file"
+        echo "" >>"$temp_file"
+
+        echo '' >&2  # This prevents the shell's default 'read>' prompt from overwriting our output in stderr
         read __dummy__
         if test $status != 0
             break
         end
-        cb -t text/html >>"$temp_file"
-        echo "" >>"$temp_file"
     end
     echo "$temp_file"
 end
