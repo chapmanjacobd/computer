80bf7d4a96e0657106ac9e7e31cd87544b3e7a5f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-08-31 12:47:11 -0500

    Modified cb_append.fish, cb_append_html.fish

--- .config/fish/functions/cb_append.fish
+++ .config/fish/functions/cb_append.fish
@@ -1,13 +1,14 @@
-# Defined interactively
+# Defined via `source`
 function cb_append --argument-names suffix
     set temp_file (mktemp -t "cb_append_XXXXXX$suffix")
 
     while true
-        read
+        read __dummy__
         if test $status != 0
             break
         end
         cb >>"$temp_file"
+        echo "" >>"$temp_file"
     end
     echo "$temp_file"
 end
--- .config/fish/functions/cb_append_html.fish
+++ .config/fish/functions/cb_append_html.fish
@@ -1,13 +1,14 @@
-# Defined interactively
+# Defined via `source`
 function cb_append_html
     set temp_file (mktemp -t 'cb_append_XXXXXX.html')
 
     while true
-        read
+        read __dummy__
         if test $status != 0
             break
         end
         cb -t text/html >>"$temp_file"
+        echo "" >>"$temp_file"
     end
     echo "$temp_file"
 end
