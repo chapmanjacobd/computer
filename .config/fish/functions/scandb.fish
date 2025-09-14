966c11dfa1e61d6329943b23c8edee782dcff754
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-09-14 10:33:29 -0500

    scandb.fish: Modified 2 lines

--- .config/fish/functions/scandb.fish
+++ .config/fish/functions/scandb.fish
@@ -1,5 +1,5 @@
 # Defined interactively
 function scandb --argument-names path
-    lb-dev fsadd --filesystem ~/(path basename -- $path).db $path
-    lb-dev fsadd --video --audio ~/(path basename -- $path).wt.db $path
+    lb fsadd --filesystem (path basename -- $path).db $path
+    lb fsadd --video --audio (path basename -- $path).wt.db $path
 end
