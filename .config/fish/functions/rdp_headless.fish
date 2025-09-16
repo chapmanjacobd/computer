7119a15eb8d1ff6019f5e858c2e41d2e7018274c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-09-16 14:08:23 -0500

    rdp_headless.fish: Added text 'sleep 3'; Modified text to 'sleep 2'

--- .config/fish/functions/rdp_headless.fish
+++ .config/fish/functions/rdp_headless.fish
@@ -2,8 +2,9 @@
 function rdp_headless
     pkill Xvfb
     b Xvfb :99 -screen 0 1024x768x24
+    sleep 2
     DISPLAY=:99 rdpserve
 
-    sleep 5
+    sleep 3
     DISPLAY=:99 dbus-run-session startplasma
 end
