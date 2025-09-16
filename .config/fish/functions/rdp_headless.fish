7af81d0c5d96762907fa7f5895a4e73ce9ab6f36
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-09-16 14:10:34 -0500

    rdp_headless.fish: Added text 'pkill freerdp-shadow-cli⏎pkill Xvfb'; Modified text to 'b DISPLAY=:99 rdpserve'

--- .config/fish/functions/rdp_headless.fish
+++ .config/fish/functions/rdp_headless.fish
@@ -3,8 +3,11 @@ function rdp_headless
     pkill Xvfb
     b Xvfb :99 -screen 0 1024x768x24
     sleep 2
-    DISPLAY=:99 rdpserve
+    b DISPLAY=:99 rdpserve
 
     sleep 3
     DISPLAY=:99 dbus-run-session startplasma
+
+    pkill freerdp-shadow-cli
+    pkill Xvfb
 end
