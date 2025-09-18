82c0ec96b7688c23079eb09b173bcf3ab63e18a9
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-09-17 21:14:46 -0500

    iiab-start.fish: Modified text to 'quickemu --vm ~/iiab/virtual/debian-13.1.0-lxqt.conf --display spice'

--- .config/fish/functions/iiab-start.fish
+++ .config/fish/functions/iiab-start.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function iiab-start
-    quickemu --vm ~/.local/iiab/virtual/debian-13.1.0-lxqt.conf --display spice
+    quickemu --vm ~/iiab/virtual/debian-13.1.0-lxqt.conf --display spice
 end
