c882417e26612cf21f6dfa576eb6fc3ceb859bf6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-09-20 09:34:42 -0500

    qbt-unseed.fish: Added 2 lines

--- .config/fish/functions/qbt-unseed.fish
+++ .config/fish/functions/qbt-unseed.fish
@@ -1,5 +1,7 @@
 function qbt-unseed
     allpc library torrents --complete --seeders=+4 --time-stalled=+3days --time-seeding=+61days --stop --move processing --delete-rows -v
+    # check file existence, prevent ghost seed
+    allpc lb torrents --no-stopped --no-errored --ul --no-all-exists --seeders=-2 --stop --move processing --delete-rows
     # stop incomplete
     allpc library torrents --incomplete --time-active=+61days --time-unseeded=+90days --delete-incomplete --stop --move processing-incomplete --tag library-delete -v
     allpc library torrents --incomplete --time-active=+61days --inactive --downloaded=0 --stop --tag library-no-seed -v
