e6b6e5ca8b92abe1fb0e659f7a7c8bb11a8a109d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-09-20 09:06:30 -0500

    qbt-unseed.fish: Deleted text 'set processing_dir processing/'; Modified 2 lines

--- .config/fish/functions/qbt-unseed.fish
+++ .config/fish/functions/qbt-unseed.fish
@@ -1,7 +1,6 @@
 function qbt-unseed
-    set processing_dir processing/
-    allpc library torrents --complete --seeders=+4 --time-stalled=+3days --time-seeding=+61days --stop --move $processing_dir --delete-rows -v
+    allpc library torrents --complete --seeders=+4 --time-stalled=+3days --time-seeding=+61days --stop --move processing --delete-rows -v
     # stop incomplete
-    allpc library torrents --incomplete --time-active=+61days --time-unseeded=+90days --delete-incomplete --stop --move $processing_dir-incomplete --tag library-delete -v
+    allpc library torrents --incomplete --time-active=+61days --time-unseeded=+90days --delete-incomplete --stop --move processing-incomplete --tag library-delete -v
     allpc library torrents --incomplete --time-active=+61days --inactive --downloaded=0 --stop --tag library-no-seed -v
 end
