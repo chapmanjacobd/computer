f503e06f6f3339b5f4c04ed6a29e6e569f8b9857
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-10-05 11:10:42 -0500

    sqlite-preview.fish: Modified 1 line

--- .config/fish/functions/sqlite-preview.fish
+++ .config/fish/functions/sqlite-preview.fish
@@ -3,7 +3,7 @@ function sqlite-preview
     for db in $argv
         echo \#\# $db
         for table in (sqlite-tables "$db") (sqlite-views "$db")
-            if not string match -q '*_fts_*' $table; and not string match -q '*_fts' $table; and not string match -q sqlite_stat1 $table
+            if not string match -q '*_fts_*' $table; and not string match -q '*_fts' $table; and not string match -q 'sqlite_*' $table
                 echo \#\#\# $table \((sqlite-count "$db" "$table") rows\)
                 lb tables "$db" -t "$table" -L 5
                 echo
