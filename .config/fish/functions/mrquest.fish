d686c236648a6c436fab6e9c2abe30a833c5db4c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-10-07 17:36:21 -0500

    mrquest.fish: Modified text to 'fd -tf --max-results=3 . ~/d/check/porn/vr/ | xargs -I{} mv {} ~/quest/Movies/'

--- .config/fish/functions/mrquest.fish
+++ .config/fish/functions/mrquest.fish
@@ -3,6 +3,6 @@ function mrquest
     ls ~/quest/Movies/
     if confirm
         mv ~/quest/Movies/* ~/d/library/porn/vr/
-        fd --max-results=3 . ~/d/check/porn/vr/ | xargs -I{} mv {} ~/quest/Movies/
+        fd -tf --max-results=3 . ~/d/check/porn/vr/ | xargs -I{} mv {} ~/quest/Movies/
     end
 end
