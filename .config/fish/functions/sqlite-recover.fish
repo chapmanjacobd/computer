198c21e3d07bcaad1774d253cea7032f932560e3
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-10-08 22:56:02 -0500

    sqlite-recover.fish: Modified text to 'mv "$tmp_bak" $db'

--- .config/fish/functions/sqlite-recover.fish
+++ .config/fish/functions/sqlite-recover.fish
@@ -2,5 +2,5 @@
 function sqlite-recover --argument db
     set tmp_bak (mktemp -d)/(path basename $db | path change-extension .db)
     sqlite3 -init /dev/null "$db" ".recover" | sqlite3 -init /dev/null "$tmp_bak"
-    and mv "$tmp_bak" $db
+    mv "$tmp_bak" $db
 end
