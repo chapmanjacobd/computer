ea3eef3985b134b7a512ccc0634a3c5154034522
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-10-12 12:10:06 -0500

    syncweb-release.fish: Added 20 lines

---
+++ .config/fish/functions/syncweb-release.fish
@@ -0,0 +1,25 @@
+# Defined interactively
+function syncweb-release --argument-names newver
+    ~/github/xk/syncweb-py
+
+    set oldver (awk -F'"' '/^__version__/{print $2}' syncweb/__main__.py)
+
+    sed -i "s|$oldver|$newver|" syncweb/__main__.py
+
+    lbformat
+
+    git add .
+    rg -i --no-heading todo:
+    git --no-pager diff "v$oldver"
+    git --no-pager diff "v$oldver" | grep TODO
+    git diff --stat "v$oldver"
+    echo
+    git status
+    if gum confirm --default=no
+        pdm lock
+        git commit -m "$newver"
+        git pull
+        git push
+        git tag -a "v$newver" && git push --tags
+    end
+end
