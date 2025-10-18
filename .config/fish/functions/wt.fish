091db38b66a5d54eff6281f6469dfa2b257c1e09
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-10-18 11:39:06 -0500

    wt.fish: Modified 2 lines

--- .config/fish/functions/wt.fish
+++ .config/fish/functions/wt.fish
@@ -1,11 +1,11 @@
 function wt
-    if not string match -q -- '*-p*' "$argv"
+    if not string match -q -- '* -p*' "$argv"
         focus_under_mouse
     end
 
     /home/xk/.local/bin/lb wt --keep-dir /home/xk/d/library/video/ --action ask_move --exit-code-confirm --ignore-errors --cmd5 'echo skip' --cmd130 exit_multiple_playback --local-media-only --loop ~/lb/video.db $argv
 
-    if not string match -q -- '*-p*' "$argv"
+    if not string match -q -- '* -p*' "$argv"
         focus_follows_mouse
     end
 end
