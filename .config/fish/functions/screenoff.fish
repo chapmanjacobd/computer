16707264f429f5cd4cdf3c022ecfa07326fad501
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-10-23 09:38:34 -0500

    Modified screenoff.fish, screenon.fish

--- .config/fish/functions/screenoff.fish
+++ .config/fish/functions/screenoff.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function screenoff
-    if type wlr-randr
+    if type -q wlr-randr
         # WAYLAND_DISPLAY="wayland-0"
         wlr-randr --output HDMI-A-1 --off
     else
