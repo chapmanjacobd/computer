ef145d2d088d43020bd0c1cd9ce1ef561b989908
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-10-23 09:54:21 -0500

    dnfinstall.fish: Added 5 lines; Deleted text 'if command -v rpm-ostree >/dev/null'

--- .config/fish/functions/dnfinstall.fish
+++ .config/fish/functions/dnfinstall.fish
@@ -1,8 +1,12 @@
 function dnfinstall --wraps='dnf install'
-    if command -v rpm-ostree >/dev/null
+    if type -q rpm-ostree
         sudo rpm-ostree install $argv
         and echo $argv | string split ' ' >>~/.github/ostree_installed
         sort --unique --ignore-case ~/.github/ostree_installed | sponge ~/.github/ostree_installed
+    else if type -q apt
+        sudo apt install $argv
+        and echo $argv | strings split ' ' >>~/.github/apt_installed
+        sort --unique --ignore-case ~/.github/apt_installed | sponge ~/.github/apt_installed
     else
         sudo dnf install $argv
         and echo $argv | string split ' ' >>~/.github/dnf_installed
