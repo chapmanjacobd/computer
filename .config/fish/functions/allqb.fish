b9974e12d8291b64058c9a9e19e1b35a366636df
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-10-26 12:10:22 -0500

    allqb.fish: Added 1 line; Deleted text 'for s in 127.0.0.1:8080 backup:8888 r730xd:8888 len:8888 hk:8888'

--- .config/fish/functions/allqb.fish
+++ .config/fish/functions/allqb.fish
@@ -2,7 +2,7 @@
 function allqb
     set -l all_failed true
 
-    for s in 127.0.0.1:8080 backup:8888 r730xd:8888 len:8888 hk:8888
+    for s in 127.0.0.1:8080 (connectable-ssh backup r730xd len hk | sed "s|\$|:8888|")
         $argv --host $s
 
         if test $status -eq 0
