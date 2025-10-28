2f48accbf03742b8c8ad6232591e8ec1f6f38af6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-10-28 11:46:34 -0500

    iiab-start.fish: Deleted text '~/iiab/virtual/debian-13.1.0-lxqt/debian-13.1.0-lxqt.sh'; Modified 1 line

--- .config/fish/functions/iiab-start.fish
+++ .config/fish/functions/iiab-start.fish
@@ -1,5 +1,4 @@
 # Defined interactively
 function iiab-start
-    # quickemu --vm ~/iiab/virtual/debian-13.1.0-lxqt.conf --display spice $argv
-    ~/iiab/virtual/debian-13.1.0-lxqt/debian-13.1.0-lxqt.sh
+    quickemu --public-dir none --vm ~/iiab/virtual/debian-13.1.0-lxqt.conf --display spice $argv
 end
