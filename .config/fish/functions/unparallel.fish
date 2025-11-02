521c13f590018b4ba007ee3c8c5387f4cc40eec4
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-01 23:08:28 -0500

    unparallel.fish: Modified text to 'ppause (pgrep $argv | tail -n12)⏎ppause (pgrep $argv | tail -n10)'

--- .config/fish/functions/unparallel.fish
+++ .config/fish/functions/unparallel.fish
@@ -2,11 +2,11 @@
 function unparallel
     ps -o etime,state,%mem,%cpu -p (pgrep $argv)
     presume-all
-    ppause (pgrep $argv | tail -n8)
+    ppause (pgrep $argv | tail -n12)
 
     while sleep 5m
         ps -o etime,state,%mem,%cpu -p (pgrep $argv)
         presume-all
-        ppause (pgrep $argv | tail -n8)
+        ppause (pgrep $argv | tail -n10)
     end
 end
