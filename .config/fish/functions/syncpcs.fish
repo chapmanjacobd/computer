38ddff0c96ca62256f8349d62563d3ccfd993397
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-05 09:55:53 -0600

    syncpcs.fish: Added text 'print hk pakon backup r730xd len | parallel sshpc {} pip install --upgrade lb/'

--- .config/fish/functions/syncpcs.fish
+++ .config/fish/functions/syncpcs.fish
@@ -2,4 +2,5 @@
 function syncpcs
     print hk pakon backup r730xd len | parallel sshpc {} git pull
     print hk pakon backup r730xd len | parallel sshpc {} git -C lb/ pull
+    print hk pakon backup r730xd len | parallel sshpc {} pip install --upgrade lb/
 end
