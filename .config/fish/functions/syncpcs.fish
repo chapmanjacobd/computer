0c693bc6bb6764b4cbfeaf2b92e9f12889ab7b06
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-09 12:39:45 -0600

    syncpcs.fish: Added text 'ssh len clean_home'

--- .config/fish/functions/syncpcs.fish
+++ .config/fish/functions/syncpcs.fish
@@ -1,5 +1,6 @@
 # Defined interactively
 function syncpcs
+    ssh len clean_home
     print hk pakon backup r730xd len | parallel sshpc {} git pull
     print hk pakon backup r730xd len | parallel sshpc {} git -C lb/ pull
     print hk pakon backup r730xd len | parallel sshpc {} pip install --upgrade lb/
