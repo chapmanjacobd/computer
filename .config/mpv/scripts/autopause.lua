a0fc87245b3a983af6d37a8028d04c33eb848522
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-12 10:11:06 -0600

    autopause.lua: 26 lines added, 3 lines deleted

--- .config/mpv/scripts/autopause.lua
+++ .config/mpv/scripts/autopause.lua
@@ -1,12 +1,35 @@
+local pause_delay = 0.3
+local pause_timer = nil
+
+local function do_pause()
+    if mp.get_property_bool("pause") == false then
+        mp.set_property_bool("pause", true)
+    end
+    pause_timer = nil
+end
+
 local function on_focus_change(name, value)
     if mp.get_property("video") == "no" or not mp.get_property_native("fullscreen") then
+        if pause_timer then
+            pause_timer:kill()
+            pause_timer = nil
+        end
         return
     end
 
     if value == true then -- mpv window is focused
-        mp.set_property_bool("pause", false)
-    else -- mpv window lost focus
-        mp.set_property_bool("pause", true)
+        if pause_timer then
+            pause_timer:kill()
+            pause_timer = nil
+        else
+            mp.set_property_bool("pause", false)
+        end
+    else
+        if pause_timer then
+            return
+        end
+        
+        pause_timer = mp.add_timeout(pause_delay, do_pause)
     end
 end
 
