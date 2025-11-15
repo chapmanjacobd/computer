1f513623860ca6801f2e2e1362ae073eef453032
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-14 22:04:01 -0600

    Modified audioCompressAndSplit.fish, funcmv.fish, fish_renames.jsonl

--- .config/fish/functions/audioCompressAndSplit.fish
+++ .config/fish/functions/audioCompressAndSplit.fish
@@ -1,5 +1,5 @@
 function audioCompressAndSplit
-    for file in (filesDeep . -r | fileTypeVideos)
+    for file in $argv
         ffmpeg -y -i $file -filter_complex "compand=attacks=0:points=-80/-900|-45/-15|-27/-9|0/-7|20/-7:gain=5" -c:a libopus -f segment -segment_time 93 (basename -- $file | tr -s ' ' | tr ' ' '_')%09d.opus
     end
 end
--- .config/fish/functions/funcmv.fish
+++ .config/fish/functions/funcmv.fish
@@ -1,7 +1,7 @@
 # Defined interactively
 function funcmv --argument old new
     set funcdir ~/.config/fish/functions
-    sed "s|\b$old\b|$new|" -i $funcdir/*.fish
+    sed "s|\b$old\b|$new|" -i $funcdir/*.fish ~/.local/share/fish/fish_history
     mv $funcdir/$old.fish $funcdir/$new.fish
     echo abbrsave $old $new
 end
