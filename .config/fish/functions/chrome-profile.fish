befa8507693cc0c2b550afa3bade92895748cfd6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-15 15:12:18 -0600

    Modified abbreviations, fish_renames.jsonl; Deleted chrome-profile.fish, connectpg.fish

--- .config/fish/functions/chrome-profile.fish
+++
@@ -1,5 +0,0 @@
-function chrome-profile
-    set -l dir ~/.chrome-profile/$argv[1]
-    mkdir $dir
-    env google-chrome --user-data-dir=$dir
-end
--- .config/fish/functions/connectpg.fish
+++
@@ -1,4 +0,0 @@
-# Defined interactively
-function connectpg
-    ssh -N -L 5432:localhost:5432 $argv
-end
