52d512ec39b4364503877153786131783ff64389
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-15 00:01:35 -0600

    Modified battery_percent.fish, fish_renames.jsonl; Deleted backupdl.fish

--- .config/fish/functions/backupdl.fish
+++
@@ -1,7 +0,0 @@
-# Defined via `source`
-function backupdl
-    ~/.local/data/
-    sshfs -o noauto,noatime,_netdev,reconnect,ConnectTimeout=10,ServerAliveInterval=8,TCPKeepAlive=no xk@pakon:/mnt/d/ ~/.local/data/d/
-    sshfs -o noauto,noatime,_netdev,reconnect,ConnectTimeout=10,ServerAliveInterval=8,TCPKeepAlive=no xk@pakon:lb/ ~/.local/data/lb/
-    lb dl --image --photos lb/fs/61_Photos_Unsorted.db --prefix d/dump/porn/image/gallery-dl/
-end
--- .config/fish/functions/battery_percent.fish
+++ .config/fish/functions/battery_percent.fish
@@ -8,10 +8,4 @@ function battery_percent
     if test -e "$bat_file"
         cat "$bat_file" | head -1
     end
-
-    # if command -vq upower
-    #    upower -i (upower -e | grep 'BAT') | grep percentage | awk '{print $2}'
-    # else
-    #    acpi -b | grep -oP '\d+%' | sed 's/%//g' | head -1
-    # end
 end
