3dec0675eee05300771b74d6af80e889a27d0501
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-15 16:26:40 -0600

    Modified url_files.fish, fish_renames.jsonl

--- .config/fish/functions/url_files.fish
+++ .config/fish/functions/url_files.fish
@@ -1,9 +1,14 @@
 # Defined interactively
 function url_files
     find . -type f -print0 | xargs -0 -n1 awk '
-    /^https/ {count++}
+    /^https/ {
+        url_count++
+    }
+    !/^https/ {
+        non_url_count++
+    }
     END {
-        if (count >= 3) {
+        if (url_count >= 3 && non_url_count <= 2) {
             print FILENAME
         }
     }
