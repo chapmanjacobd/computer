366fb89db8576ddca4727623c636f99efb0d6f2e
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-23 17:35:21 -0600

    fish_greeting.fish: Added text '# start at bottom of screen⏎printf '\033[%s;1H' $LINES'

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,4 +1,6 @@
 # Defined in /tmp/fish.1r7oen/fish_greeting.fish @ line 2
 function fish_greeting
+    # start at bottom of screen
+    printf '\033[%s;1H' $LINES
     cat /proc/loadavg | cut -d' ' -f 2
 end
