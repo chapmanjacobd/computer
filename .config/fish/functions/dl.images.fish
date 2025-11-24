cafc860b89f2b9c830eae8906cec680400353274
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-24 15:45:44 -0600

    Modified abbreviations, dl.insta.fish; Renamed dl.images.fish

--- .config/fish/functions/dl.images.fish
+++ .config/fish/functions/dl.images.fish
@@ -1,5 +1,5 @@
 # Defined interactively
-function gdl
+function dl.images
     ~/d/dump/porn/image/
     gallery-dl --filter "extension in ('jpg', 'jpeg', 'webp')" --download-archive $HOME/.local/share/gallerydl.sqlite3 $argv
 end
--- .config/fish/functions/dl.insta.fish
+++ .config/fish/functions/dl.insta.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function dl.insta
     #with_backoff.sh 
-    gdl https://www.instagram.com/$argv/
+    dl.images https://www.instagram.com/$argv/
     echo $argv >>~/mc/61_Photos_Unsorted-instagram.txt
 end
