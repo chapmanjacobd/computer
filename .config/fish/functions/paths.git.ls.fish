5974a6669853b6d817e77a72cd9cab343a0902e6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-24 15:48:41 -0600

    Modified abbreviations; Renamed paths.git.ls.fish

--- .config/fish/functions/paths.git.ls.fish
+++ .config/fish/functions/paths.git.ls.fish
@@ -1,4 +1,4 @@
 # Defined in /home/xk/.config/fish/functions/gitls.fish @ line 1
-function gitls
+function paths.git.ls
     git ls-files --sparse --full-name -z | xargs -0 -I FILE -P 20 git log --date=iso-strict-local --format='%ad %>(14) %cr %<(5) %an  %h ./FILE' -- FILE | sort --general-numeric-sort
 end
