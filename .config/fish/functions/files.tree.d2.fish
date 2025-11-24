d4ac0d5c285b59b0c01205b1e24858aa59f64d65
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-24 13:27:58 -0600

    Modified fish_variables, funcmv.fish; Renamed files.tree.d2.fish; Renamed folders.tree.d2.fish

--- .config/fish/functions/files.tree.d2.fish
+++ .config/fish/functions/files.tree.d2.fish
@@ -1,4 +1,4 @@
-function f
+function files.tree.d2
     setterm -linewrap off
     tree -FLh 3 --sort=mtime --dirsfirst --prune --du --noreport -up -C $argv | grep -v '/$'
     setterm -linewrap on
--- .config/fish/functions/folders.tree.d2.fish
+++ .config/fish/functions/folders.tree.d2.fish
@@ -1,5 +1,5 @@
 # Defined in - @ line 2
-function l --description 'alias l=tree -L 3 --sort=mtime --dirsfirst -f -h --prune --du --noreport -up'
+function folders.tree.d2 --description 'alias folders.tree.d2=tree -L 3 --sort=mtime --dirsfirst -f -h --prune --du --noreport -up'
     setterm -linewrap off
     tree -Lh 2 --sort=mtime --dirsfirst -d -h --prune --du -up -C $argv
     setterm -linewrap on
