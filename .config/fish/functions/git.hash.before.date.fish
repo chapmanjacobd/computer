02d419ec215bc4b4893845a29471ff14f1e06797
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-24 15:29:23 -0600

    Modified funcmv.fish; Renamed git.hash.before.date.fish

--- .config/fish/functions/git.hash.before.date.fish
+++ .config/fish/functions/git.hash.before.date.fish
@@ -1,5 +1,5 @@
 # Defined via `source`
-function gitcommitbefore
-    # git diff (gitcommitbefore Oct 10)
+function git.hash.before.date
+    # git diff (git.hash.before.date Oct 10)
     git rev-list --max-count=1 --before=(date -d "$argv" +"%Y-%m-%d") HEAD
 end
