eaa638eea771e3e25b4d4275cb01658c83c8949b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-24 17:41:41 -0600

    paths.audio.fish

--- .config/fish/functions/paths.audio.fish
+++ .config/fish/functions/paths.audio.fish
@@ -1,4 +1,4 @@
 # Defined via `source`
-function audio
+function paths.audio
     grep -E \\.(python3 -c 'from library.utils import consts; print("(" + "|".join(consts.AUDIO_ONLY_EXTENSIONS) + ")")')\$
 end
