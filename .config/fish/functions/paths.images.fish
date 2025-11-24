175de80755fda9513f7fbe1352664e5778eed6cf
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-24 15:46:30 -0600

    paths.images.fish

--- .config/fish/functions/paths.images.fish
+++ .config/fish/functions/paths.images.fish
@@ -1,4 +1,4 @@
 # Defined via `source`
-function image
+function paths.images
     grep -E \\.(python3 -c 'from library.utils import consts; print("(" + "|".join(consts.IMAGE_EXTENSIONS) + ")")')\$
 end
