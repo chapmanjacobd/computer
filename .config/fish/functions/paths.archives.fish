e611d911e8f686f5733f4afa23f666b774895dcb
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-24 15:47:55 -0600

    paths.archives.fish

--- .config/fish/functions/paths.archives.fish
+++ .config/fish/functions/paths.archives.fish
@@ -1,4 +1,4 @@
 # Defined interactively
-function archives
+function paths.archives
     grep -iE \\.(python3 -c 'from library.utils import consts; print("(" + "|".join(consts.ARCHIVE_EXTENSIONS) + ")$")')
 end
