442e65f0d8f050c7cea85b420855e9f2d7cf93f1
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-24 18:04:45 -0600

    Deleted fileTypeVideos.fish; Renamed paths.video.fish

--- .config/fish/functions/fileTypeVideos.fish
+++
@@ -1,4 +0,0 @@
-# Defined interactively
-function fileTypeVideos
-    grep -E $argv 'avi$|mp4$|m4a$|m4v$|mov$|mkv$|webm$|ogv$|flv$|wmv$|ogm$'
-end
--- .config/fish/functions/paths.video.fish
+++ .config/fish/functions/paths.video.fish
@@ -1,4 +1,4 @@
 # Defined via `source`
-function video
+function paths.video
     grep -E \\.(python3 -c 'from library.utils import consts; print("(" + "|".join(consts.VIDEO_EXTENSIONS) + ")")')\$
 end
