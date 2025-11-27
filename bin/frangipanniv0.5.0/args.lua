1a3c1d2d3c3534c4832ee48c6cb6e90bc9acca6a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-27 16:57:22 -0600

    Modified go_installed; Deleted 5 files in bin/frangipanniv0.5.0, pishrink.sh, zimdump

--- bin/frangipanniv0.5.0/args.lua
+++
@@ -1,5 +0,0 @@
---
--- Output command-line in JSON format
---
-local json = require("json")
-print(json.encode(frangipanni_args))
--- bin/frangipanniv0.5.0/json.lua
+++
@@ -1,6 +0,0 @@
---
--- Output in JSON format
---
-local json = require("json")
-
-print(json.encode(frangipanni))
