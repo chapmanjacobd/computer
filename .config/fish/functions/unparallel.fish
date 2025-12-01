2a12a161ccf3e12d2b241e9c53ae0595287acc2b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-12-01 10:24:25 -0600

    unparallel.fish: Modified text to 'ps.resume.all⏎ps.resume.all'

--- .config/fish/functions/unparallel.fish
+++ .config/fish/functions/unparallel.fish
@@ -1,11 +1,11 @@
 # Defined interactively
 function unparallel
-    ps.resume-all
+    ps.resume.all
     ps.pause (pgrep $argv | offset 4)
     ps -o etime,state,%mem,%cpu -p (pgrep $argv)
 
     while sleep 30s
-        ps.resume-all
+        ps.resume.all
         ps.pause (pgrep $argv | offset 6)
         ps -o etime,state,%mem,%cpu -p (pgrep $argv)
     end
