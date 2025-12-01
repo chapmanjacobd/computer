249ba3c8ba4ccef08877f55d63114be3c09149d4
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-11-30 20:38:08 -0600

    _ytdl.fish: Added 2 lines; Deleted 1 line

--- .config/fish/functions/_ytdl.fish
+++ .config/fish/functions/_ytdl.fish
@@ -1,6 +1,7 @@
 function _ytdl
     yt-dlp \
-        -o "%(ie_key,extractor_key,extractor)s/%(uploader,uploader_id,channel,channel_id,creator,artist,author,playlist_uploader,playlist_uploader_id,playlist_channel,playlist_channel_id)s - %(album,playlist_title,playlist,playlist_id,webpage_url_basename)s/%(title).200B_%(view_count)3.2D_[%(id).60B].%(ext)s" \
+        -o "%(extractor_key,extractor,ie_key).220B/%(uploader,uploader_id,channel,channel_id,creator,artist,author,playlist_uploader,playlist_uploader_id,playlist_channel,playlist_channel_id,webpage_url_basen
+ame).220B/%(album,playlist_title,playlist,playlist_id).220B/%(title).170B_%(view_count)3.2D_[%(id).64B].%(ext)s" \
         --download-archive ~/.local/share/yt_archive.txt --retries 13 --extractor-retries 13 \
         --embed-metadata --no-progress --sleep-requests 1 --compat-options no-live-chat \
         --reject-title "Trailer|Sneak Peek|Preview|Review|Teaser|Promo|Live |Stream|Twitch|Crypto|Meetup|Montage|Bitcoin|Makeup|Apology|Clip |Top 10|Top Ten|Panel|Red Carpet Premiere|Now Playing|In Theaters| Scene |Announcement|First Look|Final Look|Outtakes|360|World Premiere|Event|TV Spot|Advert|campaign|Horror|Graphics Card| GPU | Terror| AWARD|Ceremony" \
