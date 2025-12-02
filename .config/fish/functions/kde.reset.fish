f5b75ca5f16939d1045172cc0c46044bdcfa9f1c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-12-01 18:16:29 -0600

    kde.reset.fish: Added text 'ps.pause ffmpeg⏎ps.resume.all'

--- .config/fish/functions/kde.reset.fish
+++ .config/fish/functions/kde.reset.fish
@@ -1,9 +1,12 @@
 # Defined interactively
 function kde.reset
+    ps.pause ffmpeg
+
     bash -c 'kquitapp6 plasmashell || killall plasmashell; systemd-run --user kstart plasmashell'
     systemd-run --user kwin_x11 --replace
     systemctl --user restart pipewire-pulse.socket pipewire-pulse.service pipewire.service wireplumber.service
 
     sleep 3
     windows.maximize.all
+    ps.resume.all
 end
