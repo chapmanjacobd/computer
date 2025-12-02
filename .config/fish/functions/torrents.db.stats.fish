327e665c4100f2bbc5f11c35e94a0aacb07cf5dc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-12-02 11:46:20 -0600

    torrents.db.stats.fish: Added 1 line; Deleted 1 line

--- .config/fish/functions/torrents.db.stats.fish
+++ .config/fish/functions/torrents.db.stats.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function torrents.db.stats
     lb playlists ~/lb/torrents.db -pa
-    duckdb ~/lb/torrents.db "select tracker, count(*), format_bytes(sum(size)::BIGINT) size from playlists WHERE time_deleted=0 group by 1 order by sum(size)"
+    duckdb ~/lb/torrents.db "select tracker, count(*) count, format_bytes(sum(size)::BIGINT) total_size, format_bytes(min(size)::BIGINT) min_size, format_bytes(max(size)::BIGINT) max_size from playlists WHERE time_deleted=0 group by 1 order by sum(size)"
     python ~/bin/lowcharts_size_hist.py (lb playlists ~/lb/torrents.db -pf --cols size | psub -s csv)
 end
