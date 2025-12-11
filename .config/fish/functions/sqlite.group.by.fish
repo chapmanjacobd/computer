f7eb88258c4affbb33df40650ab2218571150c80
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-12-10 23:19:53 -0600

    sqlite.group.by.fish: Added text 'if test -z "$where"⏎set where "1=1"⏎end'; Modified 2 lines

--- .config/fish/functions/sqlite.group.by.fish
+++ .config/fish/functions/sqlite.group.by.fish
@@ -1,4 +1,8 @@
 # Defined interactively
-function sqlite.group.by --argument db table column
-    sqlite3 $db "select $column, count(*) from $table group by 1 order by 2"
+function sqlite.group.by --argument db table column where
+    if test -z "$where"
+        set where "1=1"
+    end
+
+    sqlite3 $db "select $column, count(*) from $table where $where group by 1 order by 2"
 end
