c0f08047c8851f69cbdfcaab59b7bc3850771cfc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2025-12-14 17:33:40 -0600

    Deleted __fish_history_long_postexec.fish, __fish_history_long_preexec.fish

--- .config/fish/functions/__fish_history_long_postexec.fish
+++
@@ -1,4 +0,0 @@
-# Defined interactively
-function __fish_history_long_postexec --on-event fish_postexec
-    set -e __fish_ignore_history
-end
--- .config/fish/functions/__fish_history_long_preexec.fish
+++
@@ -1,6 +0,0 @@
-# Defined interactively
-function __fish_history_long_preexec --on-event fish_preexec
-    if test (count (string split \n -- $argv)) -ge 10
-        set -g __fish_ignore_history 1
-    end
-end
