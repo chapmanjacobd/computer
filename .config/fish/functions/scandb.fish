9a73928dd82e8d7b6bdc5343cadc6dd93da38f3b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-01-01 20:47:21 -0600

    scandb.fish: Modified 1 line

--- .config/fish/functions/scandb.fish
+++ .config/fish/functions/scandb.fish
@@ -1,5 +1,5 @@
 # Defined interactively
 function scandb --argument-names path
     lb fsadd --filesystem ~/disks/(path basename -- $path).fs.db $path
-    lb fsadd --video --audio ~/disks/(path basename -- $path).wt.db $path
+    lb fsadd --video --audio --delete-unplayable ~/disks/(path basename -- $path).wt.db $path
 end
