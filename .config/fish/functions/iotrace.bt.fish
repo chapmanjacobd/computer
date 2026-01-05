28f9c65b4749e69cc0245bdc5576cfa3d75b4b7d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-01-04 19:43:54 -0600

    Added iotrace.bt.fish; Renamed iotrace.detailed.bt

---
+++ .config/fish/functions/iotrace.bt.fish
@@ -0,0 +1,4 @@
+# Defined interactively
+function iotrace.bt
+    sudo bpftrace -e 'tracepoint:block:block_rq_issue { @bytes[comm] = sum(args.bytes); } interval:s:1 { exit(); }' | grep -vE 'kworker|btrfs-transacti' | bpftrace_human_size.awk
+end
