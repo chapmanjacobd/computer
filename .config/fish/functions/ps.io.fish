d2cb4e09c2be40a62dd9912eb285b5a046ea0793
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-01-04 19:45:17 -0600

    ps.io.fish

--- .config/fish/functions/ps.io.fish
+++ .config/fish/functions/ps.io.fish
@@ -1,4 +1,4 @@
 # Defined interactively
-function iotrace.bt
+function ps.io
     sudo bpftrace -e 'tracepoint:block:block_rq_issue { @bytes[comm] = sum(args.bytes); } interval:s:1 { exit(); }' | grep -vE 'kworker|btrfs-transacti' | bpftrace_human_size.awk
 end
