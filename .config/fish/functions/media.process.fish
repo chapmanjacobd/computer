42d872b8ea92beabb2b5031fcdb1c07933937b6c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-01-12 11:14:34 -0600

    media.process.fish: Modified 1 line

--- .config/fish/functions/media.process.fish
+++ .config/fish/functions/media.process.fish
@@ -1,4 +1,4 @@
 # Defined via `source`
 function media.process
-    printf 'lb shrink -vy %s --move ../processed/ --move-broken ../processed-broken/ --delete-unplayable\n' (fd -td --exact-depth=2 . /mnt/ | grep -Ev '/lost\+found/|/processed/|/processing-incomplete/|/processed-broken/|/downloading/|/seeding/|/mnt/quest/|/mnt/scratch/') (fd -HI -td -d1 processing /home/xk/) | grep -v /mnt/d/ | sort -u | head -n (nproc) | tmux.waitgroup
+    printf 'lb shrink -vy %s --move ../processed/ --move-broken ../processed-broken/ --delete-unplayable\n' (fd -td --exact-depth=2 . /mnt/ | grep -Ev '/lost\+found/|/processed/|/processing-incomplete/|/processed-broken/|/downloading/|/seeding/|/mnt/quest/|/mnt/scratch/') (fd -HI -td -d1 processing /home/xk/) | grep -v /mnt/d/ | sort -u | head -n (threads.max 2) | tmux.waitgroup
 end
