aa6e19c7ed57fb9140fc012935e70b777f381370
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-01-13 00:43:56 -0600

    mam_upload_credit.sh: Added 9 lines

--- bin/mam_upload_credit.sh
+++ bin/mam_upload_credit.sh
@@ -25,7 +25,19 @@ if [ $POINTS -gt $UPLOADPOINTS ]
 then
   echo More than $UPLOADPOINTS points - buying ${UPLOAD}G of upload
   if [ $(date +%w) -eq 0 ]; then
+    # 10 weeks in seconds (10 * 7 * 24 * 60 * 60)
+    THRESHOLD=6048000
+
+    target_date=$(cat $SPEND_JSON 2>/dev/null | jq -r '.vip_until')
+    target_seconds=$(date -d "$target_date" +%s)
+    now_seconds=$(date +%s)
+
+    diff=$((target_seconds - now_seconds))
+
+    if [ "$diff" -lt "$THRESHOLD" ]; then
+      echo "Less than 10 weeks away."
       curl -s -b mam_id=$MAM_COOKIE "https://www.myanonamouse.net/json/bonusBuy.php/?spendtype=VIP&duration=max&_=$(date +%s)"
+    fi
   fi
   curl -s -b mam_id=$MAM_COOKIE 'https://www.myanonamouse.net/json/bonusBuy.php/?spendtype=upload&amount=Max%20Affordable%20&_='$(date +%s) -H 'Referer: https://www.myanonamouse.net/store.php'
 else
