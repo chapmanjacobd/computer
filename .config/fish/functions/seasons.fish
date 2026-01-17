a5e7d19b2b9dabc686483af7e2d1d7632302bca5
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-01-17 11:51:46 -0600

    seasons.fish: Added 9 lines

---
+++ .config/fish/functions/seasons.fish
@@ -0,0 +1,9 @@
+# Defined interactively
+function seasons
+    print Season(seq -w 0 20)
+    print Season(seq 0 20)
+    print "Season "(seq -w 0 20)
+    print "Season "(seq 0 20)
+    print S(seq -w 0 20)
+    print S(seq 0 20)
+end
