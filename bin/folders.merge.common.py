ba6bf855f6249333dded1bddbc543250044b16f7
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-01-17 20:28:03 -0600

    folders.merge.common.py: Modified 1 line

--- bin/folders.merge.common.py
+++ bin/folders.merge.common.py
@@ -40,7 +40,7 @@ def get_all_folders(root_paths, exclude_names):
             if s in exclude_names:
                 continue
 
-            if bool(re.match(r"^(disc|disk|dvd|d|season|s|volume|vol|v|batch)[ ._-]?\d+$", s, re.IGNORECASE)):
+            if bool(re.match(r"^(disc|disk|cd|dvd|d|season|s|volume|vol|v|batch)[ ._-]?\d+$", s, re.IGNORECASE)):
                 continue
 
             depth = rel_path.count(os.sep)
