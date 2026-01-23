5b60e26acf69bf36c0359388d5f3e2ff5c91f831
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-01-22 20:11:20 -0600

    cb.html.plaintext.fish: Modified 1 line

--- .config/fish/functions/cb.html.plaintext.fish
+++ .config/fish/functions/cb.html.plaintext.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function cb.html.plaintext
-    lb extract-text --skip-links --local-file (cb.file.html) | lb cs --groups | jq -r '.[] | .grouped_paths | "\n" + join("\n")'
+    lb extract-text --skip-links --local-html (cb.file.html) | lb cs --groups | jq -r '.[] | .grouped_paths | "\n" + join("\n")'
 end
