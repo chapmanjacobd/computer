f2517dc1d743470931a43a363fcff83d5aabff88
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-01-25 10:22:59 -0600

    tmux.append.window.fish: 7 lines added, 1 lines deleted

--- .config/fish/functions/tmux.append.window.fish
+++ .config/fish/functions/tmux.append.window.fish
@@ -1,4 +1,10 @@
 # Defined in /home/xk/.config/fish/functions/tmux.append.fish @ line 2, copied in /home/xk/.config/fish/functions/funccp.fish @ line 3
 function tmux.append.window
-    tmux new-window -d fish -c (string join -- ' ' (string escape -- $argv))
+    if test -t 0
+        tmux new-window -d -- fish -c (string join -- ' ' (string escape -- $argv))
+    else
+        while read -l cmd
+            tmux new-window -d -- $cmd
+        end
+    end
 end
