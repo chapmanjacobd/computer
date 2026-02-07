36432cacf7eb2088e633f0275e6e863d2e025e73
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-02-07 12:32:07 -0600

    daily.links.extra.fish: 2 lines added, 2 lines deleted

--- .config/fish/functions/daily.links.extra.fish
+++ .config/fish/functions/daily.links.extra.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function daily.links.extra
-    for db in ~/mc/*.db
-        lb openlinks --max-same-domain 2 "$db" -w 'play_count=0' -rs --browser
+    for db in ~/mc/*.db ~/lb/sites/manual/pornolab.net.db
+        lb openlinks --max-same-domain 1 "$db" -w 'play_count=0' -rs --browser
     end
 end
