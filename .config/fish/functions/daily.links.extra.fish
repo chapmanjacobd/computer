846d91db31e12b72b2d18b70dec37116fbf5516a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-02-12 17:37:00 -0600

    Modified daily.links.extra.fish, tabs.service

--- .config/fish/functions/daily.links.extra.fish
+++ .config/fish/functions/daily.links.extra.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function daily.links.extra
     for db in ~/mc/*.db ~/lb/sites/manual/pornolab.net.db
-        lb openlinks --max-same-domain 1 "$db" -w 'play_count=0' -rs --browser -c p0
+        lb openlinks --max-same-domain 1 "$db" -w 'play_count=0' -rs --browser
     end
 end
