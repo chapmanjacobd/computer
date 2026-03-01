abc967f90c14c496b4aab83b81b792cc0c210d05
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-03-01 11:28:12 -0600

    file.lines.filter.fish: Added 4 lines; Modified text to 'function file.lines.filter --argument-names file'

--- .config/fish/functions/file.lines.filter.fish
+++ .config/fish/functions/file.lines.filter.fish
@@ -1,5 +1,10 @@
 # Defined interactively
-function file.lines.filter --argument file
+function file.lines.filter --argument-names file
+    if not test -f "$file"
+        echo "Error: '$file' is not a valid file." >&2
+        return 1
+    end
+
     for word in $argv[2..-1]
         if test -n "$word"
             grep -i "$word" "$file"
