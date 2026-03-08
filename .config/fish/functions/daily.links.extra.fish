c0228d230325b12495083a48650b4163cfc08ebd
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-03-08 15:51:22 -0500

    daily.links.extra.fish: Modified text to 'command links open -R -m1 -L7 --db "$db" $argv'

--- .config/fish/functions/daily.links.extra.fish
+++ .config/fish/functions/daily.links.extra.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function daily.links.extra
     for db in ~/mc/*.db ~/lb/sites/manual/pornolab.net.db
-        command links open -R -m1 -L7 --db "$db"
+        command links open -R -m1 -L7 --db "$db" $argv
     end
 end
