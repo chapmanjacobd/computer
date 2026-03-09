57415cd5d69a343dab395bcbcf1261ada02c2b32
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-03-09 14:28:55 -0500

    gws.fish: 4 lines added, 1 lines deleted

--- .config/fish/functions/gws.fish
+++ .config/fish/functions/gws.fish
@@ -1,4 +1,7 @@
 # Defined interactively
 function gws
-    fd -td -d2 . ~/.git-worktrees
+    fd -td -d2 . ~/.git-worktrees \
+        | fzf --preview 'git -C {} status -sb' \
+        | read -l dir
+    and cd $dir
 end
