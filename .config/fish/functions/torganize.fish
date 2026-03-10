f4dd5013a4a4dfce087a4d0c95a073a05d0bf7c3
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-03-10 14:51:25 -0500

    torganize.fish: Added 5 lines; Deleted 2 lines

--- .config/fish/functions/torganize.fish
+++ .config/fish/functions/torganize.fish
@@ -3,12 +3,15 @@ function torganize
     parallel server.ssh {} qbt_prioritize.py ::: $servers
 
     if test (count ~/Downloads/\[(seq 0 9)*.torrent) -gt 0
-        rsync -auh --remove-sent-files ~/Downloads/\[(seq 0 9)*.torrent backup:.local/data/rtorrent/watch/new/
+        lb mv (fd --max-depth=1 '\[[0-9].*\.torrent' --changed-before 5mins ~/Downloads/) /net/backup/.local/data/rtorrent/watch/new/
     end
 
     lb playlists ~/lb/torrents.db -pa
 
-    lb mv -etorrent ~/Downloads/ ~/.local/data/qbittorrent/queue/
+    set files (fd --max-depth 1 '.torrent$' --changed-before '5min' ~/Downloads/)
+    if test (count $files) -gt 0
+        lb mv $files ~/.local/data/qbittorrent/queue/
+    end
     lb torrents-add ~/lb/torrents.db ~/.local/data/qbittorrent/queue/ -v --delete-files
 
     torrents.db.stats
