94eea2d4d4905fee13f6769a0a7c5d2c1bc882b4
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-03-16 11:19:23 -0500

    git.tag.delete.fish: 2 lines added, 2 lines deleted

--- .config/fish/functions/git.tag.delete.fish
+++ .config/fish/functions/git.tag.delete.fish
@@ -1,5 +1,5 @@
 # Defined interactively
-function git.tag.delete
+function git.tag.delete --wraps='git tag -d'
     git tag --delete $argv
-    git push --delete :refs/tags/$argv
+    git push origin :refs/tags/$argv
 end
