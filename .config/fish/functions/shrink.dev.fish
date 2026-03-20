045d32cc0846a00336a4fa695f71254387c027e4
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-03-20 15:24:03 -0500

    shrink.dev.fish: Added 4 lines

---
+++ .config/fish/functions/shrink.dev.fish
@@ -0,0 +1,4 @@
+# Defined in /home/xk/.config/fish/functions/disco.dev.fish @ line 2, copied in /home/xk/.config/fish/functions/funccp.fish @ line 3
+function shrink.dev
+    go run ./cmd/shrink $argv
+end
