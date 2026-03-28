a72b8249e0bef6c4f5716cdbee5b0804fd451fff
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-03-27 23:35:25 -0500

    dorganize.fish: Modified 6 lines

--- .config/fish/functions/dorganize.fish
+++ .config/fish/functions/dorganize.fish
@@ -7,16 +7,16 @@ function dorganize
     unardel *.7z
     unardel *.xz
 
-    fd -d1 -eEPUB -edjvu -x mv "{}" ~/d/dump/text/ebooks/
-    fd -d1 -eHTML -ePDF -x mv "{}" ~/d/dump/text/web/
+    fd -d1 -eEPUB -edjvu -x mv "{}" (d dump/text/ebooks/)
+    fd -d1 -eHTML -ePDF -x mv "{}" (d dump/text/web/)
 
     fd -d1 -eJPEG -x mv "{}" {.}.jpg
-    fd -d1 -eJPG -ePNG -eWEBP -eGIF -eAVIF -x mv "{}" ~/d/dump/image/
+    fd -d1 -eJPG -ePNG -eWEBP -eGIF -eAVIF -x mv "{}" (d dump/image/)
 
-    lb relmv --ext mid . ~/d/dump/audio/midi/
-    lb relmv --ext srt,ass,ssa,vtt,sub,idx . ~/d/dump/video/
+    lb relmv --ext mid . (d dump/audio/midi/)
+    lb relmv --ext srt,ass,ssa,vtt,sub,idx . (d dump/video/)
     fd -ersrc -enfo -x rm
-    lb relmv --ext mp3,wav,mka . ~/d/dump/audio/
+    lb relmv --ext mp3,wav,mka . (d dump/audio/)
 
     folders.empty.delete
     ls
