b23c618748de30ff67e91b2f88d30fc7d238057b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-04-02 13:22:44 -0500

    shrink.dev.fish: Added text '~/github/xk/shrink'

--- .config/fish/functions/shrink.dev.fish
+++ .config/fish/functions/shrink.dev.fish
@@ -1,4 +1,5 @@
 # Defined in /home/xk/.config/fish/functions/disco.dev.fish @ line 2, copied in /home/xk/.config/fish/functions/funccp.fish @ line 3
 function shrink.dev
+    ~/github/xk/shrink
     go run -tags fts5 ./cmd/shrink $argv
 end
