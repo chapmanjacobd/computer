52ffdc71407bc3e429d8e8aea76c4cd7f3228166
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-04-09 15:35:47 -0500

    fixup! uncommit.fish: Added text 'git log HEAD -1'

--- .config/fish/functions/uncommit.fish
+++ .config/fish/functions/uncommit.fish
@@ -1,5 +1,6 @@
 function uncommit
     git reset --soft HEAD^
     git log HEAD -1
-    echo git push --force-with-lease --force-if-includes
+    echo
+    commandline -r -- 'git push --force-with-lease --force-if-includes'
 end
