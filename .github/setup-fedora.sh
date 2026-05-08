44b9b77e8fd331a9091f0b143cf0c214486243ca
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-05-08 18:48:47 -0500

    setup-fedora.sh: Added text 'curl https://zed.dev/install.sh | sh'; Deleted 19 lines; Modified 2 lines

--- .github/setup-fedora.sh
+++ .github/setup-fedora.sh
@@ -5,8 +5,8 @@ set -Eeuox pipefail
 #source "${BASH_SOURCE%/setup-server.sh}"
 sudo systemctl set-default graphical.target
 
-sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
-sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld || true
+sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld || true
 #sudo dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
 #sudo dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
 
@@ -18,30 +18,7 @@ sudo dnf group install Multimedia -y
 sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
 sudo dnf install -y lame\* --exclude=lame-devel
 
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf install -y code
-
-cat .github/vscode_extensions.txt | xargs -I{} -n1 code --install-extension {} --force
-
-sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
-[google-cloud-sdk]
-name=Google Cloud SDK
-baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el8-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=0
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOM
-
-sudo dnf install -y google-cloud-sdk
-
-sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
-sudo dnf install -y codium
-
-sudo dnf copr enable cboxdoerfer/fsearch
+curl https://zed.dev/install.sh | sh
 
 sudo dnf update
 sudo dnf install -y mpv ffmpeg python3 python3-pip git android-tools R ImageMagick bat code copyq darktable java-latest-openjdk detox cowsay dnscrypt-proxy exa evince expect fdk-aac fdupes fish gdal fzf gdal-devel ffmpeg-devel gdal-java geos-devel gdal-python-tools gimp git-delta git-lfs htop inotify-tools jq kdiff3 keepassxc kitty krename lato-fonts libpq-devel libreoffice mediawriter meld moreutils ncdu nmon nomacs pdfmod pg_top picard plasma-workspace-x11 podman-docker pspg python3-qgis ripgrep rstudio-desktop syncthing udunits2 wine xdotool xclip xbacklight trash-cli wget fedora-workstation-repositories dnf-plugins-extras-tracer cpufrequtils postgresql-devel breeze-gtk gdal-devel sqlite-devel bash-completion fd-find krusader xmodmap sxhkd xsel xinput kcron parallel mkvtoolnix oniguruma-devel libacl-devel libattr-devel libcap-devel btrfsmaintenance ddcutil dnf-automatic xset rmlint skanpage mpv-mpris code fsearch
