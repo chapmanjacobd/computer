7435716dc6e68b6a726a7a053c931be5dc173652
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-05-18 18:51:10 +0000

    .config/fish/functions/search.web.fish

--- .config/fish/functions/search.web.fish
+++ .config/fish/functions/search.web.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function search.web
-    library expand-links -s(fzf --multi < ~/.local/share/search_urls.list) $argv | open_browser.py
+    library expand-links -s(fzf --multi < ~/.local/share/search_urls.list) "$argv" | open_browser.py
 end
