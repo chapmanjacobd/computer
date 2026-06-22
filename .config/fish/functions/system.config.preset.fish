86f6110086c2a266e7c0493265eeb6c580ed6137
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-06-22 05:16:03 +0000

    .config/fish/functions/system.config.preset.fish:

--- .config/fish/functions/system.config.preset.fish
+++ .config/fish/functions/system.config.preset.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function system.config.preset
-    sudo rm /etc/xdg/plasma-workspace/env/nx-sourceenv.sh
+    sudo rm -f /etc/xdg/plasma-workspace/env/nx-sourceenv.sh
 
     if not grep (hostnamectl | grep -i "static hostname:" | cut -f2- -d:) /etc/hosts
         echo -e '127.0.0.1\t' $(hostnamectl | grep -i "static hostname:" | cut -f2- -d:) | sudo tee -a /etc/hosts
