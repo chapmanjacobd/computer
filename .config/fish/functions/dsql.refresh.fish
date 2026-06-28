445fd1ca7a8075747106907b256fc047bfe789c3
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-06-28 17:39:01 -0500

    dsql.refresh.fish: Modified 4 lines

--- .config/fish/functions/dsql.refresh.fish
+++ .config/fish/functions/dsql.refresh.fish
@@ -1,10 +1,10 @@
 # Defined interactively
 function dsql.refresh
-    set -l name (path basename $argv)
+    set -l name (path basename "$argv")
 
     set joblog (mktemp)
     for m in $argv/*
-        echo lb fsadd --filesystem ~/disks/$name.fs.db "$m"
-    end | parallel --shuf --joblog $joblog
-    parallel --retry-failed --joblog $joblog -j1
+        echo lb fsadd --filesystem ~/disks/"$name.fs.db" "$m"
+    end | parallel --shuf --joblog "$joblog"
+    parallel --retry-failed --joblog "$joblog" -j1
 end
