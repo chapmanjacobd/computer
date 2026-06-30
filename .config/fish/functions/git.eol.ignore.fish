8def99f27f52498475e21419725ddec8213a78da
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-06-30 18:24:13 -0500

    Modified git.eol.ignore.fish, settings.json

--- .config/fish/functions/git.eol.ignore.fish
+++ .config/fish/functions/git.eol.ignore.fish
@@ -3,5 +3,5 @@ function git.eol.ignore
     git config --local core.autocrlf false
     git config --local core.safecrlf false
     git config --local core.text false
-    # echo '* -text' >> .git/info/attributes
+    echo '* -text' >> .git/info/attributes
 end
