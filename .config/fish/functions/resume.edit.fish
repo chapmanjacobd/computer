e8ca12273989287fd538069f5e22ed2cfd1e6769
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-07-16 20:01:23 -0500

    resume.edit.fish: Added text 'zed .'; Deleted text '$VISUAL .'

--- .config/fish/functions/resume.edit.fish
+++ .config/fish/functions/resume.edit.fish
@@ -1,7 +1,7 @@
 # Defined interactively
 function resume.edit
     cd ~/github/o/resume/
-    $VISUAL .
+    zed .
 
     open outputs/jchapman_resume.pdf
     while inotifywait -e modify inputs/resume.yaml templates/*/*
