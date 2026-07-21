965e607b0a4bf5a3b4ca1054ed1ec47d5ba3ad16
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-07-20 20:53:47 -0500

    Modified cargoinstall.fish, cargo_installed

--- .config/fish/functions/cargoinstall.fish
+++ .config/fish/functions/cargoinstall.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function cargoinstall --wraps='cargo install'
-    cargo install $argv
+    cargo install --locked $argv
     and echo $argv | string split ' ' >>~/.github/cargo_installed
 
     sort --unique --ignore-case ~/.github/cargo_installed | sponge ~/.github/cargo_installed
