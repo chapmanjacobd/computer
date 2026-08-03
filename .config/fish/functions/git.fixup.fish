fb9ff744efeb48cda969017fd53de89fa683dd02
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-08-03 00:34:24 -0500

    git.fixup.fish: 1 lines added, 1 lines deleted

--- .config/fish/functions/git.fixup.fish
+++ .config/fish/functions/git.fixup.fish
@@ -12,7 +12,7 @@ function git.fixup --argument-names target
 
     set -l target_subject (git log -1 --format=%s $commit)
     set -l clean_subject (string replace -r '^(fixup! |squash! |amend! )+' '' $target_subject)
-    
+
     git commit -m "fixup! $clean_subject"
     # git push
 end
