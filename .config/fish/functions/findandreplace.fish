766ae71d0dfc0ccafad57740a0548b838ed30db0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-08-05 23:04:38 -0500

    Modified findandreplace.fish, findandreplace.regex.fish

--- .config/fish/functions/findandreplace.fish
+++ .config/fish/functions/findandreplace.fish
@@ -1,8 +1,13 @@
 # Defined interactively
 function findandreplace -a find -a replace
-    set -l files (rg -i -. --files-with-matches --fixed-strings "$find" | tee /dev/tty)
-
-    if confirm
+    if set -q argv[3]
+        set -l files $argv[3..-1]
         sd --string-mode "$find" "$replace" $files
+    else
+        set -l files (rg -i -. --files-with-matches --fixed-strings "$find" | tee /dev/tty)
+
+        if confirm
+            sd --string-mode "$find" "$replace" $files
+        end
     end
 end
--- .config/fish/functions/findandreplace.regex.fish
+++ .config/fish/functions/findandreplace.regex.fish
@@ -1,8 +1,13 @@
 # Defined via `source`
 function findandreplace.regex --argument-names find replace
-    set -l files (rg -i -. --files-with-matches "$find" | tee /dev/tty)
-
-    if confirm
+    if set -q argv[3]
+        set -l files $argv[3..-1]
         sd "$find" "$replace" $files
+    else
+        set -l files (rg -i -. --files-with-matches "$find" | tee /dev/tty)
+
+        if confirm
+            sd "$find" "$replace" $files
+        end
     end
 end
