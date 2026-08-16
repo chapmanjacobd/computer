124b09209f2e12dda4460f205da68463334d1b3f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-08-16 00:48:46 -0500

    lines.count.fish: Added text 'if not set -q argv[1]⏎cat | count⏎return⏎end'

--- .config/fish/functions/lines.count.fish
+++ .config/fish/functions/lines.count.fish
@@ -1,5 +1,10 @@
 # Defined interactively
 function lines.count
+    if not set -q argv[1]
+        cat | count
+        return
+    end
+
     for s in $argv
         wc -l "$s" | cut -f1 -d' '
     end
