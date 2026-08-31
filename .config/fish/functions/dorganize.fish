f7c3b64b12491acf80bf2d119c740982413c3423
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-08-30 23:45:41 -0500

    dorganize.fish: Added text 'fd --no-ignore -ersrc -enfo -x rm'; Deleted text 'fd -ersrc -enfo -x rm'; Modified 4 lines

--- .config/fish/functions/dorganize.fish
+++ .config/fish/functions/dorganize.fish
@@ -8,15 +8,15 @@ function dorganize
     unardel *.7z
     unardel *.xz
 
-    fd -d1 -eEPUB -edjvu -x mv "{}" (d dump/text/ebooks/)
-    fd -d1 -eHTML -ePDF -x mv "{}" (d dump/text/web/)
+    fd -d1 --no-ignore -eEPUB -edjvu -x mv "{}" (d dump/text/ebooks/)
+    fd -d1 --no-ignore -eHTML -ePDF -x mv "{}" (d dump/text/web/)
 
-    fd -d1 -eJPEG -x mv "{}" {.}.jpg
-    fd -d1 -eJPG -ePNG -eWEBP -eGIF -eAVIF -x mv "{}" (d dump/image/)
+    fd -d1 --no-ignore -eJPEG -x mv "{}" {.}.jpg
+    fd -d1 --no-ignore -eJPG -ePNG -eWEBP -eGIF -eAVIF -x mv "{}" (d dump/image/)
 
     lb relmv --ext mid . (d dump/audio/midi/)
     lb relmv --ext srt,ass,ssa,vtt,sub,idx . (d dump/video/)
-    fd -ersrc -enfo -x rm
+    fd --no-ignore -ersrc -enfo -x rm
     lb relmv --ext mp3,wav,mka . (d dump/audio/)
 
     folders.empty.delete
