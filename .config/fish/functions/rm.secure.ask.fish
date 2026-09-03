082a5da827b3fc5112c3ad65a4a6eb32a4c7113b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-09-03 00:18:43 -0500

    rm.secure.ask.fish: Added 5 lines; Deleted text 'echo⏎echo (count (find $target -type f)) files'; Modified 3 lines; Moved 2 lines

--- .config/fish/functions/rm.secure.ask.fish
+++ .config/fish/functions/rm.secure.ask.fish
@@ -1,19 +1,21 @@
 function rm.secure.ask
     if test (count $argv) -eq 0
-        echo "Usage: rm.secure.ask <directory>..." >&2
+        echo "Usage: rm.secure.ask <file-or-directory>..." >&2
         return 1
     end
 
     for target in $argv
-        if not test -d $target
-            echo "rm.secure.ask: $target: not a directory" >&2
+        if test -d $target
+            set -l nfiles (count (find $target -type f))
+            echo "--- $target ($nfiles files) ---"
+            find $target -type f -exec ls -lh {} \; | awk '{print $5, $NF}'
+        else if test -f $target
+            echo "--- $target ---"
+            ls -lh $target | awk '{print $5, $NF}'
+        else
+            echo "rm.secure.ask: $target: not a file or directory" >&2
             continue
         end
-
-        echo "--- $target ---"
-        find $target -type f -exec ls -lh {} \; | awk '{print $5, $NF}'
-        echo
-        echo (count (find $target -type f)) files
         echo
 
         if confirm "Shred $target?"
