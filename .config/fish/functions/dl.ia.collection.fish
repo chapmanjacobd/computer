ed8c7867968f5d934fb2eb8fe566123738fb7ac0
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-09-09 19:31:01 -0500

    Modified dl.ia.collection.fish, dl.ia.fish

--- .config/fish/functions/dl.ia.collection.fish
+++ .config/fish/functions/dl.ia.collection.fish
@@ -1,6 +1,8 @@
 # Defined interactively
 function dl.ia.collection
-    for s in $argv
+    for collection in $argv
+        set -l s (string replace --regex '^https://archive\.org/details/' '' "$collection")
+        set s (string split / "$s")[1]
         mkdir "$s"
         ia search "collection:$s" --itemlist | parallel --eta -j6 --timeout 800s ia download --source original {} --checksum --destdir "./$s"
     end
--- .config/fish/functions/dl.ia.fish
+++ .config/fish/functions/dl.ia.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function dl.ia
-    print $argv | parallel --eta -j6 --timeout 800s ia download --source original {} --checksum
+    print $argv | string replace --all 'https://archive.org/details/' '' | parallel --eta -j6 --timeout 800s ia download --source original {} --checksum
 end
