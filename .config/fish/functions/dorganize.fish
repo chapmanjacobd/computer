7bb21be1cbc230112ffcad14a6dc6ed745592842
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-09-20 07:51:57 -0500

    dorganize.fish: 3 lines added, 3 lines deleted

--- .config/fish/functions/dorganize.fish
+++ .config/fish/functions/dorganize.fish
@@ -3,9 +3,9 @@ function dorganize
     ~/sync/world/downloads/
 
     if test (count ~/Downloads/\[(seq 0 9)*.torrent) -gt 0
-        lb mv (fd --max-depth=1 '\[[0-9].*\.torrent' ~/Downloads/) ~/.local/data/rtorrent/watch/new/
+        lb mv ~/Downloads/\[(seq 0 9)*.torrent ~/.local/data/rtorrent/watch/new/
     end
-    lb mv . -etorrent ~/.local/data/qbittorrent/queue/
+    lb mv -etorrent . ~/.local/data/qbittorrent/queue/
 
     lb unar -y *Subtitle*
 
@@ -25,6 +25,6 @@ function dorganize
     fd --no-ignore -ersrc -enfo -x rm
     lb relmv --ext mp3,wav,mka . (d dump/audio/)
 
-    folders.empty.delete
+    folders.empty.delete .
     ls
 end
