c064c58650400dbf4645dab7be7a765a04f96085
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-09-21 14:36:39 -0500

    torganize.fish: Added 3 lines; Deleted 2 lines

--- .config/fish/functions/torganize.fish
+++ .config/fish/functions/torganize.fish
@@ -2,8 +2,9 @@
 function torganize
     parallel server.ssh {} qbt.prioritize ::: $servers
 
-    if test (count ~/Downloads/\[(seq 0 9)*.torrent) -gt 0
-        lb mv (fd --max-depth=1 '\[[0-9].*\.torrent' --changed-before 5mins ~/Downloads/) ~/.local/data/rtorrent/watch/new/
+    set rtorrent_files (fd --max-depth=1 '\[[0-9].*\.torrent' --changed-before 5mins ~/Downloads/)
+    if test (count $rtorrent_files) -gt 0
+        lb mv $rtorrent_files ~/.local/data/rtorrent/watch/new/
     end
 
     lb playlists ~/lb/torrents.db -pa
