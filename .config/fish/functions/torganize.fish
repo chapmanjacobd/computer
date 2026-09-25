5904cbd74c0191fdc7248bac16764e8adef408d7
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2026-09-24 23:10:53 -0500

    torganize.fish: Modified 2 lines

--- .config/fish/functions/torganize.fish
+++ .config/fish/functions/torganize.fish
@@ -2,14 +2,14 @@
 function torganize
     parallel server.ssh {} qbt.prioritize ::: $servers
 
-    set rtorrent_files (fd --max-depth=1 '\[[0-9].*\.torrent' --changed-before 5mins ~/Downloads/)
+    set rtorrent_files (fd --no-ignore --max-depth=1 '\[[0-9].*\.torrent' --changed-before 5mins ~/Downloads/)
     if test (count $rtorrent_files) -gt 0
         lb mv $rtorrent_files ~/.local/data/rtorrent/watch/new/
     end
 
     lb playlists ~/lb/torrents.db -pa
 
-    set files (fd --max-depth 1 '.torrent$' --changed-before '5min' ~/Downloads/)
+    set files (fd --no-ignore --max-depth 1 '.torrent$' --changed-before '5min' ~/Downloads/)
     if test (count $files) -gt 0
         lb mv $files ~/.local/data/qbittorrent/queue/
     end
